fab6368107b1631946d878b903112e20f3add022
Joey Zhou <josephyzhou@gmail.com>
2014-07-16 21:28:20 -0700

    typo

--- neth/main.cpp
+++ neth/main.cpp
@@ -134,9 +134,9 @@ string credits()
 	int pocnumber = stoi(vs);
 	string m_servers;
 	if (pocnumber == 4)
-		m_servers = Eth::PrevSeedAddress;
+		m_servers = eth::PrevSeedAddress;
 	else
-		m_servers = Eth::SeedAddress;
+		m_servers = eth::SeedAddress;
 
 	ccout << "Type 'netstart 30303' to start networking" << endl;
 	ccout << "Type 'connect " << m_servers << " 30303' to connect" << endl;
