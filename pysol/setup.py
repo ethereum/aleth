780bb7951bf26049a8d09d824db382269d583cbb
Lefteris Karapetsas <lefteris@refu.co>
2015-04-09 12:34:01 +0200

    pysol work in progress

--- pysol/setup.py
+++ pysol/setup.py
@@ -1,9 +1,7 @@
+from setuptools import setup, Extension
+from distutils.sysconfig import get_config_vars
 import os
 os.chdir('..')
-
-from setuptools import setup, Extension
-
-from distutils.sysconfig import get_config_vars
 
 (opt,) = get_config_vars('OPT')
 os.environ['OPT'] = " ".join(
@@ -27,9 +25,21 @@ setup(
     ext_modules=[
         Extension(
             'solidity',         # Python name of the module
-            sources= ['libdevcore/Common.cpp', 'libdevcore/CommonData.cpp', 'libdevcore/CommonIO.cpp', 'libdevcore/FixedHash.cpp', 'libdevcore/Guards.cpp', 'libdevcore/Log.cpp', 'libdevcore/RangeMask.cpp', 'libdevcore/RLP.cpp', 'libdevcore/Worker.cpp', 'libdevcrypto/AES.cpp', 'libdevcrypto/Common.cpp', 'libdevcrypto/CryptoPP.cpp', 'libdevcrypto/ECDHE.cpp', 'libdevcrypto/FileSystem.cpp', 'libdevcrypto/MemoryDB.cpp', 'libdevcrypto/OverlayDB.cpp', 'libdevcrypto/SHA3.cpp', 'libdevcrypto/TrieCommon.cpp', 'libdevcrypto/TrieDB.cpp', 'libethcore/CommonEth.cpp', 'libethcore/CommonJS.cpp', 'libethcore/Exceptions.cpp', 'libsolidity/AST.cpp', 'libsolidity/ASTJsonConverter.cpp', 'libsolidity/ASTPrinter.cpp', 'libsolidity/CompilerContext.cpp', 'libsolidity/Compiler.cpp', 'libsolidity/CompilerStack.cpp', 'libsolidity/CompilerUtils.cpp', 'libsolidity/DeclarationContainer.cpp', 'libsolidity/ExpressionCompiler.cpp', 'libsolidity/GlobalContext.cpp', 'libsolidity/InterfaceHandler.cpp', 'libsolidity/NameAndTypeResolver.cpp', 'libsolidity/Parser.cpp', 'libsolidity/Scanner.cpp', 'libsolidity/SourceReferenceFormatter.cpp', 'libsolidity/Token.cpp', 'libsolidity/Types.cpp', 'libevmcore/Assembly.cpp', 'libevmcore/Instruction.cpp', 'pysol/pysolidity.cpp'],
-            libraries=['boost_python', 'boost_filesystem', 'boost_chrono', 'boost_thread', 'cryptopp', 'leveldb', 'jsoncpp'],
-            include_dirs=['/usr/include/boost', '..', '../..', '.'],
+            sources=['pysol/pysolidity.cpp'],
+            library_dirs=['build/libsolidity/',
+                          'build/libdevcrypto/'],
+            libraries=['boost_python',
+                       'boost_filesystem',
+                       'boost_chrono',
+                       'boost_thread',
+                       'devcrypto',
+                       'leveldb',
+                       'jsoncpp',
+                       'solidity'],
+            include_dirs=['/usr/include/boost',
+                          '..',
+                          '../..',
+                          '.'],
             extra_compile_args=['--std=c++11', '-Wno-unknown-pragmas']
         )],
     py_modules=[
