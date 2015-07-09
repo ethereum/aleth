ee1b1fb2be0782c5016f81bca27f054c5613e83e
Dimitry Khokhlov <winsvega@mail.ru>
2015-07-09 23:41:37 +0400

    Coverage: file rename

--- test/libethereum/AccountDiff.cpp
+++ test/libethereum/AccountDiff.cpp
@@ -14,7 +14,7 @@
 	You should have received a copy of the GNU General Public License
 	along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
 */
-/** @file unit.cpp
+/** @file AccountDiff.cpp
  * @author Dimitry Khokhlov <Dimitry@ethdev.com>
  * @date 2015
  * libethereum unit test functions coverage.
