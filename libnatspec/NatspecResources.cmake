019ac3c9736e7f4b2b81987ea4ff7c737caac908
debris <marek.kotewicz@gmail.com>
2015-08-23 23:41:58 +0200

    libnatspec is jsengine agnostic

---
+++ libnatspec/NatspecResources.cmake
@@ -0,0 +1,5 @@
+set(natspec "${ETH_RES_DIR}/natspecjs/natspec.js")
+
+set(ETH_RESOURCE_NAME "NatspecResources")
+set(ETH_RESOURCE_LOCATION "${CMAKE_CURRENT_BINARY_DIR}")
+set(ETH_RESOURCES "natspec")
