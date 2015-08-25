3ffff24eb1673cad801d1a55685e11970b564d72
Paweł Bylica <chfast@gmail.com>
2015-08-25 15:44:16 +0200

    Do not try to find built libraries in Eth package.

--- FindEth.cmake
+++ FindEth.cmake
@@ -19,20 +19,7 @@ endif()
 set(ALL_LIBS ${CORE_LIBS};evmjit;solidity;secp256k1;testutils)
 
 set(ETH_INCLUDE_DIRS ${ETH_INCLUDE_DIR})
-set(ETH_CORE_LIBRARIES ${ETH_LIBRARY})
-
-foreach (l ${ALL_LIBS})
-	string(TOUPPER ${l} L)
-	find_library(ETH_${L}_LIBRARY
-		NAMES ${l}
-		PATH_SUFFIXES "lib${l}" "${l}" "lib${l}/Release"
-	)
-endforeach()
-
-foreach (l ${CORE_LIBS})
-	string(TOUPPER ${l} L)
-	list(APPEND ETH_CORE_LIBRARIES ${ETH_${L}_LIBRARY})
-endforeach()
+set(ETH_CORE_LIBRARIES ${CORE_LIBS})
 
 # handle the QUIETLY and REQUIRED arguments and set ETH_FOUND to TRUE
 # if all listed variables are TRUE, hide their existence from configuration view
@@ -40,4 +27,3 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(ethereum DEFAULT_MSG
 	ETH_CORE_LIBRARIES)
 mark_as_advanced (ETH_CORE_LIBRARIES)
-
