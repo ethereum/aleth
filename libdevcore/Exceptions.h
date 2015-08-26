a8c31cfa917b5462594c2c82f6e2862e355c847f
arkpar <arkady.paronyan@gmail.com>
2015-08-26 15:32:12 +0200

    cleaned up includes

--- libdevcore/Exceptions.h
+++ libdevcore/Exceptions.h
@@ -23,8 +23,12 @@
 
 #include <exception>
 #include <string>
-#include <boost/exception/all.hpp>
+#include <boost/exception/exception.hpp>
+#include <boost/exception/info.hpp>
+#include <boost/exception/info_tuple.hpp>
+#include <boost/exception/diagnostic_information.hpp>
 #include <boost/throw_exception.hpp>
+#include <boost/tuple/tuple.hpp>
 #include "CommonData.h"
 #include "FixedHash.h"
 
