629c92b6773a6279e777724670fbab0572650d06
Lefteris Karapetsas <lefteris@refu.co>
2015-08-27 11:16:10 +0200

    Adding scripts to help building the split repositories

---
+++ scripts/ethupdate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# author: Lefteris Karapetsas <lefteris@refu.co>
+#
+# A script to update the different ethereum repositories to latest develop
+# Invoke from the root directory and make sure you have upstream set as the
+# canonical repository and origin as your fork
+
+ROOT_DIR=$(pwd)
+PROJECTS=(cpp-ethereum cpp-ethereum-cmake webthree solidity alethzero mix)
+
+for project in "${PROJECTS[@]}"
+do
+    cd $project >/dev/null 2>/dev/null
+    if [[ $? -ne 0 ]]; then
+	echo "Skipping ${project} because directory does not exit";
+	continue
+    fi
+    BRANCH="$(git symbolic-ref HEAD 2>/dev/null)" ||
+    BRANCH="(unnamed branch)"     # detached HEAD
+    BRANCH=${BRANCH##refs/heads/}
+    if [[ $BRANCH != "develop" ]]; then
+	echo "Not updating ${project} because it's not in the develop branch"
+	continue
+    fi
+
+    # Assuming origin is your fork and upstream is the canonical repository under the /ethereum domain
+    git pull upstream develop
+    git push origin develop
+    echo "${project} succesfully updated!"
+    # Go back to root directory
+    cd $ROOT_DIR
+done
