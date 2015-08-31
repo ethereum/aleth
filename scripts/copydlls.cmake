c4f336b4bf8c6478444f8b7043d293b30b1a96f9
Vlad Gluhovsky <gluk256@gmail.com>
2015-08-31 15:39:08 +0200

    hotfix

--- scripts/copydlls.cmake
+++ scripts/copydlls.cmake
@@ -9,10 +9,16 @@
 # this script is created cause we do not know configuration in multiconfiguration generators at cmake configure phase ;)
 
 if ("${CONF}" STREQUAL "Debug")
-    set(DLL ${DLL_DEBUG})
+	execute_process(COMMAND ${CMAKE_COMMAND} -E copy "${DLL_DEBUG}" "${DESTINATION}")
+	
+	# hack, copy it twice. with and without d.dll suffix
+	# at first let's get the file name part
+	get_filename_component(DLL_DEBUG_D_NAME ${DLL_DEBUG} NAME)
+	string(REPLACE "d.dll" ".dll" DLL_DEBUG_D_NAME ${DLL_DEBUG_D_NAME})
+	string(REPLACE "_.dll" ".dll" DLL_DEBUG_D_NAME ${DLL_DEBUG_D_NAME})
+	
+	set(DESTINATION_D "${DESTINATION}/${DLL_DEBUG_D_NAME}")
+	execute_process(COMMAND ${CMAKE_COMMAND} -E copy "${DLL_DEBUG}" "${DESTINATION_D}")
 else ()
-    set(DLL ${DLL_RELEASE})
+	execute_process(COMMAND ${CMAKE_COMMAND} -E copy "${DLL_RELEASE}" "${DESTINATION}")
 endif()
-
-execute_process(COMMAND ${CMAKE_COMMAND} -E copy "${DLL}" "${DESTINATION}")
-
