43419fcfcb42eceb746ab358abd67c9efbef3a9a
CJentzsch <jentzsch.software@gmail.com>
2015-09-09 18:23:48 +0200

    add OverlayDB tests

--- test/libdevcrypto/overlaydb.cpp
+++ test/libdevcrypto/overlaydb.cpp
@@ -20,16 +20,112 @@
  * OverlayDB tests.
  */
 
-using namespace std;
-
 #include <boost/test/unit_test.hpp>
+#include <libdevcore/TransientDirectory.h>
 #include <libdevcrypto/OverlayDB.h>
 
-BOOST_AUTO_TEST_SUITE(OverlayDB)
+using namespace std;
+using namespace dev;
+
+BOOST_AUTO_TEST_SUITE(OverlayDBTests)
 
 BOOST_AUTO_TEST_CASE(basicUsage)
 {
+	ldb::Options o;
+	o.max_open_files = 256;
+	o.create_if_missing = true;
+	ldb::DB* db = nullptr;
+	TransientDirectory td;
+	ldb::Status status = ldb::DB::Open(o, td.path(), &db);
+	BOOST_REQUIRE(status.ok() && db);
 
+	OverlayDB odb(db);
+	BOOST_CHECK(!odb.get().size());
+
+	// commit nothing
+	odb.commit();
+
+	bytes value = fromHex("43");
+	BOOST_CHECK(!odb.get().size());
+
+	odb.insert(h256(42), &value);
+	BOOST_CHECK(odb.get().size());
+	BOOST_CHECK(odb.exists(h256(42)));
+	BOOST_CHECK_EQUAL(odb.lookup(h256(42)), toString(value[0]));
+
+	odb.commit();
+	BOOST_CHECK(!odb.get().size());
+	BOOST_CHECK(odb.exists(h256(42)));
+	BOOST_CHECK_EQUAL(odb.lookup(h256(42)), toString(value[0]));
+
+	odb.insert(h256(41), &value);
+	odb.commit();
+	BOOST_CHECK(!odb.get().size());
+	BOOST_CHECK(odb.exists(h256(41)));
+	BOOST_CHECK_EQUAL(odb.lookup(h256(41)), toString(value[0]));
+	BOOST_CHECK(odb.deepkill(h256(41)));
+	BOOST_CHECK(!odb.exists(h256(41)));
+	BOOST_CHECK_EQUAL(odb.lookup(h256(41)), string());
+}
+
+BOOST_AUTO_TEST_CASE(auxMem)
+{
+	ldb::Options o;
+	o.max_open_files = 256;
+	o.create_if_missing = true;
+	ldb::DB* db = nullptr;
+	TransientDirectory td;
+	ldb::Status status = ldb::DB::Open(o, td.path(), &db);
+	BOOST_REQUIRE(status.ok() && db);
+
+	OverlayDB odb(db);
+
+	bytes value = fromHex("43");
+	bytes valueAux = fromHex("44");
+
+	odb.insert(h256(42), &value);
+	odb.insert(h256(0), &value);
+	odb.insert(h256(numeric_limits<u256>::max()), &value);
+
+	odb.insertAux(h256(42), &valueAux);
+	odb.insertAux(h256(0), &valueAux);
+	odb.insertAux(h256(numeric_limits<u256>::max()), &valueAux);
+
+	odb.commit();
+
+	BOOST_CHECK(!odb.get().size());
+
+	BOOST_CHECK(odb.exists(h256(42)));
+	BOOST_CHECK_EQUAL(odb.lookup(h256(42)), toString(value[0]));
+
+	BOOST_CHECK(odb.exists(h256(0)));
+	BOOST_CHECK_EQUAL(odb.lookup(h256(0)), toString(value[0]));
+
+	BOOST_CHECK(odb.exists(h256(std::numeric_limits<u256>::max())));
+	BOOST_CHECK_EQUAL(odb.lookup(h256(std::numeric_limits<u256>::max())), toString(value[0]));
+
+	BOOST_CHECK(odb.lookupAux(h256(42)) == valueAux);
+	BOOST_CHECK(odb.lookupAux(h256(0)) == valueAux);
+	BOOST_CHECK(odb.lookupAux(h256(std::numeric_limits<u256>::max())) == valueAux);
+}
+
+BOOST_AUTO_TEST_CASE(rollback)
+{
+	ldb::Options o;
+	o.max_open_files = 256;
+	o.create_if_missing = true;
+	ldb::DB* db = nullptr;
+	TransientDirectory td;
+	ldb::Status status = ldb::DB::Open(o, td.path(), &db);
+	BOOST_REQUIRE(status.ok() && db);
+
+	OverlayDB odb(db);
+	bytes value = fromHex("42");
+
+	odb.insert(h256(43), &value);
+	BOOST_CHECK(odb.get().size());
+	odb.rollback();
+	BOOST_CHECK(!odb.get().size());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
