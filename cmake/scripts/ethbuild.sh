3c0a8e738a5ea56d82d99a775b011e4e18017bf2
Lefteris Karapetsas <lefteris@refu.co>
2015-09-09 11:11:15 +0200

    Move build scripts from old repo and update them
    - Have a special shell file to create an environment similar to what
    vsvars32.bat would create for our windows buildbot
    
    - Updated repositories list to reflect the new split repos configuration
    
    - Added an option to clone from the ssh url instead of the https
    one. HTTPS is still the default.
    
    - Just like before both scripts require you to add a project argument in
    order to know which repositories to clone/update/build and in which
    order.

--- cmake/scripts/ethbuild.sh
+++ cmake/scripts/ethbuild.sh
@@ -4,7 +4,43 @@
 # A script to build the different ethereum repositories. For usage look
 # at the string echoed in the very beginning.
 
-PROJECTS=(cpp-ethereum webthree solidity alethzero mix)
+# Get SCRIPT_DIR, the directory the script is located even if there are symlinks involved
+FILE_SOURCE="${BASH_SOURCE[0]}"
+# resolve $FILE_SOURCE until the file is no longer a symlink
+while [ -h "$FILE_SOURCE" ]; do
+	SCRIPT_DIR="$( cd -P "$( dirname "$FILE_SOURCE" )" && pwd )"
+	FILE_SOURCE="$(readlink "$FILE_SOURCE")"
+	# if $FILE_SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+	[[ $FILE_SOURCE != /* ]] && FILE_SOURCE="$SCRIPT_DIR/$FILE_SOURCE"
+done
+SCRIPT_DIR="$( cd -P "$( dirname "$FILE_SOURCE" )" && pwd )"
+
+# Now that we got the directory, source some common functionality
+source "${SCRIPT_DIR}/ethbuildcommon.sh"
+
+
+REPO_MSVC_SLN=""
+REPOS_MSVC_SLN_MAP=("cpp-ethereum:ethereum.sln"
+	"solidity:solidity.sln")
+
+function get_repo_sln() {
+	if [[ $1 == "" ]]; then
+		echo "ETHUPDATE - ERROR: get_repo_sln() function called without an argument."
+		exit 1
+	fi
+	for repo in "${REPOS_MSVC_SLN_MAP[@]}" ; do
+		KEY=${repo%%:*}
+		if [[ $KEY =~ $1 ]]; then
+			REPO_MSVC_SLN=${repo#*:}
+			break
+		fi
+	done
+	if [[ $REPO_MSVC_SLN == "" ]]; then
+		echo "ETHBUILD - ERROR: Requested MSVC Solution of unknown repo: ${1}."
+		exit 1
+	fi
+}
+
 ROOT_DIR=$(pwd)
 REQUESTED_BRANCH=develop
 CLEAN_BUILD=0
@@ -12,15 +48,18 @@ BUILD_TYPE=RelWithDebInfo
 REQUESTED_ARG=""
 EXTRA_BUILD_ARGS=""
 MAKE_CORES=1
+NOGIT=0
 
 function print_help {
 	echo "Usage: ethbuild.sh [extra-options]"
 	echo "Arguments:"
 	echo "    --help                  Print this help message."
-	echo "    --branch NAME           The branch requested to build. Default is ${REQUESTED_BRANCH}."
 	echo "    --clean-build           If given the build directories will be cleared."
+	echo "${PROJECTS_HELP}"
+	echo "    --no-git                If given no git branch check and no git checkout will be performed."
+	echo "    --branch NAME           The branch requested to build. Default is ${REQUESTED_BRANCH}."
 	echo "    --build-type BUILDTYPE  If given then this is gonna be the value of -DCMAKE_BUILD_TYPE. Default is ${BUILD_TYPE} "
-	echo "   --cores NUMBER           the value to the cores argument of make. e.g.: make -j4. Default is ${MAKE_CORES}."
+	echo "    --cores NUMBER          The value to the cores argument of make. e.g.: make -j4. Default is ${MAKE_CORES}."
 }
 
 for arg in ${@:1}
@@ -41,6 +80,9 @@ do
 			fi
 			MAKE_CORES=$arg
 			;;
+		"project")
+			set_repositories "ETHBUILD" $arg
+			;;
 		*)
 			echo "ERROR: Unrecognized argument \"$arg\".";
 			print_help
@@ -60,13 +102,23 @@ do
 		continue
 	fi
 
+	if [[ $arg == "--no-git" ]]; then
+		NOGIT=1
+		continue
+	fi
+
+	if [[ $arg == "--project" ]]; then
+		REQUESTED_ARG="project"
+		continue
+	fi
+
 	if [[ $arg == "--build-type" ]]; then
 		REQUESTED_ARG="build-type"
 		continue
 	fi
 
 	if [[ $arg == "--branch" ]]; then
-		REQUESTED_ARG="build-type"
+		REQUESTED_ARG="branch"
 		continue
 	fi
 
@@ -85,24 +137,27 @@ if [[ ${REQUESTED_ARG} != "" ]]; then
 	exit 1
 fi
 
-for project in "${PROJECTS[@]}"
+for repository in "${BUILD_REPOSITORIES[@]}"
 do
-	cd $project >/dev/null 2>/dev/null
+	cd $repository >/dev/null 2>/dev/null
 	if [[ $? -ne 0 ]]; then
-		echo "Skipping ${project} because directory does not exit";
+		echo "Skipping ${repository} because directory does not exit";
 		# Go back to root directory
 		cd $ROOT_DIR
 		continue
 	fi
-	BRANCH="$(git symbolic-ref HEAD 2>/dev/null)" ||
-	BRANCH="(unnamed branch)"	  # detached HEAD
-	BRANCH=${BRANCH##refs/heads/}
 
-	if [[ $BRANCH != $REQUESTED_BRANCH ]]; then
-		echo "BUILD WARNING: ${project} was in ${BRANCH} branch, while building on ${REQUESTED_BRANCH} was requested.";
+	if [[ $NOGIT -eq 0 ]]; then
+		BRANCH="$(git symbolic-ref HEAD 2>/dev/null)" ||
+		BRANCH="(unnamed branch)"	  # detached HEAD
+		BRANCH=${BRANCH##refs/heads/}
+	fi
+
+	if [[ $NOGIT -eq 0 && $BRANCH != $REQUESTED_BRANCH ]]; then
+		echo "BUILD WARNING: ${repository} was in ${BRANCH} branch, while building on ${REQUESTED_BRANCH} was requested.";
 		git checkout $REQUESTED_BRANCH
 		if [[ $? -ne 0 ]]; then
-			echo "ERROR: Checking out branch ${REQUESTED_BRANCH} for ${project} failed";
+			echo "ERROR: Checking out branch ${REQUESTED_BRANCH} for ${repository} failed";
 			exit 1
 		fi
 	fi
@@ -117,19 +172,35 @@ do
 		mkdir build
 	fi
 
-	# Go in build directory for the project and configure
+	# Go in build directory for the repository and configure/build
 	cd build
-	cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE $EXTRA_BUILD_ARGS;
-	if [[ $? -ne 0 ]]; then
-		echo "ERROR: cmake configure phase for project \"$project\" failed.";
-		exit 1
-	fi
+	if [[ $OSTYPE == "cygwin" ]]; then
+		# For Windows we gotta emulate the Visual studio environment
+		source "${SCRIPT_DIR}/ethwindowsenv.sh"
+		cmake .. -G "Visual Studio 12 2013 Win64"
+		if [[ $? -ne 0 ]]; then
+			echo "ERROR: cmake configure phase for repository \"$repository\" failed.";
+			exit 1
+		fi
 
-	# Build the project
-	make -j${MAKE_CORES}
-	if [[ $? -ne 0 ]]; then
-		echo "ERROR: Building project \"$project\" failed.";
-		exit 1
+		get_repo_sln $repository
+		"${MSBUILD_EXECUTABLE}" $REPO_MSVC_SLN /p:Configuration=$BUILD_TYPE /m:${MAKE_CORES}
+		if [[ $? -ne 0 ]]; then
+			echo "ERROR: Building repository \"$repository\" failed.";
+			exit 1
+		fi
+	else
+		cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE $EXTRA_BUILD_ARGS;
+		if [[ $? -ne 0 ]]; then
+			echo "ERROR: cmake configure phase for repository \"$repository\" failed.";
+			exit 1
+		fi
+
+		make -j${MAKE_CORES}
+		if [[ $? -ne 0 ]]; then
+			echo "ERROR: Building repository \"$repository\" failed.";
+			exit 1
+		fi
 	fi
 	# Go back to root directory
 	cd $ROOT_DIR
--- cmake/scripts/ethupdate.sh
+++ cmake/scripts/ethupdate.sh
@@ -5,20 +5,81 @@
 # Invoke from the root directory and make sure you have the arguments set as explained
 # in the usage string.
 
+
+# Get SCRIPT_DIR, the directory the script is located even if there are symlinks involved
+FILE_SOURCE="${BASH_SOURCE[0]}"
+# resolve $FILE_SOURCE until the file is no longer a symlink
+while [ -h "$FILE_SOURCE" ]; do
+	SCRIPT_DIR="$( cd -P "$( dirname "$FILE_SOURCE" )" && pwd )"
+	FILE_SOURCE="$(readlink "$FILE_SOURCE")"
+	# if $FILE_SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+	[[ $FILE_SOURCE != /* ]] && FILE_SOURCE="$SCRIPT_DIR/$FILE_SOURCE"
+done
+SCRIPT_DIR="$( cd -P "$( dirname "$FILE_SOURCE" )" && pwd )"
+
+# Now that we got the directory, source some common functionality
+source "${SCRIPT_DIR}/ethbuildcommon.sh"
+
 ROOT_DIR=$(pwd)
-PROJECTS=(cpp-ethereum cpp-ethereum-cmake webthree solidity alethzero mix)
+NO_PUSH=0
+USE_SSH=0
 UPSTREAM=upstream
 ORIGIN=origin
 REQUESTED_BRANCH=develop
 REQUESTED_ARG=""
+REQUESTED_PROJECT=""
+REPO_URL=""
+REPOS_MAP=("webthree-helpers:https://github.com/ethereum/webthree-helpers"
+	   "tests:https://github.com/ethereum/tests"
+	   "libweb3core:https://github.com/ethereum/libweb3core"
+	   "libethereum:https://github.com/ethereum/libethereum"
+	   "libwhisper:https://github.com/ethereum/libwhisper"
+	   "webthree:https://github.com/ethereum/webthree"
+	   "solidity:https://github.com/ethereum/solidity"
+	   "alethzero:https://github.com/ethereum/alethzero"
+	   "mix:https://github.com/ethereum/mix")
+REPOS_SSH_MAP=("webthree-helpers:git@github.com:ethereum/webthree-helpers.git"
+	   "tests:git@github.com:ethereum/tests.git"
+	   "libweb3core:git@github.com:ethereum/libweb3core.git"
+	   "libethereum:git@github.com:ethereum/libethereum.git"
+	   "libwhisper:git@github.com:ethereum/libwhisper.git"
+	   "webthree:git@github.com:ethereum/webthree.git"
+	   "solidity:git@github.com:ethereum/solidity.git"
+	   "alethzero:git@github.com:ethereum/alethzero.git"
+	   "mix:git@github.com:ethereum/mix.git")
+
+function get_repo_url() {
+	if [[ $1 == "" ]]; then
+		echo "ETHUPDATE - ERROR: get_repo_url() function called without an argument."
+		exit 1
+	fi
+	REPOS_MAP_VAR="${REPOS_MAP[@]}"
+	if [[ $USE_SSH -eq 1 ]]; then
+		REPOS_MAP_VAR="${REPOS_SSH_MAP[@]}"
+	fi
+	for repo in $REPOS_MAP_VAR ; do
+		KEY=${repo%%:*}
+		if [[ $KEY =~ $1 ]]; then
+			REPO_URL=${repo#*:}
+			break
+		fi
+	done
+	if [[ $REPO_URL == "" ]]; then
+		echo "ETHUPDATE - ERROR: Requested url of unknown repo: ${1}."
+		exit 1
+	fi
+}
 
 function print_help {
 	echo "Usage: ethupdate.sh [options]"
 	echo "Arguments:"
 	echo "    --help                    Will print this help message."
+	echo "${PROJECTS_HELP}"
 	echo "    --branch NAME             Will update to the specified branch. Default is ${REQUESTED_BRANCH}."
 	echo "    --origin NAME             Will send the updates back to origin NAME if specified."
 	echo "    --upstream NAME           The name of the remote to pull from. Default is ${UPSTREAM}."
+	echo "    --no-push                 Don't push anything back to origin."
+	echo "    --use-ssh                 Use ssh to clone the repos instead of https."
 }
 
 for arg in ${@:1}
@@ -34,8 +95,11 @@ do
 			"branch")
 				REQUESTED_BRANCH=$arg
 				;;
+			"project")
+				set_repositories "ETHUPDATE" $arg
+				;;
 			*)
-				echo "ERROR: Unrecognized argument \"$arg\".";
+				echo "ETHUPDATE - ERROR: Unrecognized argument \"$arg\".";
 				print_help
 				exit 1
 		esac
@@ -53,6 +117,11 @@ do
 		continue
 	fi
 
+	if [[ $arg == "--project" ]]; then
+		REQUESTED_ARG="project"
+		continue
+	fi
+
 	if [[ $arg == "--origin" ]]; then
 		REQUESTED_ARG="origin"
 		continue
@@ -63,49 +132,77 @@ do
 		continue
 	fi
 
-	echo "ERROR: Unrecognized argument \"$arg\".";
+	if [[ $arg == "--no-push" ]]; then
+		NO_PUSH=1
+		continue
+	fi
+
+	if [[ $arg == "--use-ssh" ]]; then
+		USE_SSH=1
+		continue
+	fi
+
+	echo "ETHUPDATE - ERROR: Unrecognized argument \"$arg\".";
 	print_help
 	exit 1
 done
 
 if [[ ${REQUESTED_ARG} != "" ]]; then
-	echo "ERROR: Expected value for the \"${REQUESTED_ARG}\" argument";
+	echo "ETHUPDATE - ERROR: Expected value for the \"${REQUESTED_ARG}\" argument";
 	exit 1
 fi
 
-for project in "${PROJECTS[@]}"
+for repository in "${CLONE_REPOSITORIES[@]}"
 do
-	cd $project >/dev/null 2>/dev/null
+	CLONED_THE_REPO=0
+	cd $repository >/dev/null 2>/dev/null
 	if [[ $? -ne 0 ]]; then
-		echo "Skipping ${project} because directory does not exit";
-		cd $ROOT_DIR
-		continue
+		if [[ $REQUESTED_PROJECT == "" ]]; then
+			echo "ETHUPDATE - INFO: Skipping ${repository} because directory does not exit";
+			cd $ROOT_DIR
+			continue
+		else
+			echo "ETHUPDATE - INFO: Repository ${repository} for requested project ${REQUESTED_PROJECT} did not exist. Cloning ..."
+			get_repo_url $repository
+			git clone $REPO_URL
+			CLONED_THE_REPO=1
+			cd $repository >/dev/null 2>/dev/null
+		fi
 	fi
 	BRANCH="$(git symbolic-ref HEAD 2>/dev/null)" ||
 		BRANCH="(unnamed branch)"     # detached HEAD
 	BRANCH=${BRANCH##refs/heads/}
 	if [[ $BRANCH != $REQUESTED_BRANCH ]]; then
-		echo "WARNING: Not updating ${project} because it's not in the ${REQUESTED_BRANCH} branch"
+		echo "ETHUPDATE - WARNING: Not updating ${repository} because it's not in the ${REQUESTED_BRANCH} branch"
 		cd $ROOT_DIR
 		continue
 	fi
 
-	# Pull changes from what the user set as the upstream repository
-	git pull $UPSTREAM $REQUESTED_BRANCH
+	# Pull changes from what the user set as the upstream repository, unless it's just been cloned
+	if [[ $CLONED_THE_REPO -eq 0 ]]; then
+		git pull $UPSTREAM $REQUESTED_BRANCH
+	else
+		# if just cloned, make a local branch tracking the origin's requested branch
+		git fetch origin
+		if [[ $BRANCH != $REQUESTED_BRANCH ]]; then
+			git checkout --track -b $REQUESTED_BRANCH origin/$REQUESTED_BRANCH
+		fi
+	fi
+
 	if [[ $? -ne 0 ]]; then
-		echo "ERROR: Pulling changes for project ${project} from ${UPSTREAM} into the ${REQUESTED_BRANCH} branch failed."
+		echo "ETHUPDATE - ERROR: Pulling changes for repository ${repository} from ${UPSTREAM} into the ${REQUESTED_BRANCH} branch failed."
 		cd $ROOT_DIR
 		continue
 	fi
-	# If upstream and origin are not the same, push the changes back to origin
-	if [[ $UPSTREAM != $ORIGIN ]]; then
+	# If upstream and origin are not the same, push the changes back to origin and no push has not been asked
+	if [[ $NO_PUSH -eq 0 && $UPSTREAM != $ORIGIN ]]; then
 		git push $ORIGIN $REQUESTED_BRANCH
 		if [[ $? -ne 0 ]]; then
-			echo "ERROR: Could not update origin ${ORIGIN} of project ${project} for the ${REQUESTED_BRANCH}."
+			echo "ETHUPDATE - ERROR: Could not update origin ${ORIGIN} of repository ${repository} for the ${REQUESTED_BRANCH}."
 			cd $ROOT_DIR
 			continue
 		fi
 	fi
-	echo "${project} succesfully updated!"
+	echo "ETHUPDATE - INFO: ${repository} succesfully updated!"
 	cd $ROOT_DIR
 done
