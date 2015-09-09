d441b71a57661da85a6a0d2a3cd634d87f496b4e
CJentzsch <jentzsch.software@gmail.com>
2015-09-09 09:34:09 +0200

    complete MemoryDB tests

--- test/libdevcore/memorydb.cpp
+++ test/libdevcore/memorydb.cpp
@@ -21,6 +21,7 @@
  */
 
 #include <boost/test/unit_test.hpp>
+#include <iostream>
 #include <libdevcore/MemoryDB.h>
 
 using namespace std;
@@ -52,10 +53,73 @@ BOOST_AUTO_TEST_CASE(purgeMainMem)
 	bytes value = fromHex("43");
 
 	myDB.insert(h256(42), &value);
+	MemoryDB copy;
+	copy = myDB;
 	BOOST_CHECK(myDB.exists(h256(42)));
 	BOOST_CHECK_EQUAL(myDB.lookup(h256(42)), toString(value[0]));
 	BOOST_CHECK_EQUAL(myDB.get().size(), 1);
 	BOOST_CHECK(myDB.kill(h256(42)));
+
+	BOOST_CHECK(myDB.get() == copy.get());
+	BOOST_CHECK(myDB.exists(h256(42)));
+	BOOST_CHECK_EQUAL(myDB.lookup(h256(42)), toString(value[0]));
+
+	BOOST_CHECK_EQUAL(myDB.get().size(), 1);
+	myDB.purge();
+	BOOST_CHECK_EQUAL(myDB.get().size(), 0);
+	myDB.insert(h256(43), &value);
+	BOOST_CHECK_EQUAL(myDB.get().size(), 1);
+	myDB.clear();
+	BOOST_CHECK_EQUAL(myDB.get().size(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(purgeMainMem_Refs)
+{
+	MemoryDB myDB;
+	{
+		EnforceRefs enforceRefs(myDB, true);
+
+		BOOST_CHECK(myDB.get().empty());
+		bytes value = fromHex("43");
+
+		myDB.insert(h256(42), &value);
+		MemoryDB copy;
+		copy = myDB;
+		BOOST_CHECK(myDB.exists(h256(42)));
+		BOOST_CHECK_EQUAL(myDB.lookup(h256(42)), toString(value[0]));
+		BOOST_CHECK_EQUAL(myDB.get().size(), 1);
+		BOOST_CHECK(myDB.kill(h256(42)));
+
+		BOOST_CHECK(myDB.get() != copy.get());
+		BOOST_CHECK(!myDB.exists(h256(42)));
+		BOOST_CHECK_EQUAL(myDB.lookup(h256(42)), std::string());
+
+		BOOST_CHECK_EQUAL(myDB.get().size(), 0);
+		myDB.purge();
+		BOOST_CHECK_EQUAL(myDB.get().size(), 0);
+		myDB.insert(h256(43), &value);
+		BOOST_CHECK_EQUAL(myDB.get().size(), 1);
+		myDB.clear();
+		BOOST_CHECK_EQUAL(myDB.get().size(), 0);
+		// call EnforceRefs destructor
+	}
+
+	// do same tests again without EnforceRefs
+	BOOST_CHECK(myDB.get().empty());
+	bytes value = fromHex("43");
+
+	myDB.insert(h256(42), &value);
+	MemoryDB copy;
+	copy = myDB;
+	BOOST_CHECK(myDB.exists(h256(42)));
+	BOOST_CHECK_EQUAL(myDB.lookup(h256(42)), toString(value[0]));
+	BOOST_CHECK_EQUAL(myDB.get().size(), 1);
+	BOOST_CHECK(myDB.kill(h256(42)));
+
+	BOOST_CHECK(myDB.get() == copy.get());
+	BOOST_CHECK(myDB.exists(h256(42)));
+	BOOST_CHECK_EQUAL(myDB.lookup(h256(42)), toString(value[0]));
+
 	BOOST_CHECK_EQUAL(myDB.get().size(), 1);
 	myDB.purge();
 	BOOST_CHECK_EQUAL(myDB.get().size(), 0);
@@ -90,6 +154,59 @@ BOOST_AUTO_TEST_CASE(purgeAuxMem)
 	BOOST_CHECK_EQUAL(myDB.getAux().size(), 1);
 	myDB.clear();
 	BOOST_CHECK_EQUAL(myDB.getAux().size(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(copy)
+{
+	MemoryDB myDB;
+	BOOST_CHECK(myDB.get().empty());
+	bytes value = fromHex("43");
+	myDB.insert(h256(42), &value);
+	BOOST_CHECK(myDB.exists(h256(42)));
+	BOOST_CHECK_EQUAL(myDB.get().size(), 1);
+
+	MemoryDB copyToDB;
+	copyToDB = myDB;
+	BOOST_CHECK(copyToDB.exists(h256(42)));
+	BOOST_CHECK_EQUAL(copyToDB.get().size(), 1);
+	BOOST_CHECK(myDB.keys() == copyToDB.keys());
+	BOOST_CHECK(myDB.get() == copyToDB.get());
+	myDB.insert(h256(43), &value);
+	BOOST_CHECK(myDB.keys() != copyToDB.keys());
+}
+
+BOOST_AUTO_TEST_CASE(lookUp)
+{
+	MemoryDB myDB;
+	BOOST_CHECK(myDB.get().empty());
+	bytes value = fromHex("43");
+
+	myDB.insert(h256(42), &value);
+	BOOST_CHECK(myDB.exists(h256(42)));
+	BOOST_CHECK_EQUAL(myDB.lookup(h256(42)), toString(value[0]));
+	BOOST_CHECK_EQUAL(myDB.get().size(), 1);
+
+	myDB.insert(h256(0), &value);
+	BOOST_CHECK(myDB.exists(h256(0)));
+	BOOST_CHECK_EQUAL(myDB.lookup(h256(0)), toString(value[0]));
+
+	myDB.insert(h256(std::numeric_limits<u256>::max()), &value);
+	BOOST_CHECK(myDB.exists(h256(std::numeric_limits<u256>::max())));
+	BOOST_CHECK_EQUAL(myDB.lookup(h256(std::numeric_limits<u256>::max())), toString(value[0]));
+
+	BOOST_CHECK_EQUAL(myDB.get().size(), 3);
+}
+
+BOOST_AUTO_TEST_CASE(stream)
+{
+	MemoryDB myDB;
+	BOOST_CHECK(myDB.get().empty());
+	bytes value = fromHex("43");
+	myDB.insert(h256(42), &value);
+	myDB.insert(h256(43), &value);
+	std::ostringstream stream;
+	stream << myDB;
+	BOOST_CHECK_EQUAL(stream.str(), "000000000000000000000000000000000000000000000000000000000000002a: 0x43 43\n000000000000000000000000000000000000000000000000000000000000002b: 0x43 43\n");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
