e4f0740405bc9732409d4d8da583efaef4d907b4
arkpar <arkady.paronyan@gmail.com>
2015-09-11 18:15:31 +0200

    fixed qrc generation

--- cmake/scripts/genqrc.cmake
+++ cmake/scripts/genqrc.cmake
@@ -3,7 +3,7 @@
 # cmake -DETH_RES_FILES=a.png;b.js -DETH_QRC_DEST=resources.qrc -P genqrc.cmake
 #
 
-file(APPEND ${ETH_QRC_DEST} "<RCC>\n<qresource prefix=\"/\">\n")
+file(WRITE ${ETH_QRC_DEST} "<RCC>\n<qresource prefix=\"/\">\n")
 string(REPLACE " " ";" RES_LIST ${ETH_RES_FILES})
 foreach(resource ${RES_LIST})
 	get_filename_component(fname ${resource} NAME)
