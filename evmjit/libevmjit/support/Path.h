54899d8e663d2e639ffd8856e5729c84716c0ec4
Paweł Bylica <chfast@gmail.com>
2015-09-16 21:32:07 +0200

    Get home/cache paths on Windows.

--- evmjit/libevmjit/support/Path.h
+++ evmjit/libevmjit/support/Path.h
@@ -3,34 +3,34 @@
 #include <string>
 
 #if __linux
-  #define UTILS_OS_LINUX 1
+	#define UTILS_OS_LINUX 1
 #else
-  #define UTILS_OS_LINUX 0
+	#define UTILS_OS_LINUX 0
 #endif
 
 #if __APPLE__
-  #define UTILS_OS_MAC 1
+	#define UTILS_OS_MAC 1
 #else
-  #define UTILS_OS_MAC 0
+	#define UTILS_OS_MAC 0
 #endif
 
 #if UTILS_OS_LINUX
-  #define UTILS_OS_POSIX 1
+	#define UTILS_OS_POSIX 1
 #else
-  #define UTILS_OS_POSIX 0
+	#define UTILS_OS_POSIX 0
 #endif
 
 #if _WIN32
-  #define UTILS_OS_WINDOWS 1
+	#define UTILS_OS_WINDOWS 1
 #else
-  #define UTILS_OS_WINDOWS 0
+	#define UTILS_OS_WINDOWS 0
 #endif
 
 namespace dev
 {
 namespace path
 {
-  std::string user_home_directory();
-  std::string user_cache_directory();
+	std::string user_home_directory();
+	std::string user_cache_directory();
 }
 }
