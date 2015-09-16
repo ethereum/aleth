1a6765b295bb2922e798e5d1185a3b417a270488
CJentzsch <jentzsch.software@gmail.com>
2015-09-16 09:00:39 +0200

    clear aux memory in memoryDB

--- libdevcore/MemoryDB.h
+++ libdevcore/MemoryDB.h
@@ -48,7 +48,7 @@ public:
 
 	MemoryDB& operator=(MemoryDB const& _c);
 
-	void clear() { m_main.clear(); }	// WARNING !!!! didn't originally clear m_refCount!!!
+	void clear() { m_main.clear(); m_aux.clear(); }	// WARNING !!!! didn't originally clear m_refCount!!!
 	std::unordered_map<h256, std::string> get() const;
 
 	std::string lookup(h256 const& _h) const;
