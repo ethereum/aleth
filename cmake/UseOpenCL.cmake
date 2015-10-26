02fa4668d57f3f5ea7d956afe15406608be414fb
arkpar <arkady.paronyan@gmail.com>
2015-10-26 11:54:59 +0100

    force opencl loading into executables

--- cmake/UseOpenCL.cmake
+++ cmake/UseOpenCL.cmake
@@ -3,7 +3,13 @@ function(eth_apply TARGET REQUIRED)
 	eth_show_dependency(OpenCL OpenCL)
 	if (OpenCL_FOUND)
 		target_include_directories(${TARGET} SYSTEM PUBLIC ${OpenCL_INCLUDE_DIRS})
-		target_link_libraries(${TARGET} ${OpenCL_LIBRARIES})
+		if ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux" AND ETHASHCL)
+			# workaround for https://github.com/ethereum/alethzero/issues/69
+			# force linking to libOpenCL as early as possible
+			target_link_libraries(${TARGET} "-Wl,--no-as-needed -l${OpenCL_LIBRARIES} -Wl,--as-needed")
+		else()
+			target_link_libraries(${TARGET} ${OpenCL_LIBRARIES})
+		endif()
 		eth_copy_dlls(${TARGET} OpenCL_DLLS)
 		# as per this comment: http://cmake.3232098.n2.nabble.com/Scope-of-find-package-inside-a-function-block-tp6821980p6822888.html
 		# It seems that calling find_package() inside a cmake function will only set the found
