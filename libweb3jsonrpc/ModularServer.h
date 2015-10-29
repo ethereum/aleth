c7b34977d89edb9b3225eb9a0c464221244ef22f
debris <marek.kotewicz@gmail.com>
2015-10-29 20:18:40 +0800

    removed blank lines

--- libweb3jsonrpc/ModularServer.h
+++ libweb3jsonrpc/ModularServer.h
@@ -41,19 +41,19 @@ class ServerInterface
 public:
 	using MethodPointer = AbstractMethodPointer<I>;
 	using NotificationPointer = AbstractNotificationPointer<I>;
-	
+
 	using MethodBinding = std::tuple<jsonrpc::Procedure, AbstractMethodPointer<I>>;
 	using NotificationBinding = std::tuple<jsonrpc::Procedure, AbstractNotificationPointer<I>>;
 	using Methods = std::vector<MethodBinding>;
 	using Notifications = std::vector<NotificationBinding>;
-	
+
 	Methods const& methods() const { return m_methods; }
 	Notifications const& notifications() const { return m_notifications; }
-	
+
 protected:
 	void bindAndAddMethod(jsonrpc::Procedure const& _proc, MethodPointer _pointer) { m_methods.emplace_back(_proc, _pointer); }
 	void bindAndAddNotification(jsonrpc::Procedure const& _proc, NotificationPointer _pointer) { m_notifications.emplace_back(_proc, _pointer); }
-	
+
 private:
 	Methods m_methods;
 	Notifications m_notifications;
@@ -65,34 +65,34 @@ class ModularServer: public jsonrpc::IProcedureInvokationHandler
 public:
 	ModularServer()
 	: m_handler(jsonrpc::RequestHandlerFactory::createProtocolHandler(jsonrpc::JSONRPC_SERVER_V2, *this)) {}
-	
+
 	virtual ~ModularServer() { StopListening(); }
-	
+
 	virtual void StartListening()
 	{
 		for (auto const& connector: m_connectors)
 			connector->StartListening();
 	}
-	
+
 	virtual void StopListening()
 	{
 		for (auto const& connector: m_connectors)
 			connector->StopListening();
 	}
-	
+
 	virtual void HandleMethodCall(jsonrpc::Procedure& _proc, Json::Value const& _input, Json::Value& _output) override
 	{
 		(void)_proc;
 		(void)_input;
 		(void)_output;
 	}
-	
+
 	virtual void HandleNotificationCall(jsonrpc::Procedure& _proc, Json::Value const& _input) override
 	{
 		(void)_proc;
 		(void)_input;
 	}
-	
+
 	/// server takes ownership of the connector
 	unsigned addConnector(jsonrpc::AbstractServerConnector* _connector)
 	{
@@ -100,12 +100,12 @@ public:
 		_connector->SetHandler(m_handler.get());
 		return m_connectors.size() - 1;
 	}
-	
+
 	jsonrpc::AbstractServerConnector* connector(unsigned _i) const
 	{
 		return m_connectors.at(_i).get();
 	}
-	
+
 protected:
 	std::vector<std::unique_ptr<jsonrpc::AbstractServerConnector>> m_connectors;
 	std::unique_ptr<jsonrpc::IProtocolHandler> m_handler;
@@ -117,7 +117,7 @@ class ModularServer<I, Is...> : public ModularServer<Is...>
 public:
 	using MethodPointer = AbstractMethodPointer<I>;
 	using NotificationPointer = AbstractNotificationPointer<I>;
-	
+
 	ModularServer<I, Is...>(I* _i, Is*... _is): ModularServer<Is...>(_is...), m_interface(_i)
 	{
 		for (auto const& method: m_interface->methods())
@@ -125,14 +125,14 @@ public:
 			m_methods[std::get<0>(method).GetProcedureName()] = std::get<1>(method);
 			this->m_handler->AddProcedure(std::get<0>(method));
 		}
-		
+
 		for (auto const& notification: m_interface->notifications())
 		{
 			m_notifications[std::get<0>(notification).GetProcedureName()] = std::get<1>(notification);
 			this->m_handler->AddProcedure(std::get<0>(notification));
 		}
 	}
-	
+
 	virtual void HandleMethodCall(jsonrpc::Procedure& _proc, Json::Value const& _input, Json::Value& _output) override
 	{
 		auto pointer = m_methods.find(_proc.GetProcedureName());
@@ -141,7 +141,7 @@ public:
 		else
 			ModularServer<Is...>::HandleMethodCall(_proc, _input, _output);
 	}
-	
+
 	virtual void HandleNotificationCall(jsonrpc::Procedure& _proc, Json::Value const& _input) override
 	{
 		auto pointer = m_notifications.find(_proc.GetProcedureName());
@@ -150,7 +150,7 @@ public:
 		else
 			ModularServer<Is...>::HandleNotificationCall(_proc, _input);
 	}
-	
+
 private:
 	std::unique_ptr<I> m_interface;
 	std::map<std::string, MethodPointer> m_methods;
