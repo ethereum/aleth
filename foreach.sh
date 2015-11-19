f69014c44f94eb7c2a815be095accc73a6b597ac
Gav Wood <i@gavwood.com>
2015-11-19 12:29:21 +0100

    updated submodules

--- foreach.sh
+++ foreach.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-for i in libweb3core libethereum webthree alethzero; do
+branch=$(git branch | grep "\*" | cut -c 3-)
+for i in webthree-helpers libweb3core libethereum web3.js solidity webthree alethzero mix; do
 cd $i
+lbranch=$(git branch | grep "\*" | cut -c 3-)
+if [ "$lbranch" == "$branch" ] ; then
+echo Entering $i ...
 "$@"
+else
+echo "Skipping $i (branch $lbranch is not $branch)".
+fi
 cd ..
 done
--- nameeach.sh
+++ nameeach.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-for i in libweb3core libethereum webthree alethzero; do
+branch=$(git branch | grep "\*" | cut -c 3-)
+for i in webthree-helpers libweb3core libethereum web3.js solidity webthree alethzero mix; do
+cd $i
+lbranch=$(git branch | grep "\*" | cut -c 3-)
+cd ..
+if [ "$lbranch" == "$branch" ] ; then
+echo Entering $i ...
 "$@" "$i"
+else
+echo "Skipping $i (branch $lbranch is not $branch)".
+fi
 done
