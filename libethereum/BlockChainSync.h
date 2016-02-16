c7ab96031ab8539810abb2942f2d199b1e8df85a
Bob Summerwill <bob@summerwill.net>
2016-02-15 22:36:31 -0800

    Removed unused private member variable, which was used in PV60. This was causing a build warning on OS X.

--- libethereum/BlockChainSync.h
+++ libethereum/BlockChainSync.h
@@ -136,7 +136,6 @@ private:
 	Handler<> m_bqRoomAvailable;				///< Triggered once block queue has space for more blocks
 	mutable RecursiveMutex x_sync;
 	SyncState m_state = SyncState::NotSynced;	///< Current sync state
-	unsigned m_estimatedHashes = 0;				///< Number of estimated hashes for the last peer over PV60. Used for status reporting only.
 	h256Hash m_knownNewHashes; 					///< New hashes we know about use for logging only
 	unsigned m_startingBlock = 0;      	    	///< Last block number for the start of sync
 	unsigned m_highestBlock = 0;       	     	///< Highest block number seen
