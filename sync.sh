51cfe612a65792bacf5f3269eb25dc200f250708
chriseth <c@ethdev.com>
2016-04-06 14:58:44 +0200

    updated submodules

--- sync.sh
+++ sync.sh
@@ -6,8 +6,10 @@ git submodule sync
 git submodule update --init --remote .
 cd build
 make -j 4
-cd ..
-./webthree-helpers/scripts/ethtests.sh libethereum --umbrella
+# Only update if tests do not fail (disabled for now)
+#cd ..
+#./webthree-helpers/scripts/ethtests.sh all --umbrella
+git checkout -B develop
 git commit -am 'updated submodules'   
 git push
 
