cd733b6754f2b1615dbd4d9c2f5c64242bf9e2f1
chriseth <c@ethdev.com>
2016-04-06 15:06:13 +0200

    Fix sync script.

--- sync.sh
+++ sync.sh
@@ -6,8 +6,8 @@ git submodule sync
 git submodule update --init --remote .
 cd build
 make -j 4
+cd ..
 # Only update if tests do not fail (disabled for now)
-#cd ..
 #./webthree-helpers/scripts/ethtests.sh all --umbrella
 git checkout -B develop
 git commit -am 'updated submodules'   
