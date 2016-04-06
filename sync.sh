dd004a7da6905d83cfdf59a088056c842633894c
chriseth <c@ethdev.com>
2016-04-06 18:16:32 +0200

    Changed back to "push" as this might work now.

--- sync.sh
+++ sync.sh
@@ -13,9 +13,5 @@ cd ..
 #./webthree-helpers/scripts/ethtests.sh all --umbrella
 git checkout -B develop
 git commit -am 'updated submodules'   
-echo "You can now push the commit."
-# We do not push as part of the script because
-# it complicates user access issues. On Jenkins,
-# this is done by a post-build publish step.
-#git push
+git push
 
