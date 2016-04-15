52cc33025dcbdb793ad26412753d9f3848d51f97
Greg Colvin <greg@colvin.org>
2016-04-15 01:06:18 -0600

    programs for timing interpreter performance

---
+++ times/loop.sol
@@ -0,0 +1,7 @@
+contract loop {
+
+    function loop(){
+        for (int i = 0; i < 100000; ++1) {
+        }
+    }
+}
---
+++ times/rng.sh
@@ -0,0 +1,5 @@
+time ~/webthree-umbrella/build/libethereum/ethvm/ethvm --gas-limit 100000000 --gas 10000000 --vm interpreter rng.bin
+
+time ~/webthree-umbrella/build/libethereum/ethvm/ethvm --gas-limit 100000000 --gas 10000000 --vm interpreter output rng.bin
+
+time ~/webthree-umbrella/build/libethereum/ethvm/ethvm --gas-limit 100000000 --gas 10000000 --vm jit output rng.bin
