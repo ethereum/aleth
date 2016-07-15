892397abd8b219ac8c5e889a1f62f91dc6dbfbee
Bob Summerwill <bob@summerwill.net>
2016-07-14 22:33:06 -0700

    Updated to recreate-cpp-ethereum.sh to reflect the loose-file cleanup in the various sub-modules. Getting exceedingly close to done with this script, and then will be on to the history-preserving version of the same mappings.

--- recreate-cpp-ethereum.sh
+++ recreate-cpp-ethereum.sh
@@ -15,9 +15,7 @@ mkdir -p  $outputDirectory/test/libethereum/
 mkdir -p  $outputDirectory/test/libweb3core/
 mkdir -p  $outputDirectory/test/webthree/
 
-rsync -r ./dependency_graph/              $outputDirectory/dependency_graph/
 rsync -r ./doc/                           $outputDirectory/doc/
-rsync -r ./docker/                        $outputDirectory/docker/
 rsync -r ./libethereum/ethkey/            $outputDirectory/ethkey/
 rsync -r ./libethereum/ethminer/          $outputDirectory/ethminer/
 rsync -r ./libethereum/ethvm/             $outputDirectory/ethvm/
@@ -29,18 +27,21 @@ rsync -r ./libethereum/libethereum/       $outputDirectory/libethereum/
 rsync -r ./libethereum/libevm/            $outputDirectory/libevm/
 rsync -r ./libethereum/libevmcore/        $outputDirectory/libevmcore/
 rsync -r ./libethereum/test/              $outputDirectory/test/libethereum/test/
+# intentionally left /libethereum root files behind: (CMakeLists.txt, fetch_umbrella_build_and_test.sh, install_dependencies.sh, LICENSE, README.md)
 rsync -r ./libweb3core/bench/             $outputDirectory/bench/
 rsync -r ./libweb3core/libdevcore/        $outputDirectory/libdevcore/
 rsync -r ./libweb3core/libdevcrypto/      $outputDirectory/libdevcrypto/
 rsync -r ./libweb3core/libp2p/            $outputDirectory/libp2p/
 rsync -r ./libweb3core/rlp/               $outputDirectory/rlp/
 rsync -r ./libweb3core/test/              $outputDirectory/test/libweb3core/test/
+# intentionally left /libweb3core root files behind: (CMakeLists.txt, LICENSE, README.md)
 # solidity intentionally omitted
 rsync -r ./webthree/eth/                  $outputDirectory/eth/
 rsync -r ./webthree/libweb3jsonrpc/       $outputDirectory/libweb3jsonrpc/
 rsync -r ./webthree/libwebthree/          $outputDirectory/libwebthree/
 rsync -r ./webthree/libwhisper/           $outputDirectory/libwhisper/
 rsync -r ./webthree/test/                 $outputDirectory/test/webthree/test/
+# intentionally left /webthree root files behind: (CMakeLists.txt, LICENSE, README.md)
 rsync -r ./webthree-helpers/cmake/        $outputDirectory/cmake/
 rsync -r ./webthree-helpers/extdep/       $outputDirectory/extdep/
 rsync -r ./webthree-helpers/homebrew/     $outputDirectory/homebrew/
@@ -48,10 +49,7 @@ rsync -r ./webthree-helpers/js/           $outputDirectory/js/
 rsync -r ./webthree-helpers/scripts/      $outputDirectory/scripts/
 rsync -r ./webthree-helpers/templates/    $outputDirectory/templates/
 rsync -r ./webthree-helpers/utils/        $outputDirectory/utils/
-# intentionally left /webthree-helpers root files behind: (LICENSE, new.sh, README.md)
-# TODO /webthree-helpers/cmake has (LICENSE, README.md), but why?
-# TODO /webthree-helpers/homebrew has (LICENSE, README.md), but why?
-# Tried unsuccessfully to delete homebrew ones.   Needed in some release flow?
+# intentionally left /webthree-helpers root files behind: (LICENSE, README.md)
 
 # Loose files in the root directory of webthree-umbrella.
 # TODO - Move all these loose scripts in the root into /scripts
@@ -61,7 +59,6 @@ rsync -r ./CONTRIBUTING.md                $outputDirectory/CONTRIBUTING.md
 rsync -r ./LICENSE                        $outputDirectory/LICENSE
 # README.md intentionally omitted.
 rsync -r ./sanitizer-blacklist.txt        $outputDirectory/sanitizer-blacklist.txt
-# sync.sh intentionally omitted.
 
 # These files cannot be upstreamed, but instead need to be manually maintained and then dropped into 'cpp-ethereum' when we merge.
 # These CMakeLists.txt were manually synthesized by Bob.
