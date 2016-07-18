ethereum/aleth

commit 80b971951092ddfb8f2affdd6d1083f238a62a2a

Enable OpenCL mining on Mesa Free Drivers
Mesa's OpenCL implementation, Clover, currently supports only OpenCL 1.1, and requires explicit enabling of an extension in order to support `static` storage class specifier. The extension is a `#pragma` directive, and therefore should not affect other OpenCL implementations.

libethash-cl/ethash_cl_miner_kernel.cl
@@ -4,6 +4,8 @@
 // Bandwidth: 124533 MB/s
 // search kernel should fit in <= 84 VGPRS (3 wavefronts)
 
+#pragma OPENCL EXTENSION cl_clang_storage_class_specifiers : enable
+
 #define THREADS_PER_HASH (128 / 16)
 #define HASHES_PER_LOOP (GROUP_SIZE / THREADS_PER_HASH)
 

commit 86d472ab705eb40a6e504949e334ec1e48da3f43

Fix regression introduced by commit 80b97195.
Commit 80b97195 enabled mining on Mesa's OpenCL implementation, while
breaking OpenCL mining for everyone else. This commit includes a
feature check around directive to enable exclusive Mesa extension.

libethash-cl/ethash_cl_miner_kernel.cl
@@ -4,7 +4,9 @@
 // Bandwidth: 124533 MB/s
 // search kernel should fit in <= 84 VGPRS (3 wavefronts)
 
+#ifdef cl_clang_storage_class_specifiers
 #pragma OPENCL EXTENSION cl_clang_storage_class_specifiers : enable
+#endif
 
 #define THREADS_PER_HASH (128 / 16)
 #define HASHES_PER_LOOP (GROUP_SIZE / THREADS_PER_HASH)