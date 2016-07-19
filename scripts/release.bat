f6861918acf0fb05c5817d0c9dac05f11fd587bd
Bob Summerwill <bob@summerwill.net>
2016-07-18 21:52:10 -0700

    Enable cache support for both Appveyor and TravisCI. Let's see how much of a speed-up we can get. Disabled tests too, to get quick enough to complete.

--- scripts/release.bat
+++ scripts/release.bat
@@ -36,9 +36,6 @@ set ETHEREUM_DEPS_PATH=%2
     .\build\ethminer\%CONFIGURATION%\ethminer.exe ^
     .\build\ethvm\%CONFIGURATION%\ethvm.exe ^
     .\build\rlp\%CONFIGURATION%\rlp.exe ^
-    .\build\test\libethereum\test\%CONFIGURATION%\testeth.exe ^
-    .\build\test\libweb3core\test\%CONFIGURATION%\testweb3core.exe ^
-    .\build\test\webthree\test\%CONFIGURATION%\testweb3.exe ^
     "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.CRT\msvc*.dll" ^
     %ETHEREUM_DEPS_PATH%\x64\bin\libcurl.dll ^
     %ETHEREUM_DEPS_PATH%\x64\bin\libmicrohttpd-dll.dll ^
