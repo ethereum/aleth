9a4f50bc4cbb763004a1752a74a5bc644835437f
chriseth <c@ethdev.com>
2016-10-25 13:59:34 +0200

    Use references for performance.

--- test/TestHelper.h
+++ test/TestHelper.h
@@ -182,8 +182,8 @@ void executeTests(const std::string& _name, const std::string& _testPathAppendix
 void userDefinedTest(std::function<void(json_spirit::mValue&, bool)> doTests);
 RLPStream createRLPStreamFromTransactionFields(json_spirit::mObject const& _tObj);
 eth::LastHashes lastHashes(u256 _currentBlockNumber);
-json_spirit::mObject fillJsonWithState(eth::State _state);
-json_spirit::mObject fillJsonWithTransaction(eth::Transaction _txn);
+json_spirit::mObject fillJsonWithState(eth::State const& _state);
+json_spirit::mObject fillJsonWithTransaction(eth::Transaction const& _txn);
 
 //Fill Test Functions
 int createRandomTest(std::vector<char*> const& _parameters);
