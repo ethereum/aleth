ad3bcb9d32aa1fb1cb71825ae854938ec53e68a6
Paweł Bylica <chfast@gmail.com>
2016-10-25 11:09:27 +0200

    Remove keccack counter

--- libdevcore/SHA3.cpp
+++ libdevcore/SHA3.cpp
@@ -211,14 +211,11 @@ defsha3(512)
 
 }
 
-unsigned g_sha3Counter = 0;
-
 bool sha3(bytesConstRef _input, bytesRef o_output)
 {
 	// FIXME: What with unaligned memory?
 	if (o_output.size() != 32)
 		return false;
-	++g_sha3Counter;
 	keccak::sha3_256(o_output.data(), 32, _input.data(), _input.size());
 //	keccak::keccak(ret.data(), 32, (uint64_t const*)_input.data(), _input.size());
 	return true;
