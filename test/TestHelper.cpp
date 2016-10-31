4892c7601cd784a13e4242a1c8129028ec3dccf2
Dimitry <winsvega@mail.ru>
2016-10-31 21:57:39 +0300

    eip158 test fillers

--- test/TestHelper.cpp
+++ test/TestHelper.cpp
@@ -563,7 +563,7 @@ string compileLLL(string const& _code)
 #else
 	char input[1024];
 	boost::filesystem::path path(boost::filesystem::temp_directory_path() / boost::filesystem::unique_path());
-	string cmd = string("lllc -o 0 ") + path.string();
+	string cmd = string("lllc ") + path.string();
 	writeFile(path.string(), _code);
 
 	FILE *fp = popen(cmd.c_str(), "r");
