23dbe027a931dea7929e41fef512d316a6eec8b8
Greg Colvin <greg@colvin.org>
2017-01-10 19:35:48 -0700

    extend code array after JUMPDEST/BEGINSUB pass, clean up hashing and surrounding ifdefs

--- libevm/VMOpt.cpp
+++ libevm/VMOpt.cpp
@@ -49,12 +49,12 @@ void VM::initMetrics()
 	done = true;
 }
 
-void VM::copyCode()
+void VM::copyCode(int _extraBytes)
 {
 	// Copy code so that it can be safely modified and extend code by
-	// 33 zero bytes to allow reading virtual data at the end of the
-	// code without bounds checks.
-	auto extendedSize = m_ext->code.size() + 33;
+	// _extraBytes zero bytes to allow reading virtual data at the end
+	// of the code without bounds checks.
+	auto extendedSize = m_ext->code.size() + _extraBytes;
 	m_codeSpace.reserve(extendedSize);
 	m_codeSpace = m_ext->code;
 	m_codeSpace.resize(extendedSize);
@@ -63,12 +63,12 @@ void VM::copyCode()
 
 void VM::optimize()
 {
-	size_t nBytes = m_codeSpace.size();
+	size_t pc, nBytes = m_codeSpace.size();
 
 	// build a table of jump destinations for use in verifyJumpDest
 	
 	TRACE_STR(1, "Build JUMPDEST table")
-	for (size_t pc = 0; pc < nBytes; ++pc)
+	for (pc = 0; pc < nBytes; ++pc)
 	{
 		Instruction op = Instruction(m_code[pc]);
 		TRACE_OP(2, pc, op);
@@ -114,61 +114,56 @@ void VM::optimize()
 		}
 #endif
 	}
+	
+	copyCode(pc - nBytes);
 
 #ifdef EVM_DO_FIRST_PASS_OPTIMIZATION
-
+	
 	#ifdef EVM_USE_CONSTANT_POOL
+		// start out FNV hash at magic number
+		uint32_t hash = 2166136261;
 		for (int i = 0; i < 256; ++i)
-			m_pool[i] = 0;
+			m_pool[i] = hash;
 	#endif
 
 	TRACE_STR(1, "Do first pass optimizations")
-	for (size_t i = 0; i < nBytes; ++i)
+	for (pc = 0; pc < nBytes; ++pc)
 	{
 		u256 val = 0;
-		Instruction op = Instruction(m_code[i]);
+		Instruction op = Instruction(m_code[pc]);
 
 		if ((byte)Instruction::PUSH1 <= (byte)op && (byte)op <= (byte)Instruction::PUSH32)
 		{
 			byte nPush = (byte)op - (byte)Instruction::PUSH1 + 1;
 
 		#ifdef EVM_USE_CONSTANT_POOL
-			uint32_t hash = 2166136261;
-		#endif
-		#ifndef EVM_REPLACE_CONST_JUMP 
-			if (1 < nPush)
-		#endif
+
 			// decode pushed bytes to integral value
-			{
-				val = m_code[i+1];
-				for (uint64_t j = i+2, n = nPush; --n; ++j) {
-					val = (val << 8) | m_code[j];
-		#ifdef EVM_USE_CONSTANT_POOL		
-					// compute FNV hash of constant
-					hash ^= m_code[j];
-					hash *= 16777619;
-				}
+			val = m_code[pc+1];
+			for (uint64_t i = pc+2, n = nPush; --n; ++i) {
+				val = (val << 8) | m_code[i];
+				
+				// maintain FNV hash of constant
+				hash ^= m_code[i];
+				hash *= 16777619;
 			}
-		
-			// add value to constant pool by folding hash to one byye and indexing into table
+
+			// index into constant pool by folding hash to one byte
 			// if there is no collision replace PUSHn with PUSHC
 			if (1 < nPush)
 			{
-				TRACE_PRE_OPT(1, i, op);
+				TRACE_PRE_OPT(1, pc, op);
 				byte h = ((hash >> 8) ^ hash) & 0xff;
 				if (m_pool[h] == 0)
 					m_pool[h] = val;
 				if (m_pool[h] == val)
 				{
-					m_code[i] = (byte)Instruction::PUSHC;
-					m_code[i+1] = h;
-					m_code[i+2] = nPush - 1;
+					m_code[pc] = (byte)Instruction::PUSHC;
+					m_code[pc+1] = h;
+					m_code[pc+2] = nPush - 1;
 					TRACE_VAL(1, "constant pooled", val);
 				}
-				TRACE_POST_OPT(1, i, op);
-			}
-		#else
-				}
+				TRACE_POST_OPT(1, pc, op);
 			}
 		#endif
 
@@ -177,32 +172,31 @@ void VM::optimize()
 			// verifyJumpDest is M = log(number of jump destinations)
 			// outer loop is N = number of bytes in code array
 			// so complexity is N log M, worst case is N log N
-			size_t ii = i + nPush + 1;
-			op = Instruction(m_code[ii]);
+			size_t i = pc + nPush + 1;
+			op = Instruction(m_code[i]);
 			if (op == Instruction::JUMP)
 			{
 				TRACE_STR(1, "Replace const JUMPC")
-				TRACE_PRE_OPT(1, ii, op);
+				TRACE_PRE_OPT(1, i, op);
 				
 				if (0 <= verifyJumpDest(val, false))
-					m_code[ii] = byte(op = Instruction::JUMPC);
+					m_code[i] = byte(op = Instruction::JUMPC);
 				
-				TRACE_POST_OPT(1, ii, op);
+				TRACE_POST_OPT(1, i, op);
 			}
 			else if (op == Instruction::JUMPI)
 			{
 				TRACE_STR(1, "Replace const JUMPCI")
-				TRACE_PRE_OPT(1, ii, op);
+				TRACE_PRE_OPT(1, i, op);
 				
 				if (0 <= verifyJumpDest(val, false))
-					m_code[ii] = byte(op = Instruction::JUMPCI);
+					m_code[i] = byte(op = Instruction::JUMPCI);
 				
 				TRACE_POST_OPT(1, ii, op);
 			}
-
 		#endif
-			
-			i += nPush;
+
+			pc += nPush;
 		}
 		
 	}
@@ -216,13 +210,9 @@ void VM::optimize()
 //
 void VM::initEntry()
 {
-	m_bounce = &VM::interpretCases; 
-	
+	m_bounce = &VM::interpretCases; 	
 	interpretCases(); // first call initializes jump table
-	
 	initMetrics();
-	
-	copyCode();
 	optimize();
 }
 
@@ -246,4 +236,3 @@ u256 VM::exp256(u256 _base, u256 _exponent)
 	}
 	return result;
 }
-
