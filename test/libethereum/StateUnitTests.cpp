dd5de26fe2c6758ec0de8170460a378d4de5985b
Paweł Bylica <chfast@gmail.com>
2017-01-24 11:43:44 +0100

    State unit tests: add account code loading test

--- test/libethereum/StateUnitTests.cpp
+++ test/libethereum/StateUnitTests.cpp
@@ -39,6 +39,21 @@ BOOST_AUTO_TEST_CASE(Basic)
 	Block s(Block::Null);
 }
 
+BOOST_AUTO_TEST_CASE(LoadAccountCode)
+{
+	Address addr{"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"};
+	State s{0};
+	s.createContract(addr);
+	uint8_t codeData[] = {'c', 'o', 'd', 'e'};
+	s.setNewCode(addr, {std::begin(codeData), std::end(codeData)});
+	s.commit(State::CommitBehaviour::RemoveEmptyAccounts);
+
+	auto& loadedCode = s.code(addr);
+	BOOST_CHECK(std::equal(
+			std::begin(codeData), std::end(codeData), std::begin(loadedCode)
+	));
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 }
