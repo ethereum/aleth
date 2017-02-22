93aeea2c338941a6c140b451d5f7d7910a37ca75
Paweł Bylica <chfast@gmail.com>
2017-02-22 17:33:58 +0100

    CMake: add coverage.info target
    This custom target can be used to generate coverage data if lcov tools is available.

--- cmake/EthCompilerSettings.cmake
+++ cmake/EthCompilerSettings.cmake
@@ -172,4 +172,12 @@ if (COVERAGE)
 	set(CMAKE_C_FLAGS "-g --coverage ${CMAKE_C_FLAGS}")
 	set(CMAKE_SHARED_LINKER_FLAGS "--coverage ${CMAKE_SHARED_LINKER_FLAGS}")
 	set(CMAKE_EXE_LINKER_FLAGS "--coverage ${CMAKE_EXE_LINKER_FLAGS}")
+	find_program(LCOV_TOOL lcov)
+	message(STATUS "lcov tool: ${LCOV_TOOL}")
+	if (LCOV_TOOL)
+		add_custom_target(coverage.info
+			COMMAND ${LCOV_TOOL} -o ${CMAKE_BINARY_DIR}/coverage.info -c -d ${CMAKE_BINARY_DIR}
+			COMMAND ${LCOV_TOOL} -o ${CMAKE_BINARY_DIR}/coverage.info -r ${CMAKE_BINARY_DIR}/coverage.info '/usr*' '${CMAKE_BINARY_DIR}/deps/*' '${CMAKE_SOURCE_DIR}/deps/*'
+		)
+	endif()
 endif ()
