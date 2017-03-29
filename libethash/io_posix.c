8209f08ba25486d4508b1e6b9872050e3eec5a1c
Andrei Maiboroda <andrei@ethereum.org>
2017-03-29 14:24:38 +0200

    Use 64-bit seek functions instead of fseek when generating DAG file

--- libethash/io_posix.c
+++ libethash/io_posix.c
@@ -34,6 +34,11 @@ FILE* ethash_fopen(char const* file_name, char const* mode)
 	return fopen(file_name, mode);
 }
 
+int ethash_fseek(FILE* f, size_t offset, int origin)
+{
+	return fseeko(f, offset, origin);
+}
+
 char* ethash_strncat(char* dest, size_t dest_size, char const* src, size_t count)
 {
 	return strlen(dest) + count + 1 <= dest_size ? strncat(dest, src, count) : NULL;
