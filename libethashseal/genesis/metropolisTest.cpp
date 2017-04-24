225485a048f335dc81a0a71e9939377b3b42c07d
Dimitry <winsvega@mail.ru>
2017-04-24 18:17:28 +0300

    fix precompiled defenition in genesis.json

--- libethashseal/genesis/metropolisTest.cpp
+++ libethashseal/genesis/metropolisTest.cpp
@@ -55,7 +55,7 @@ R"E(
 		"0000000000000000000000000000000000000001": { "wei": "1", "precompiled": { "name": "ecrecover", "linear": { "base": 3000, "word": 0 } } },
 		"0000000000000000000000000000000000000002": { "wei": "1", "precompiled": { "name": "sha256", "linear": { "base": 60, "word": 12 } } },
 		"0000000000000000000000000000000000000003": { "wei": "1", "precompiled": { "name": "ripemd160", "linear": { "base": 600, "word": 120 } } },
-		"0000000000000000000000000000000000000004": { "wei": "1", "precompiled": { "name": "identity", "linear": { "base": 15, "word": 3 } } }
+		"0000000000000000000000000000000000000004": { "wei": "1", "precompiled": { "name": "identity", "linear": { "base": 15, "word": 3 } } },
 		"0000000000000000000000000000000000000005": { "wei": "1", "precompiled": { "name": "modexp" } }
 	}
 }
