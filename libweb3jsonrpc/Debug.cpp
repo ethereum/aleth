srowlison/cpp-ethereum

Make sure debug_traceBlock, debug_traceTransaction, debug_storageRangeAt, admin_eth_vmTrace RPC methods work even when intermediate state root hash is not available in receipts
(this going to be the case after Metropolis). In this case execute all previous transactions in the block to get the intermediate state.

libweb3jsonrpc/Debug.cpp
@@ -61,13 +61,19 @@ Json::Value Debug::traceTransaction(Executive& _e, Transaction const& _t, Json::
 
 Json::Value Debug::traceBlock(Block const& _block, Json::Value const& _json)
 {
+	State s(_block.state());
+	s.setRoot(_block.stateRootBeforeTx(0));
+
 	Json::Value traces(Json::arrayValue);
 	for (unsigned k = 0; k < _block.pending().size(); k++)
 	{
 		Transaction t = _block.pending()[k];
-		State s(State::Null);
+
+		u256 const gasUsed = k ? _block.receipt(k - 1).gasUsed() : 0;
+		EnvInfo envInfo(_block.info(), m_eth.blockChain().lastHashes(_block.info().parentHash()), gasUsed);
+		Executive e(s, envInfo, *m_eth.blockChain().sealEngine());
+
 		eth::ExecutionResult er;
-		Executive e(s, _block, k, m_eth.blockChain());
 		e.setResultRecipient(er);
 		traces.append(traceTransaction(e, t, _json));
 	}
@@ -136,7 +142,15 @@ Json::Value Debug::debug_storageRangeAt(string const& _blockHashOrNumber, int _t
 		Block block = m_eth.block(blockHash(_blockHashOrNumber));
 
 		unsigned const i = ((unsigned)_txIndex < block.pending().size()) ? (unsigned)_txIndex : block.pending().size();
-		State state = block.fromPending(i);
+		State state(block.state());
+		u256 const rootHash = block.stateRootBeforeTx(_txIndex);
+		if (rootHash)
+			state.setRoot(rootHash);
+		else
+		{
+			state.setRoot(block.stateRootBeforeTx(0));
+			state.executeBlockTransactions(block, _txIndex, m_eth.blockChain().lastHashes(block.info().parentHash()), *m_eth.blockChain().sealEngine());
+		}
 
 		map<h256, pair<u256, u256>> const storage(state.storage(Address(_address)));
 

ethereum/aleth

libweb3jsonrpc/Debug.cpp
@@ -61,13 +61,19 @@ Json::Value Debug::traceTransaction(Executive& _e, Transaction const& _t, Json::
 
 Json::Value Debug::traceBlock(Block const& _block, Json::Value const& _json)
 {
+	State s(_block.state());
+	s.setRoot(_block.stateRootBeforeTx(0));
+
 	Json::Value traces(Json::arrayValue);
 	for (unsigned k = 0; k < _block.pending().size(); k++)
 	{
 		Transaction t = _block.pending()[k];
-		State s(State::Null);
+
+		u256 const gasUsed = k ? _block.receipt(k - 1).gasUsed() : 0;
+		EnvInfo envInfo(_block.info(), m_eth.blockChain().lastHashes(_block.info().parentHash()), gasUsed);
+		Executive e(s, envInfo, *m_eth.blockChain().sealEngine());
+
 		eth::ExecutionResult er;
-		Executive e(s, _block, k, m_eth.blockChain());
 		e.setResultRecipient(er);
 		traces.append(traceTransaction(e, t, _json));
 	}
@@ -136,7 +142,15 @@ Json::Value Debug::debug_storageRangeAt(string const& _blockHashOrNumber, int _t
 		Block block = m_eth.block(blockHash(_blockHashOrNumber));
 
 		unsigned const i = ((unsigned)_txIndex < block.pending().size()) ? (unsigned)_txIndex : block.pending().size();
-		State state = block.fromPending(i);
+		State state(block.state());
+		u256 const rootHash = block.stateRootBeforeTx(_txIndex);
+		if (rootHash)
+			state.setRoot(rootHash);
+		else
+		{
+			state.setRoot(block.stateRootBeforeTx(0));
+			state.executeBlockTransactions(block, i, m_eth.blockChain().lastHashes(block.info().parentHash()), *m_eth.blockChain().sealEngine());
+		}
 
 		map<h256, pair<u256, u256>> const storage(state.storage(Address(_address)));
 