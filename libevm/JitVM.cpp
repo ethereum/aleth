2987c8dcb392e4a86b15fe8d4767d292bb70dc4e
Paweł Bylica <chfast@gmail.com>
2017-04-28 15:27:38 +0200

    EVM-C: update call callback, do not copy output

--- libevm/JitVM.cpp
+++ libevm/JitVM.cpp
@@ -47,7 +47,7 @@ void queryState(
 	evm_uint256be const* _storageKey
 ) noexcept
 {
-	auto &env = *reinterpret_cast<ExtVMFace*>(_opaqueEnv);
+	auto &env = reinterpret_cast<ExtVMFace&>(*_opaqueEnv);
 	Address addr = fromEvmC(*_addr);
 	switch (_key)
 	{
@@ -85,7 +85,7 @@ void updateState(
 ) noexcept
 {
 	(void) _addr;
-	auto &env = *reinterpret_cast<ExtVMFace*>(_opaqueEnv);
+	auto &env = reinterpret_cast<ExtVMFace&>(*_opaqueEnv);
 	assert(fromEvmC(*_addr) == env.myAddress);
 	switch (_key)
 	{
@@ -115,7 +115,7 @@ void updateState(
 
 void getTxContext(evm_tx_context* result, evm_env* _opaqueEnv) noexcept
 {
-	auto &env = *reinterpret_cast<ExtVMFace*>(_opaqueEnv);
+	auto &env = reinterpret_cast<ExtVMFace&>(*_opaqueEnv);
 	result->tx_gas_price = toEvmC(env.gasPrice);
 	result->tx_origin = toEvmC(env.origin);
 	result->block_coinbase = toEvmC(env.envInfo().author());
@@ -127,25 +127,19 @@ void getTxContext(evm_tx_context* result, evm_env* _opaqueEnv) noexcept
 
 void getBlockHash(evm_uint256be* o_hash, evm_env* _envPtr, int64_t _number)
 {
-	auto &env = *reinterpret_cast<ExtVMFace*>(_envPtr);
+	auto &env = reinterpret_cast<ExtVMFace&>(*_envPtr);
 	*o_hash = toEvmC(env.blockHash(_number));
 }
 
-int64_t call(
-	evm_env* _opaqueEnv,
-	evm_message const* _msg,
-	uint8_t* _outputData,
-	size_t _outputSize
-) noexcept
+void call(evm_result* o_result, evm_env* _opaqueEnv, evm_message const* _msg) noexcept
 {
 	assert(_msg->gas >= 0 && "Invalid gas value");
-	auto &env = *reinterpret_cast<ExtVMFace*>(_opaqueEnv);
+	auto &env = reinterpret_cast<ExtVMFace&>(*_opaqueEnv);
 	u256 value = fromEvmC(_msg->value);
 	bytesConstRef input{_msg->input, _msg->input_size};
 
 	if (_msg->kind == EVM_CREATE)
 	{
-		assert(_outputSize == 20);
 		u256 gas = _msg->gas;
 		// ExtVM::create takes the sender address from .myAddress.
 		assert(fromEvmC(_msg->sender) == env.myAddress);
@@ -154,13 +148,23 @@ int64_t call(
 		//       the output is ignored here.
 		h160 addr;
 		std::tie(addr, std::ignore) = env.create(value, gas, input, {});
-		auto gasLeft = static_cast<int64_t>(gas);
+		o_result->gas_left = static_cast<int64_t>(gas);
+		o_result->release = nullptr;
 		if (addr)
-			std::copy(addr.begin(), addr.end(), _outputData);
+		{
+			o_result->code = EVM_SUCCESS;
+			// Use reserved data to store the address.
+			std::copy(addr.begin(), addr.end(), o_result->reserved.data);
+			o_result->output_data = o_result->reserved.data;
+			o_result->output_size = addr.size;
+		}
 		else
-			gasLeft |= EVM_CALL_FAILURE;
-
-		return gasLeft;
+		{
+			o_result->code = EVM_FAILURE;
+			o_result->output_data = nullptr;
+			o_result->output_size = 0;
+		}
+		return;
 	}
 
 	CallParameters params;
@@ -173,15 +177,35 @@ int64_t call(
 	params.data = input;
 	params.onOp = {};
 
-	auto output = env.call(params);
-	auto gasLeft = static_cast<int64_t>(params.gas);
+	bool success = false;
+	owning_bytes_ref output;
+	std::tie(success, output) = env.call(params);
+	// FIXME: We have a mess here. It is hard to distinguish reverts from failures.
+	// In first case we want to keep the output, in the second one the output
+	// is optional and should not be passed to the contract, but can be useful
+	// for EVM in general.
+	o_result->code = success ? EVM_SUCCESS : EVM_REVERT;
+	o_result->gas_left = static_cast<int64_t>(params.gas);
 
-	output.second.copyTo({_outputData, _outputSize});
-	if (!output.first)
-		// Add failure indicator.
-		gasLeft |= EVM_CALL_FAILURE;
+	// Pass the output to the EVM without a copy. The EVM will delete it
+	// when finished with it.
 
-	return gasLeft;
+	// First assign reference. References are not invalidated when vector
+	// of bytes is moved. See `.takeBytes()` below.
+	o_result->output_data = output.data();
+	o_result->output_size = output.size();
+
+	// Place a new vector of bytes containing output in result's reserved memory.
+	static_assert(sizeof(bytes) <= sizeof(o_result->reserved), "Vector is too big");
+	new(&o_result->reserved) bytes(output.takeBytes());
+	// Set the destructor to delete the vector.
+	o_result->release = [](evm_result const* _result)
+	{
+		auto& output = reinterpret_cast<bytes const&>(_result->reserved);
+		// Explicitly call vector's destructor to release its data.
+		// This is normal pattern when placement new operator is used.
+		output.~bytes();
+	};
 }
 
 
