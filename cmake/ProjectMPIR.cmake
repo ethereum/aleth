88a0786cd6d76f065d3120935bfb7fea115a6971
Paweł Bylica <chfast@gmail.com>
2017-05-08 22:04:06 +0200

    libff: Fix configuration

--- cmake/ProjectMPIR.cmake
+++ cmake/ProjectMPIR.cmake
@@ -1,18 +1,19 @@
 ExternalProject_Add(mpir
     PREFIX ${CMAKE_SOURCE_DIR}/deps
-    DOWNLOAD_NAME mpir-master.tar.gz
+    DOWNLOAD_NAME mpir-cmake.tar.gz
     DOWNLOAD_NO_PROGRESS TRUE
-    URL https://github.com/chfast/mpir/archive/master.tar.gz
-    URL_HASH SHA256=ed52442ab345c9dbb463d9c4bee4436785dd6a3ab6d24e461302dd3f93bedbb4
+    URL https://github.com/chfast/mpir/archive/cmake.tar.gz
+    URL_HASH SHA256=2bf35ad7aa2b61bf4c2dcdab9aab48b2b4f9d6db8b937be4b0b5fe3b5efbdfa6
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-        -DCMAKE_BUILD_TYPE=Release
+        -DCMAKE_BUILD_TYPE=Debug
+        -DMPIR_GMP=On
 )
 
 ExternalProject_Get_Property(mpir INSTALL_DIR)
-add_library(MPIR::mpz STATIC IMPORTED)
-set(MPIR_LIBRARY ${INSTALL_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}mpz${CMAKE_STATIC_LIBRARY_SUFFIX})
+add_library(MPIR::mpir STATIC IMPORTED)
+set(MPIR_LIBRARY ${INSTALL_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}mpir${CMAKE_STATIC_LIBRARY_SUFFIX})
 set(MPIR_INCLUDE_DIR ${INSTALL_DIR}/include)
-set_property(TARGET MPIR::mpz PROPERTY IMPORTED_LOCATION ${MPIR_LIBRARY})
-set_property(TARGET MPIR::mpz PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${MPIR_INCLUDE_DIR})
-add_dependencies(MPIR::mpz mpir)
+set_property(TARGET MPIR::mpir PROPERTY IMPORTED_LOCATION ${MPIR_LIBRARY})
+set_property(TARGET MPIR::mpir PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${MPIR_INCLUDE_DIR})
+add_dependencies(MPIR::mpir mpir)
 unset(INSTALL_DIR)
--- cmake/ProjectSnark.cmake
+++ cmake/ProjectSnark.cmake
@@ -4,14 +4,18 @@ include(ProjectMPIR)
 
 ExternalProject_Add(snark
     PREFIX ${CMAKE_SOURCE_DIR}/deps
-    DOWNLOAD_NAME libff-a1021d20.tar.gz
+    DOWNLOAD_NAME libff-97d3fa6c.tar.gz
     DOWNLOAD_NO_PROGRESS TRUE
-    URL https://github.com/chfast/libff/archive/a1021d2066d1b73fe701fdb60b2e521134b126bf.tar.gz
-    URL_HASH SHA256=f71052cd97e55898853f44434dbf3a804d3b66afd950a81093a277bcc9e002f6
+    URL https://github.com/chfast/libff/archive/97d3fa6cdbd4b7549c7a8a179dc97308dbfd044d.tar.gz
+    URL_HASH SHA256=f102f3ee43c96c9a81c20d8c0446c805c6b8c0e3121518b3625f08e2c230096e
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
         -DGMP_INCLUDE_DIR=${MPIR_INCLUDE_DIR}
+        -DGMP_LIBRARIES=${MPIR_LIBRARY}
+        -DGMPXX_LIBRARIES=${MPIR_LIBRARY}
         -DCURVE=ALT_BN128 -DPERFORMANCE=Off -DWITH_PROCPS=Off
         -DUSE_PT_COMPRESSION=Off
+    BUILD_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --config Release
+    INSTALL_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --config Release --target install
 )
 add_dependencies(snark boost)
 
@@ -23,6 +27,6 @@ set(SNARK_INCLUDE_DIR ${INSTALL_DIR}/include/libff)
 file(MAKE_DIRECTORY ${SNARK_INCLUDE_DIR})
 set_property(TARGET Snark PROPERTY IMPORTED_LOCATION ${SNARK_LIBRARY})
 set_property(TARGET Snark PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${SNARK_INCLUDE_DIR})
-set_property(TARGET Snark PROPERTY INTERFACE_LINK_LIBRARIES MPIR::mpz)
+set_property(TARGET Snark PROPERTY INTERFACE_LINK_LIBRARIES MPIR::mpir)
 add_dependencies(Snark snark)
 unset(INSTALL_DIR)
