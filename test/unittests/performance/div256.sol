54c844191caa8dc33efbc8c6d808ad760d6f32bf
gcolvin <greg@colvin.org>
2017-05-30 08:44:40 -0600

    get number of itereations right

--- test/unittests/performance/div256.sol
+++ test/unittests/performance/div256.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.4.0;
 contract div256 {
 	function div256() {
 		uint r;
-		for (uint i = 0; i < 4000000; ++i) {	// DIV by square root gives same divisor
+		for (uint i = 0; i < 2000000; ++i) {	// DIV by square root gives same divisor
 			assembly {
 				0xff3f9014f20db29ae04af2c2d265de17
 				0xfe7fb0d1f59dfe9492ffbf73683fd1e870eec79504c60144cc7f5fc2bad1e611
