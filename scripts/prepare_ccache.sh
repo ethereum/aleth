cf1304d010f4eb2c438776a0c22b0519e26233d1
Yoichi Hirai <i@yoichihirai.com>
2017-06-19 12:17:55 +0200

    Removing ccache to see if that fixes the build failure

--- scripts/prepare_ccache.sh
+++ scripts/prepare_ccache.sh
@@ -5,8 +5,7 @@ set -e
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     # In Travis Ubuntu 14.04 ccache is too old and does not support some of
     # the compiler flags and does not work. Upgrade it to version 3.2.
-    curl -O http://de.archive.ubuntu.com/ubuntu/pool/main/c/ccache/ccache_3.2.4-1_amd64.deb
-    sudo dpkg -i ccache_3.2.4-1_amd64.deb
+    sudo rm /usr/bin/ccache
 fi
 
 ccache --version
