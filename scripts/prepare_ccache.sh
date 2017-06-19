c2ef40b87a2878dfd8f618cf2c39bd66a59ba03c
Yoichi Hirai <i@yoichihirai.com>
2017-06-19 13:58:17 +0200

    Fix prepare_cache.sh

--- scripts/prepare_ccache.sh
+++ scripts/prepare_ccache.sh
@@ -6,9 +6,10 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     # In Travis Ubuntu 14.04 ccache is too old and does not support some of
     # the compiler flags and does not work. Upgrade it to version 3.2.
     sudo rm /usr/bin/ccache
+else
+    ccache --version
+    ccache --show-stats
+    ccache --zero-stats
+    ccache --max-size=1G
 fi
 
-ccache --version
-ccache --show-stats
-ccache --zero-stats
-ccache --max-size=1G
