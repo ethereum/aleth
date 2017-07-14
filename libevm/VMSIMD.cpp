4764f1440337c4a0abde1568efea87fe82f46b7e
gcolvin <greg@colvin.org>
2017-07-13 23:07:32 -0600

    name nits

--- libevm/VMSIMD.cpp
+++ libevm/VMSIMD.cpp
@@ -32,16 +32,16 @@ using a64x4  = uint64_t[4];
 using a32x8  = uint32_t[8];
 using a16x16 = uint16_t[16];
 using a8x32  = uint8_t [32];
-inline a64x4       & v64x4 (u256      & _stack_item) { return (a64x4&) *(a64x4*) &_stack_item; }
-inline a32x8       & v32x8 (u256      & _stack_item) { return (a32x8&) *(a32x8*) &_stack_item; }
-inline a16x16      & v16x16(u256      & _stack_item) { return (a16x16&)*(a16x16*)&_stack_item; }
-inline a8x32       & v8x32 (u256      & _stack_item) { return (a8x32&) *(a8x32*) &_stack_item; }
-inline a64x4  const& v64x4 (u256 const& _stack_item) { return (a64x4&) *(a64x4*) &_stack_item; }
-inline a32x8  const& v32x8 (u256 const& _stack_item) { return (a32x8&) *(a32x8*) &_stack_item; }
-inline a16x16 const& v16x16(u256 const& _stack_item) { return (a16x16&)*(a16x16*)&_stack_item; }
-inline a8x32  const& v8x32 (u256 const& _stack_item) { return (a8x32&) *(a8x32*) &_stack_item; }
+inline a64x4       & v64x4 (u256      & _stackItem) { return (a64x4&) *(a64x4*) &_stackItem; }
+inline a32x8       & v32x8 (u256      & _stackItem) { return (a32x8&) *(a32x8*) &_stackItem; }
+inline a16x16      & v16x16(u256      & _stackItem) { return (a16x16&)*(a16x16*)&_stackItem; }
+inline a8x32       & v8x32 (u256      & _stackItem) { return (a8x32&) *(a8x32*) &_stackItem; }
+inline a64x4  const& v64x4 (u256 const& _stackItem) { return (a64x4&) *(a64x4*) &_stackItem; }
+inline a32x8  const& v32x8 (u256 const& _stackItem) { return (a32x8&) *(a32x8*) &_stackItem; }
+inline a16x16 const& v16x16(u256 const& _stackItem) { return (a16x16&)*(a16x16*)&_stackItem; }
+inline a8x32  const& v8x32 (u256 const& _stackItem) { return (a8x32&) *(a8x32*) &_stackItem; }
 
-class Pow2Bits enum { BITS_8, BITS_16, BITS_32, BITS_64 };
+enum { Bits8, Bits16, Bits32, Bits64 };
 
 // tried using template template functions, gave up fighting the compiler after a day
 #define EVALXOPS(OP, _type) EVALXOP(OP, int8_t, int16_t, int32_t, int64_t, _type)
@@ -52,19 +52,19 @@ class Pow2Bits enum { BITS_8, BITS_16, BITS_32, BITS_64 };
 	m_SPP[0] = 0; \
 	switch (t) \
 	{ \
-	case BITS_8: \
+	case Bits8: \
 		for (int i = 0; i < 32; ++i) \
 			v8x32(m_SPP[0])[i]  = (uint8_t) OP((T8) v8x32(m_SP[0])[i],  (T8) v8x32(m_SP[1])[i]); \
 		break; \
-	case BITS_16: \
+	case Bits16: \
 		for (int i = 0; i < 16; ++i) \
 			v16x16(m_SPP[0])[i] = (uint16_t)OP((T16)v16x16(m_SP[0])[i], (T16)v16x16(m_SP[1])[i]); \
 		break; \
-	case BITS_32: \
+	case Bits32: \
 		for (int i = 0; i < 8; ++i) \
 			v32x8(m_SPP[0])[i]  = (uint32_t)OP((T32)v32x8(m_SP[0])[i],  (T32)v32x8(m_SP[1])[i]); \
 		break; \
-	case BITS_64: \
+	case Bits64: \
 		for (int i = 0; i < 4; ++i) \
 			v64x4(m_SPP[0])[i]  = (uint64_t)OP((T64)v64x4(m_SP[0])[i],  (T64)v64x4(m_SP[1])[i]); \
 		break; \
@@ -136,28 +136,28 @@ u256 VM::vtow(uint8_t _type, const u256& _in)
 	uint8_t const width = laneWidth(_type);
 	switch (width)
 	{
-	case BITS_8:
+	case Bits8:
 		for (int i = count - 1; 0 <= i; --i)
 		{ 
 			out << 8;
 			out |= v8x32(_in) [i];
 		}
 		break;
-	case BITS_16:
+	case Bits16:
 		for (int i = count - 1; 0 <= i; --i)
 		{ 
 			out << 16;
 			out |= v16x16(_in)[i];
 		}
 		break;
-	case BITS_32:
+	case Bits32:
 		for (int i = count - 1; 0 <= i; --i)
 		{ 
 			out << 32;
 			out |= v32x8(_in) [i];
 		}
 		break;
-	case BITS_64:
+	case Bits64:
 		for (int i = count - 1; 0 <= i; --i)
 		{ 
 			out << 64;
@@ -171,37 +171,37 @@ u256 VM::vtow(uint8_t _type, const u256& _in)
 }
 
 // out must be by reference because it is really just memory for a vector
-void VM::wtov(uint8_t _type, u256 _in, u256& _o_out)
+void VM::wtov(uint8_t _type, u256 _in, u256& o_out)
 {
 	uint8_t const count = laneCount(_type);
 	uint8_t const width = laneWidth(_type);
 	switch (width)
 	{
-	case BITS_8:
+	case Bits8:
 		for (int i = count - 1; 0 <= i; --i)
 		{ 
-			v8x32(_o_out) [i] = (uint8_t )(_in & 0xff);
+			v8x32(o_out) [i] = (uint8_t )(_in & 0xff);
 			_in >>= 8;
 		}
 		break;
-	case BITS_16:
+	case Bits16:
 		for (int i = count - 1; 0 <= i; --i)
 		{ 
-			v16x16(_o_out)[i] = (uint16_t)(_in & 0xffff);
+			v16x16(o_out)[i] = (uint16_t)(_in & 0xffff);
 			_in >>= 16;
 		}
 		break;
-	case BITS_32:
+	case Bits32:
 		for (int i = count - 1; 0 <= i; --i)
 		{ 
-			v32x8(_o_out) [i] = (uint32_t)(_in & 0xffffff);
+			v32x8(o_out) [i] = (uint32_t)(_in & 0xffffff);
 			_in >>= 32;
 		}
 		break;
-	case BITS_64:
+	case Bits64:
 		for (int i = count - 1; 0 <= i; --i)
 		{ 
-			v64x4(_o_out) [i] = (uint64_t)(_in & 0xffffffff);
+			v64x4(o_out) [i] = (uint64_t)(_in & 0xffffffff);
 			_in >>= 64;
 		}
 		break;
@@ -219,7 +219,7 @@ void VM::xmload (uint8_t _type)
 
 	switch (width)
 	{
-	case BITS_8:
+	case Bits8:
 		for (int j = 1,  i = count - 1; 0 <= i; --i)
 		{
 			int v = 0;
@@ -227,7 +227,7 @@ void VM::xmload (uint8_t _type)
 			v8x32(m_SPP[0])[i] = v;
 		}
 		break;
-	case BITS_16:
+	case Bits16:
 		for (int j = 2,  i = count - 1; 0 <= i; --i)
 		{
 			int v = 0;
@@ -237,7 +237,7 @@ void VM::xmload (uint8_t _type)
 			v16x16(m_SPP[0])[i] = v;
 		}
 		break;
-	case BITS_32:
+	case Bits32:
 		for (int j = 4,  i = count - 1; 0 <= i; --i)
 		{
 			int v = 0;
@@ -251,7 +251,7 @@ void VM::xmload (uint8_t _type)
 			v32x8(m_SPP[0])[i] = v;
 		}
 		break;
-	case BITS_64:
+	case Bits64:
 		for (int j = 8,  i = count - 1; 0 <= i; --i)
 		{
 			int v = 0;
@@ -288,7 +288,7 @@ void VM::xmstore(uint8_t _type)
 
 	switch (width)
 	{
-	case BITS_8:
+	case Bits8:
 		for (int j = 1,  i = count - 1; 0 <= i; --i)
 		{
 			int v = 0;
@@ -296,7 +296,7 @@ void VM::xmstore(uint8_t _type)
 			p[--j] = (uint8_t)v;
 		}
 		break;
-	case BITS_16:
+	case Bits16:
 		for (int j = 2,  i = count - 1; 0 <= i; --i)
 		{
 			int v = 2;
@@ -306,7 +306,7 @@ void VM::xmstore(uint8_t _type)
 			p[--j] = (uint8_t)v;
 		}
 		break;
-	case BITS_32:
+	case Bits32:
 		for (int j = 4,  i = count - 1; 0 <= i; --i)
 		{
 			int v = 4;
@@ -320,7 +320,7 @@ void VM::xmstore(uint8_t _type)
 			p[--j] = (uint8_t)v;
 		}
 		break;
-	case BITS_64:
+	case Bits64:
 		for (int j = 8,  i = count - 1; 0 <= i; --i)
 		{
 			int v = 0;
@@ -383,13 +383,13 @@ void VM::xpush(uint8_t _type)
 	m_SPP[0] = 0;
 	switch (width)
 	{
-	case BITS_8:
+	case Bits8:
 		for (int i = 0; i < count ; ++i)
 		{
 			v8x32(m_SPP[0])[i] = m_code[++m_PC];
 		}
 		break;
-	case BITS_16:
+	case Bits16:
 		for (int i = 0; i < count; ++i)
 		{
 			uint16_t v = m_code[++m_PC];
@@ -397,7 +397,7 @@ void VM::xpush(uint8_t _type)
 			v16x16(m_SPP[0])[i] = v;
 		}
 		break;
-	case BITS_32:
+	case Bits32:
 		for (int i = 0; i < count; ++i)
 		{
 			uint32_t v = m_code[m_PC];
@@ -407,7 +407,7 @@ void VM::xpush(uint8_t _type)
 			v32x8(m_SPP[0])[i] = v;
 		}
 		break;
-	case BITS_64:
+	case Bits64:
 		for (int i = 0; i < count; ++i)
 		{
 			uint64_t v = m_code[++m_PC];
@@ -426,33 +426,33 @@ void VM::xpush(uint8_t _type)
 	}
 }
 
-void VM::xget(uint8_t _src_type, uint8_t _idx_type)
+void VM::xget(uint8_t _srcType, uint8_t _idxType)
 {
-	uint8_t const srcWidth = laneWidth(_src_type);
-	uint8_t const idxCount = laneCount(_idx_type);
-	uint8_t const idxWidth = laneWidth(_idx_type);
+	uint8_t const srcWidth = laneWidth(_srcType);
+	uint8_t const idxCount = laneCount(_idxType);
+	uint8_t const idxWidth = laneWidth(_idxType);
 	
 	// given the type of the source and index
 	// for every element of the index get the indexed element from the source
 	switch (srcWidth)
 	{
-	case BITS_8:
+	case Bits8:
 
 		switch (idxWidth)
 		{
-		case BITS_8:
+		case Bits8:
 			for (int i = 0; i < idxCount; ++i)
 				v8x32 (m_SPP[1])[i] = v8x32(m_SP[0])[v8x32 (m_SP[1])[i] % idxCount];
 			break;
-		case BITS_16:
+		case Bits16:
 			for (int i = 0; i< idxCount; ++i)
 				v16x16(m_SPP[1])[i] = v8x32(m_SP[0])[v16x16(m_SP[1])[i] % idxCount];
 			break;
-		case BITS_32:
+		case Bits32:
 			for (int i = 0; i< idxCount; ++i)
 				v32x8 (m_SPP[1])[i] = v8x32(m_SP[0])[v32x8 (m_SP[1])[i] % idxCount];
 			break;
-		case BITS_64:
+		case Bits64:
 			for (int i = 0; i< idxCount; ++i)
 				v64x4 (m_SPP[1])[i] = v8x32(m_SP[0])[v64x4 (m_SP[1])[i] % idxCount];
 			break;
@@ -460,23 +460,23 @@ void VM::xget(uint8_t _src_type, uint8_t _idx_type)
 			throwBadInstruction();
 		}
 
-	case BITS_16:
+	case Bits16:
 
 		switch (idxWidth)
 		{
-		case BITS_8:
+		case Bits8:
 			for (int i = 0; i < idxCount; ++i)
 				v8x32 (m_SPP[0])[i] = v16x16(m_SP[1])[v8x32 (m_SP[0])[i] % idxCount];
 			break;
-		case BITS_16:
+		case Bits16:
 			for (int i = 0; i < idxCount; ++i)
 				v16x16(m_SPP[0])[i] = v16x16(m_SP[1])[v16x16(m_SP[0])[i] % idxCount];
 			break;
-		case BITS_32:
+		case Bits32:
 			for (int i = 0; i < idxCount; ++i)
 				v32x8 (m_SPP[0])[i] = v16x16(m_SP[1])[v32x8 (m_SP[0])[i] % idxCount];
 			break;
-		case BITS_64:
+		case Bits64:
 			for (int i = 0; i < idxCount; ++i)
 				v64x4 (m_SPP[0])[i] = v16x16(m_SP[1])[v64x4 (m_SP[0])[i] % idxCount];
 			break;
@@ -484,23 +484,23 @@ void VM::xget(uint8_t _src_type, uint8_t _idx_type)
 			throwBadInstruction();
 		}
 
-	case BITS_32:
+	case Bits32:
 
 		switch (idxWidth)
 		{
-		case BITS_8:
+		case Bits8:
 			for (int i = 0; i < idxCount; ++i)
 				v8x32 (m_SPP[0])[i] = v32x8(m_SP[1])[v8x32 (m_SP[0])[i] % idxCount];
 			break;
-		case BITS_16:
+		case Bits16:
 			for (int i = 0; i < idxCount; ++i)
 				v16x16(m_SPP[0])[i] = v32x8(m_SP[1])[v16x16(m_SP[0])[i] % idxCount];
 			break;
-		case BITS_32:
+		case Bits32:
 			for (int i = 0; i < idxCount; ++i)
 				v32x8 (m_SPP[0])[i] = v32x8(m_SP[1])[v32x8 (m_SP[0])[i] % idxCount];
 			break;
-		case BITS_64:
+		case Bits64:
 			for (int i = 0; i < idxCount; ++i)
 				v64x4 (m_SPP[0])[i] = v32x8(m_SP[1])[v64x4 (m_SP[0])[i] % idxCount];
 			break;
@@ -508,23 +508,23 @@ void VM::xget(uint8_t _src_type, uint8_t _idx_type)
 			throwBadInstruction();
 		}
 
-	case BITS_64:
+	case Bits64:
 
 		switch (idxWidth)
 		{
-		case BITS_8:
+		case Bits8:
 			for (int i = 0; i < idxCount; ++i)
 				v8x32 (m_SPP[0])[i] = v64x4(m_SP[1])[v8x32 (m_SP[0])[i] % idxCount];
 			break;
-		case BITS_16:
+		case Bits16:
 			for (int i = 0; i < idxCount; ++i)
 				v16x16(m_SPP[0])[i] = v64x4(m_SP[1])[v16x16(m_SP[0])[i] % idxCount];
 			break;
-		case BITS_32:
+		case Bits32:
 			for (int i = 0; i < idxCount; ++i)
 				v32x8 (m_SPP[0])[i] = v64x4(m_SP[1])[v32x8 (m_SP[0])[i] % idxCount];
 			break;
-		case BITS_64:
+		case Bits64:
 			for (int i = 0; i < idxCount; ++i)
 				v64x4 (m_SPP[0])[i] = v64x4(m_SP[1])[v64x4 (m_SP[0])[i] % idxCount];
 			break;
@@ -537,33 +537,33 @@ void VM::xget(uint8_t _src_type, uint8_t _idx_type)
 	}
 }
 
-void VM::xput(uint8_t _src_type, uint8_t _dst_type)
+void VM::xput(uint8_t _srcType, uint8_t _dstType)
 {
-	uint8_t const srcWidth = laneWidth(_src_type);
-	uint8_t const dstCount = laneCount(_dst_type);
-	uint8_t const dstWidth = laneWidth(_dst_type);
+	uint8_t const srcWidth = laneWidth(_srcType);
+	uint8_t const dstCount = laneCount(_dstType);
+	uint8_t const dstWidth = laneWidth(_dstType);
 
 	// given the type of the source, destination and index
 	// for every element of the index put the indexed replacement in the destination                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
 	switch (srcWidth)
 	{
-	case BITS_8:
+	case Bits8:
 
 		switch (dstWidth)
 		{
-		case BITS_8:
+		case Bits8:
 			for (int i = 0; i < dstCount; ++i)
 				v8x32 (m_SPP[0])[v8x32(m_SP[1])[i] % 32] = v8x32(m_SP[0])[i];
 			break;
-		case BITS_16:
+		case Bits16:
 			for (int i = 0; i < dstCount; ++i)
 				v16x16(m_SPP[0])[v8x32(m_SP[1])[i] % 16] = v8x32(m_SP[0])[i];
 			break;
-		case BITS_32:
+		case Bits32:
 			for (int i = 0; i < dstCount; ++i)
 				v32x8 (m_SPP[0])[v8x32(m_SP[1])[i] %  8] = v8x32(m_SP[0])[i];
 			break;
-		case BITS_64:
+		case Bits64:
 			for (int i = 0; i < dstCount; ++i)
 				v64x4 (m_SPP[0])[v8x32(m_SP[1])[i] %  4] = v8x32(m_SP[0])[i];
 			break;
@@ -571,23 +571,23 @@ void VM::xput(uint8_t _src_type, uint8_t _dst_type)
 			throwBadInstruction();
 		}
 
-	case BITS_16:
+	case Bits16:
 
 		switch (dstWidth)
 		{
-		case BITS_8:
+		case Bits8:
 			for (int i = 0; i < dstCount; ++i)
 				v8x32 (m_SPP[0])[v16x16(m_SP[1])[i] % 32] = v16x16(m_SP[0])[i];
 			break;
-		case BITS_16:
+		case Bits16:
 			for (int i = 0; i < dstCount; ++i)
 				v16x16(m_SPP[0])[v16x16(m_SP[1])[i] % 16] = v16x16(m_SP[0])[i];
 			break;
-		case BITS_32:
+		case Bits32:
 			for (int i = 0; i < dstCount; ++i)
 				v32x8(m_SPP[0])[v16x16(m_SP[1])[i] %  8] = v16x16(m_SP[0])[i];
 			break;
-		case BITS_64:
+		case Bits64:
 			for (int i = 0; i < dstCount; ++i)
 				v64x4(m_SPP[0])[v16x16(m_SP[1])[i] %  4] = v16x16(m_SP[0])[i];
 			break;
@@ -595,23 +595,23 @@ void VM::xput(uint8_t _src_type, uint8_t _dst_type)
 			throwBadInstruction();
 		}
 
-	case BITS_32:
+	case Bits32:
 
 		switch (dstWidth)
 		{
-		case BITS_8:
+		case Bits8:
 			for (int i = 0; i < dstCount; ++i)
 				v8x32 (m_SPP[0])[v32x8(m_SP[1])[i] % 32] = v32x8(m_SP[0])[i];
 			break;
-		case BITS_16:
+		case Bits16:
 			for (int i = 0; i < dstCount; ++i)
 				v16x16(m_SPP[0])[v32x8(m_SP[1])[i] % 16] = v32x8(m_SP[0])[i];
 			break;
-		case BITS_32:
+		case Bits32:
 			for (int i = 0; i < dstCount; ++i)
 				v32x8 (m_SPP[0])[v32x8(m_SP[1])[i] %  8] = v32x8(m_SP[0])[i];
 			break;
-		case BITS_64:
+		case Bits64:
 			for (int i = 0; i < dstCount; ++i)
 				v64x4 (m_SPP[0])[v32x8(m_SP[1])[i] %  4] = v32x8(m_SP[0])[i];
 			break;
@@ -619,23 +619,23 @@ void VM::xput(uint8_t _src_type, uint8_t _dst_type)
 			throwBadInstruction();
 		}
 
-	case BITS_64:
+	case Bits64:
 
 		switch (dstWidth)
 		{
-		case BITS_8:
+		case Bits8:
 			for (int i = 0; i < dstCount; ++i)
 				v8x32 (m_SPP[0])[v64x4(m_SP[1])[i] % 32] = v64x4(m_SP[0])[i];
 			break;
-		case BITS_16:
+		case Bits16:
 			for (int i = 0; i < dstCount; ++i)
 				v16x16(m_SPP[0])[v64x4(m_SP[1])[i] % 16] = v64x4(m_SP[0])[i];
 			break;
-		case BITS_32:
+		case Bits32:
 			for (int i = 0; i < dstCount; ++i)
 				v32x8 (m_SPP[0])[v64x4(m_SP[1])[i] %  8] = v64x4(m_SP[0])[i];
 			break;
-		case BITS_64:
+		case Bits64:
 			for (int i = 0; i < dstCount; ++i)
 				v64x4 (m_SPP[0])[v64x4(m_SP[1])[i] %  4] = v64x4(m_SP[0])[i];
 			break;
@@ -657,19 +657,19 @@ void VM::xswizzle(uint8_t _type)
 	// for every index in the mask copy out the indexed value in the source
 	switch (width)
 	{
-	case BITS_8:
+	case Bits8:
 		for (int i = 0; i < count; ++i)
 			v8x32 (m_SPP[0])[i] = v8x32(m_SP[1]) [v8x32 (m_SP[0])[i] % count];
 		break;
-	case BITS_16:
+	case Bits16:
 		for (int i = 0; i < count; ++i)
 			v16x16(m_SPP[0])[i] = v16x16(m_SP[1])[v16x16(m_SP[0])[i] % count];
 		break;
-	case BITS_32:
+	case Bits32:
 		for (int i = 0; i < count; ++i)
 			v32x8 (m_SPP[0])[i] = v32x8(m_SP[1]) [v32x8 (m_SP[0])[i] % count];
 		break;
-	case BITS_64:
+	case Bits64:
 		for (int i = 0; i < count; ++i)
 			v64x4 (m_SPP[0])[i] = v64x4(m_SP[1]) [v64x4 (m_SP[0])[i] % count];
 		break;
@@ -688,28 +688,28 @@ void VM::xshuffle(uint8_t _type)
 	// for every index in the mask copy out the indexed value in one of the sources
 	switch (width)
 	{
-	case BITS_8:
+	case Bits8:
 		for (int i = 0; i < count; ++i)
 		{
 			int j = v8x32(m_SP[0]) [i];
 			v8x32 (m_SPP[0])[i] = j < count ? v8x32(m_SP[1]) [j] : v8x32 (m_SP[2])[(j - count) % count];
 		}
 		break;
-	case BITS_16:
+	case Bits16:
 		for (int i = 0; i < count; ++i)
 		{
 			int j = v16x16(m_SP[0])[i];
 			v16x16(m_SPP[0])[i] = j < count ? v16x16(m_SP[1])[j] : v16x16(m_SP[2])[(j - count) % count];
 		}
 		break;
-	case BITS_32:
+	case Bits32:
 		for (int i = 0; i < count; ++i)
 		{
 			int j = v32x8(m_SP[0]) [i];
 			v32x8 (m_SPP[0])[i] = j < count ? v32x8(m_SP[1]) [j] : v32x8 (m_SP[2])[(j - count) % count];
 		}
 		break;
-	case BITS_64:
+	case Bits64:
 		for (int i = 0; i < count; ++i)
 		{
 			int j = v64x4(m_SP[0]) [i];
