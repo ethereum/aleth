784fb207c3f564f811e59ba6c5fd9b844e2b2273
Arun Sethupat <sethupat.arun.4@gmail.com>
2017-08-02 20:07:53 +0530

    Issue: #4097 Dropped CPUID Support

--- cmake/FindCpuid.cmake
+++ cmake/FindCpuid.cmake
@@ -10,24 +10,24 @@
 #  CPUID_FOUND, If false, do not try to use cpuid.
 
 # only look in default directories
-find_path(
-	CPUID_INCLUDE_DIR
-	NAMES libcpuid/libcpuid.h
-	DOC "libcpuid include dir"
-	)
-
-find_library(
-	CPUID_LIBRARY
-	NAMES cpuid
-	DOC "libcpuid library"
-	)
-
-set(CPUID_INCLUDE_DIRS ${CPUID_INCLUDE_DIR})
-set(CPUID_LIBRARIES ${CPUID_LIBRARY})
-
-# handle the QUIETLY and REQUIRED arguments and set CPUID_FOUND to TRUE
-# if all listed variables are TRUE, hide their existence from configuration view
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Cpuid DEFAULT_MSG CPUID_LIBRARY CPUID_INCLUDE_DIR)
-mark_as_advanced (CPUID_INCLUDE_DIR CPUID_LIBRARY)
+#find_path(
+#	CPUID_INCLUDE_DIR
+#	NAMES libcpuid/libcpuid.h
+#	DOC "libcpuid include dir"
+#	)
+#
+#find_library(
+#	CPUID_LIBRARY
+#	NAMES CPUID
+#	DOC "libcpuid library"
+#	)
+#
+#set(CPUID_INCLUDE_DIRS ${CPUID_INCLUDE_DIR})
+#set(CPUID_LIBRARIES ${CPUID_LIBRARY})
+#
+## handle the QUIETLY and REQUIRED arguments and set CPUID_FOUND to TRUE
+## if all listed variables are TRUE, hide their existence from configuration view
+#include(FindPackageHandleStandardArgs)
+#find_package_handle_standard_args(Cpuid DEFAULT_MSG CPUID_LIBRARY CPUID_INCLUDE_DIR)
+#mark_as_advanced (CPUID_INCLUDE_DIR CPUID_LIBRARY)
 
