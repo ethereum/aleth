cndpost/cpp-ethereum

Protect m_hash and m_hashWithoutWork by a mutex

libethcore/BlockHeader.cpp
@@ -58,8 +58,8 @@ BlockHeader::BlockHeader(BlockHeader const& _other) :
 	m_author(_other.author()),
 	m_difficulty(_other.difficulty()),
 	m_seal(_other.seal()),
-	m_hash(_other.m_hash),
-	m_hashWithout(_other.m_hashWithout)
+	m_hash(_other.hashRawRead()),
+	m_hashWithout(_other.hashWithoutRawRead())
 {
 	assert(*this == _other);
 }
@@ -86,8 +86,13 @@ BlockHeader& BlockHeader::operator=(BlockHeader const& _other)
 		Guard l(x_seal);
 		m_seal = std::move(seal);
 	}
-	m_hash = _other.m_hash;
-	m_hashWithout = _other.m_hashWithout;
+	h256 hash = _other.hashRawRead();
+	h256 hashWithout = _other.hashWithoutRawRead();
+	{
+		Guard l(x_hash);
+		m_hash = std::move(hash);
+		m_hashWithout = std::move(hashWithout);
+	}
 	assert(*this == _other);
 	return *this;
 }
@@ -114,6 +119,7 @@ void BlockHeader::clear()
 h256 BlockHeader::hash(IncludeSeal _i) const
 {
 	h256 dummy;
+	Guard l(x_hash);
 	h256& memo = _i == WithSeal ? m_hash : _i == WithoutSeal ? m_hashWithout : dummy;
 	if (!memo)
 	{

libethcore/BlockHeader.h
@@ -130,7 +130,7 @@ public:
 	bool operator!=(BlockHeader const& _cmp) const { return !operator==(_cmp); }
 
 	void clear();
-	void noteDirty() const { m_hashWithout = m_hash = h256(); }
+	void noteDirty() const { Guard l(x_hash); m_hashWithout = m_hash = h256(); }
 	void populateFromParent(BlockHeader const& parent);
 
 	// TODO: pull out into abstract class Verifier.
@@ -178,6 +178,16 @@ private:
 		Guard l(x_seal);
 		return m_seal;
 	}
+	h256 hashRawRead() const
+	{
+		Guard l(x_hash);
+		return m_hash;
+	}
+	h256 hashWithoutRawRead() const
+	{
+		Guard l(x_hash);
+		return m_hashWithout;
+	}
 
 	h256 m_parentHash;
 	h256 m_sha3Uncles;
@@ -199,6 +209,7 @@ private:
 
 	mutable h256 m_hash;			///< (Memoised) SHA3 hash of the block header with seal.
 	mutable h256 m_hashWithout;		///< (Memoised) SHA3 hash of the block header without seal.
+	mutable Mutex x_hash;			///< A lock for both m_hash and m_hashWithout.
 };
 
 inline std::ostream& operator<<(std::ostream& _out, BlockHeader const& _bi)

ethereum/aleth

libethcore/BlockHeader.cpp
@@ -58,8 +58,8 @@ BlockHeader::BlockHeader(BlockHeader const& _other) :
 	m_author(_other.author()),
 	m_difficulty(_other.difficulty()),
 	m_seal(_other.seal()),
-	m_hash(_other.m_hash),
-	m_hashWithout(_other.m_hashWithout)
+	m_hash(_other.hashRawRead()),
+	m_hashWithout(_other.hashWithoutRawRead())
 {
 	assert(*this == _other);
 }
@@ -86,8 +86,13 @@ BlockHeader& BlockHeader::operator=(BlockHeader const& _other)
 		Guard l(m_sealLock);
 		m_seal = std::move(seal);
 	}
-	m_hash = _other.m_hash;
-	m_hashWithout = _other.m_hashWithout;
+	h256 hash = _other.hashRawRead();
+	h256 hashWithout = _other.hashWithoutRawRead();
+	{
+		Guard l(m_hashLock);
+		m_hash = std::move(hash);
+		m_hashWithout = std::move(hashWithout);
+	}
 	assert(*this == _other);
 	return *this;
 }
@@ -114,6 +119,7 @@ void BlockHeader::clear()
 h256 BlockHeader::hash(IncludeSeal _i) const
 {
 	h256 dummy;
+	Guard l(m_hashLock);
 	h256& memo = _i == WithSeal ? m_hash : _i == WithoutSeal ? m_hashWithout : dummy;
 	if (!memo)
 	{

libethcore/BlockHeader.h
@@ -130,7 +130,7 @@ public:
 	bool operator!=(BlockHeader const& _cmp) const { return !operator==(_cmp); }
 
 	void clear();
-	void noteDirty() const { m_hashWithout = m_hash = h256(); }
+	void noteDirty() const { Guard l(m_hashLock); m_hashWithout = m_hash = h256(); }
 	void populateFromParent(BlockHeader const& parent);
 
 	// TODO: pull out into abstract class Verifier.
@@ -178,6 +178,16 @@ private:
 		Guard l(m_sealLock);
 		return m_seal;
 	}
+	h256 hashRawRead() const
+	{
+		Guard l(m_hashLock);
+		return m_hash;
+	}
+	h256 hashWithoutRawRead() const
+	{
+		Guard l(m_hashLock);
+		return m_hashWithout;
+	}
 
 	h256 m_parentHash;
 	h256 m_sha3Uncles;
@@ -199,6 +209,7 @@ private:
 
 	mutable h256 m_hash;			///< (Memoised) SHA3 hash of the block header with seal.
 	mutable h256 m_hashWithout;		///< (Memoised) SHA3 hash of the block header without seal.
+	mutable Mutex m_hashLock;		///< A lock for both m_hash and m_hashWithout.
 };
 
 inline std::ostream& operator<<(std::ostream& _out, BlockHeader const& _bi)