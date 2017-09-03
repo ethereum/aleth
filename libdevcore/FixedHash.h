5813a17ed592f21c0e6afd921460c24bdbb2b6db
Paweł Bylica <chfast@gmail.com>
2017-09-03 22:04:28 +0200

    Replace boost::random with std::random

--- libdevcore/FixedHash.h
+++ libdevcore/FixedHash.h
@@ -26,8 +26,7 @@
 #include <array>
 #include <cstdint>
 #include <algorithm>
-#include <boost/random/random_device.hpp>
-#include <boost/random/uniform_int_distribution.hpp>
+#include <random>
 #include <boost/functional/hash.hpp>
 #include "CommonData.h"
 
@@ -38,7 +37,7 @@ namespace dev
 template <unsigned N> struct StaticLog2 { enum { result = 1 + StaticLog2<N/2>::result }; };
 template <> struct StaticLog2<1> { enum { result = 0 }; };
 
-extern boost::random_device s_fixedHashEngine;
+extern std::random_device s_fixedHashEngine;
 
 /// Fixed-size raw-byte array container type, with an API optimised for storing hashes.
 /// Transparently converts to/from the corresponding arithmetic type; this will
@@ -161,7 +160,7 @@ public:
 	void randomize(Engine& _eng)
 	{
 		for (auto& i: m_data)
-			i = (uint8_t)boost::random::uniform_int_distribution<uint16_t>(0, 255)(_eng);
+			i = (uint8_t)std::uniform_int_distribution<uint16_t>(0, 255)(_eng);
 	}
 
 	/// @returns a random valued object.
