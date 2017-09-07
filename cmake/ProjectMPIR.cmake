5d3fb68ab2c5f8560a9dd5a2c649ed8c74fee99b
Paweł Bylica <chfast@gmail.com>
2017-09-07 11:54:47 +0200

    Fix MPIR config

--- cmake/ProjectMPIR.cmake
+++ cmake/ProjectMPIR.cmake
@@ -3,7 +3,7 @@ ExternalProject_Add(mpir
     DOWNLOAD_NAME mpir-cmake.tar.gz
     DOWNLOAD_NO_PROGRESS TRUE
     URL https://github.com/chfast/mpir/archive/cmake.tar.gz
-    URL_HASH SHA256=2bf35ad7aa2b61bf4c2dcdab9aab48b2b4f9d6db8b937be4b0b5fe3b5efbdfa6
+    URL_HASH SHA256=d32ea73cb2d8115a8e59b244f96f29bad7ff03367162b660bae6495826811e06
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
         -DCMAKE_BUILD_TYPE=Release
         -DMPIR_GMP=On
