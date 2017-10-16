b22d90010a7ab9179947172d620a9bbcb827bb71
Paweł Bylica <chfast@gmail.com>
2017-10-16 12:54:26 +0200

    libff: Use upstream commit

--- cmake/ProjectLibFF.cmake
+++ cmake/ProjectLibFF.cmake
@@ -6,10 +6,10 @@ set(libff_inlcude_dir "${prefix}/include/libff")
 
 ExternalProject_Add(libff
     PREFIX "${prefix}"
-    DOWNLOAD_NAME libff-v2.tar.gz
+    DOWNLOAD_NAME libff-03b719a7.tar.gz
     DOWNLOAD_NO_PROGRESS TRUE
-    URL https://github.com/chfast/libff/archive/cpp-ethereum-v2.tar.gz
-    URL_HASH SHA256=f5a23730b9076938cf0b5ba1cb67f1f33873ca23dded58c52d7819c6b6c3ec91
+    URL https://github.com/scipr-lab/libff/archive/03b719a7c81757071f99fc60be1f7f7694e51390.tar.gz
+    URL_HASH SHA256=81b476089af43025c8f253cb1a9b5038a1c375baccffea402fa82042e608ab02
     CMAKE_ARGS
         -DCMAKE_BUILD_TYPE=Release
         -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
