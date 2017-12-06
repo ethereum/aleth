89e4af6578df49d07ea4a7c32c0a39c09f31fd5c
Paweł Bylica <chfast@gmail.com>
2017-12-06 16:14:05 +0100

    CMake: Clean up EthDependencies.cmake

--- cmake/UseMiniupnpc.cmake
+++ cmake/UseMiniupnpc.cmake
@@ -1,6 +1,5 @@
 function(eth_apply TARGET REQUIRED)
 	find_package (Miniupnpc 1.8.2013)
-	eth_show_dependency(MINIUPNPC miniupnpc)
 	if (MINIUPNPC AND MINIUPNPC_FOUND)
 		target_include_directories(${TARGET} SYSTEM PRIVATE ${MINIUPNPC_INCLUDE_DIRS})
 		target_link_libraries(${TARGET} ${MINIUPNPC_LIBRARIES})
