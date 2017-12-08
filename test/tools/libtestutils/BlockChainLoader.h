0ebe132b5e195a9f5c578af5e07d6fdd5b964456
Paweł Bylica <chfast@gmail.com>
2017-12-08 18:48:02 +0100

    Fixes of random issues reported by clang-tidy

--- test/tools/libtestutils/BlockChainLoader.h
+++ test/tools/libtestutils/BlockChainLoader.h
@@ -38,7 +38,7 @@ namespace test
 class BlockChainLoader
 {
 public:
-	BlockChainLoader(Json::Value const& _json, eth::Network _sealEngineNetwork = eth::Network::TransitionnetTest);
+	explicit BlockChainLoader(Json::Value const& _json, eth::Network _sealEngineNetwork = eth::Network::TransitionnetTest);
 	eth::BlockChain const& bc() const { return *m_bc; }
 	eth::State const& state() const { return m_block.state(); }	// TODO remove?
 	eth::Block const& block() const { return m_block; }
