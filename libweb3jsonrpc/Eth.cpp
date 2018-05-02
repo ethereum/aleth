43745915e629dfc6f9bc202aa66346710c200a6b
Nils-Erik Frantzell <nilserik.frantzell@gmail.com>
2018-05-02 11:27:12 -0700

    Fix bug where Eth::eth_pendingTransactions was returning a string instead of a JSON array of transaction objects. The fix is simple - change the ToJs() call in Eth::eth_pendingTransactions to a ToJson() call.

--- libweb3jsonrpc/Eth.cpp
+++ libweb3jsonrpc/Eth.cpp
@@ -131,7 +131,7 @@ string Eth::eth_getStorageRoot(string const& _address, string const& _blockNumbe
 	}
 }
 
-string Eth::eth_pendingTransactions()
+Json::Value Eth::eth_pendingTransactions()
 {
 	//Return list of transaction that being sent by local accounts
 	Transactions ours;
@@ -147,7 +147,7 @@ string Eth::eth_pendingTransactions()
 		}
 	}
 
-	return toJS(ours);
+	return toJson(ours);
 }
 
 string Eth::eth_getTransactionCount(string const& _address, string const& _blockNumber)
