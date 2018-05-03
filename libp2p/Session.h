acb444314336437c49b295390ffe95d130b09a7d
Andrei Maiboroda <andrei@ethereum.org>
2018-05-03 16:06:36 +0200

    Get rid of ThreadContext mechanism used in Session logging; use boost.log scoped attribute instead.

--- libp2p/Session.h
+++ libp2p/Session.h
@@ -165,6 +165,8 @@ private:
 	std::chrono::steady_clock::time_point m_lastReceived;	///< Time point of last message.
 
 	std::map<CapDesc, std::shared_ptr<Capability>> m_capabilities;	///< The peer's capability set.
+
+    std::string const m_logContext;
 };
 
 template <class PeerCap>
