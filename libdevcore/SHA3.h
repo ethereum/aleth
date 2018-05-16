05cb303d1703f0cf7c2f4d7c7ca4d0bba830407f
Paweł Bylica <chfast@gmail.com>
2018-05-16 10:22:04 +0200

    More noexcept in keccak interface

--- libdevcore/SHA3.h
+++ libdevcore/SHA3.h
@@ -47,15 +47,34 @@ inline h256 sha3(bytesConstRef _input) noexcept
     return ret;
 }
 
-inline SecureFixedHash<32> sha3Secure(bytesConstRef _input) { SecureFixedHash<32> ret; sha3(_input, ret.writable().ref()); return ret; }
+inline SecureFixedHash<32> sha3Secure(bytesConstRef _input) noexcept
+{
+    SecureFixedHash<32> ret;
+    sha3(_input, ret.writable().ref());
+    return ret;
+}
 
 /// Calculate SHA3-256 hash of the given input, returning as a 256-bit hash.
-inline h256 sha3(bytes const& _input) { return sha3(bytesConstRef(&_input)); }
-inline SecureFixedHash<32> sha3Secure(bytes const& _input) { return sha3Secure(bytesConstRef(&_input)); }
+inline h256 sha3(bytes const& _input) noexcept
+{
+    return sha3(bytesConstRef(&_input));
+}
+
+inline SecureFixedHash<32> sha3Secure(bytes const& _input) noexcept
+{
+    return sha3Secure(bytesConstRef(&_input));
+}
 
 /// Calculate SHA3-256 hash of the given input (presented as a binary-filled string), returning as a 256-bit hash.
-inline h256 sha3(std::string const& _input) { return sha3(bytesConstRef(_input)); }
-inline SecureFixedHash<32> sha3Secure(std::string const& _input) { return sha3Secure(bytesConstRef(_input)); }
+inline h256 sha3(std::string const& _input) noexcept
+{
+    return sha3(bytesConstRef(_input));
+}
+
+inline SecureFixedHash<32> sha3Secure(std::string const& _input) noexcept
+{
+    return sha3Secure(bytesConstRef(_input));
+}
 
 /// Keccak hash variant optimized for hashing 256-bit hashes.
 inline h256 sha3(h256 const& _input) noexcept
@@ -65,20 +84,52 @@ inline h256 sha3(h256 const& _input) noexcept
 }
 
 /// Calculate SHA3-256 hash of the given input (presented as a FixedHash), returns a 256-bit hash.
-template<unsigned N> inline h256 sha3(FixedHash<N> const& _input) { return sha3(_input.ref()); }
-template<unsigned N> inline SecureFixedHash<32> sha3Secure(FixedHash<N> const& _input) { return sha3Secure(_input.ref()); }
+template <unsigned N>
+inline h256 sha3(FixedHash<N> const& _input) noexcept
+{
+    return sha3(_input.ref());
+}
+
+template <unsigned N>
+inline SecureFixedHash<32> sha3Secure(FixedHash<N> const& _input) noexcept
+{
+    return sha3Secure(_input.ref());
+}
 
 /// Fully secure variants are equivalent for sha3 and sha3Secure.
-inline SecureFixedHash<32> sha3(bytesSec const& _input) { return sha3Secure(_input.ref()); }
-inline SecureFixedHash<32> sha3Secure(bytesSec const& _input) { return sha3Secure(_input.ref()); }
-template<unsigned N> inline SecureFixedHash<32> sha3(SecureFixedHash<N> const& _input) { return sha3Secure(_input.ref()); }
-template<unsigned N> inline SecureFixedHash<32> sha3Secure(SecureFixedHash<N> const& _input) { return sha3Secure(_input.ref()); }
+inline SecureFixedHash<32> sha3(bytesSec const& _input) noexcept
+{
+    return sha3Secure(_input.ref());
+}
+
+inline SecureFixedHash<32> sha3Secure(bytesSec const& _input) noexcept
+{
+    return sha3Secure(_input.ref());
+}
+
+template <unsigned N>
+inline SecureFixedHash<32> sha3(SecureFixedHash<N> const& _input) noexcept
+{
+    return sha3Secure(_input.ref());
+}
+
+template <unsigned N>
+inline SecureFixedHash<32> sha3Secure(SecureFixedHash<N> const& _input) noexcept
+{
+    return sha3Secure(_input.ref());
+}
 
 /// Calculate SHA3-256 hash of the given input, possibly interpreting it as nibbles, and return the hash as a string filled with binary data.
-inline std::string sha3(std::string const& _input, bool _isNibbles) { return asString((_isNibbles ? sha3(fromHex(_input)) : sha3(bytesConstRef(&_input))).asBytes()); }
+inline std::string sha3(std::string const& _input, bool _isNibbles)
+{
+    return asString((_isNibbles ? sha3(fromHex(_input)) : sha3(bytesConstRef(&_input))).asBytes());
+}
 
 /// Calculate SHA3-256 MAC
-inline void sha3mac(bytesConstRef _secret, bytesConstRef _plain, bytesRef _output) { sha3(_secret.toBytes() + _plain.toBytes()).ref().populate(_output); }
+inline void sha3mac(bytesConstRef _secret, bytesConstRef _plain, bytesRef _output)
+{
+    sha3(_secret.toBytes() + _plain.toBytes()).ref().populate(_output);
+}
 
 extern h256 const EmptySHA3;
 
