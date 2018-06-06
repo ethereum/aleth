xcheng4git/cpp-ethereum

Fix const style

ethkey/main.cpp
@@ -37,7 +37,7 @@ namespace po = boost::program_options;
 
 void version()
 {
-    const auto* buildinfo = eth_get_buildinfo();
+    auto const* const buildinfo = eth_get_buildinfo();
     cout << "ethkey " << buildinfo->project_version << "\nBuild: " << buildinfo->system_name << "/"
          << buildinfo->build_type << endl;
     exit(0);

ethvm/main.cpp
@@ -58,7 +58,7 @@ int64_t maxBlockGasLimit()
 
 void version()
 {
-    const auto* buildinfo = eth_get_buildinfo();
+    auto const* const buildinfo = eth_get_buildinfo();
     cout << "ethvm " << buildinfo->project_version << "\n";
     cout << "Build: " << buildinfo->system_name << "/" << buildinfo->build_type << "\n";
     exit(0);

libwebthree/WebThree.cpp
@@ -56,7 +56,7 @@ WebThreeDirect::WebThreeDirect(std::string const& _clientVersion,
             m_ethereum.reset(new eth::Client(_params, (int)_params.networkID, &m_net, shared_ptr<GasPricer>(), _dbPath, _snapshotPath, _we));
         m_ethereum->startWorking();
 
-        const auto* buildinfo = eth_get_buildinfo();
+        auto const* const buildinfo = eth_get_buildinfo();
         m_ethereum->setExtraData(rlpList(0, string{buildinfo->project_version}.substr(0, 5) + "++" +
                                                 string{buildinfo->git_commit_hash}.substr(0, 4) +
                                                 string{buildinfo->build_type}.substr(0, 1) +
@@ -82,7 +82,7 @@ WebThreeDirect::~WebThreeDirect()
 
 std::string WebThreeDirect::composeClientVersion(std::string const& _client)
 {
-    const auto* buildinfo = eth_get_buildinfo();
+    auto const* const buildinfo = eth_get_buildinfo();
     return _client + "/" + buildinfo->project_version + "/" + buildinfo->system_name + "/" +
            buildinfo->compiler_id + buildinfo->compiler_version + "/" + buildinfo->build_type;
 }

ethereum/aleth

ethkey/main.cpp
@@ -37,7 +37,7 @@ namespace po = boost::program_options;
 
 void version()
 {
-    const auto* buildinfo = aleth_get_buildinfo();
+    auto const* const buildinfo = aleth_get_buildinfo();
     cout << "ethkey " << buildinfo->project_version << "\nBuild: " << buildinfo->system_name << "/"
          << buildinfo->build_type << endl;
     exit(0);

ethvm/main.cpp
@@ -58,7 +58,7 @@ int64_t maxBlockGasLimit()
 
 void version()
 {
-    const auto* buildinfo = aleth_get_buildinfo();
+    auto const* const buildinfo = aleth_get_buildinfo();
     cout << "ethvm " << buildinfo->project_version << "\n";
     cout << "Build: " << buildinfo->system_name << "/" << buildinfo->build_type << "\n";
     exit(0);

libwebthree/WebThree.cpp
@@ -56,7 +56,7 @@ WebThreeDirect::WebThreeDirect(std::string const& _clientVersion,
             m_ethereum.reset(new eth::Client(_params, (int)_params.networkID, &m_net, shared_ptr<GasPricer>(), _dbPath, _snapshotPath, _we));
         m_ethereum->startWorking();
 
-        const auto* buildinfo = aleth_get_buildinfo();
+        auto const* const buildinfo = aleth_get_buildinfo();
         m_ethereum->setExtraData(rlpList(0, string{buildinfo->project_version}.substr(0, 5) + "++" +
                                                 string{buildinfo->git_commit_hash}.substr(0, 4) +
                                                 string{buildinfo->build_type}.substr(0, 1) +
@@ -82,7 +82,7 @@ WebThreeDirect::~WebThreeDirect()
 
 std::string WebThreeDirect::composeClientVersion(std::string const& _client)
 {
-    const auto* buildinfo = aleth_get_buildinfo();
+    auto const* const buildinfo = aleth_get_buildinfo();
     return _client + "/" + buildinfo->project_version + "/" + buildinfo->system_name + "/" +
            buildinfo->compiler_id + buildinfo->compiler_version + "/" + buildinfo->build_type;
 }