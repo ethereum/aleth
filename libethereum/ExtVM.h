a21ead75edac8014e8649edf2b145d620be68ddb
Andrei Maiboroda <andrei@ethereum.org>
2018-07-30 16:25:53 +0200

    Tabs to spaces in ExtVM.h, Instruction.cpp, LegacyVM.cpp

--- libethereum/ExtVM.h
+++ libethereum/ExtVM.h
@@ -1,18 +1,18 @@
 /*
-	This file is part of cpp-ethereum.
+    This file is part of cpp-ethereum.
 
-	cpp-ethereum is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+    cpp-ethereum is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-	cpp-ethereum is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    cpp-ethereum is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
@@ -54,7 +54,7 @@ public:
         assert(m_s.addressInUse(_myAddress));
     }
 
-	/// Read storage location.
+    /// Read storage location.
     u256 store(u256 _n) final { return m_s.storage(myAddress, _n); }
 
     /// Write a value in storage.
@@ -72,22 +72,22 @@ public:
     /// Create a new contract.
     CreateResult create(u256 _endowment, u256& io_gas, bytesConstRef _code, Instruction _op, u256 _salt, OnOpFunc const& _onOp = {}) final;
 
-	/// Create a new message call.
-	CallResult call(CallParameters& _params) final;
+    /// Create a new message call.
+    CallResult call(CallParameters& _params) final;
 
-	/// Read address's balance.
+    /// Read address's balance.
     u256 balance(Address _a) final { return m_s.balance(_a); }
 
     /// Does the account exist?
     bool exists(Address _a) final
     {
-		if (evmSchedule().emptinessIsNonexistence())
-			return m_s.accountNonemptyAndExisting(_a);
-		else
-			return m_s.addressInUse(_a);
-	}
+        if (evmSchedule().emptinessIsNonexistence())
+            return m_s.accountNonemptyAndExisting(_a);
+        else
+            return m_s.addressInUse(_a);
+    }
 
-	/// Suicide the associated contract to the given address.
+    /// Suicide the associated contract to the given address.
     void suicide(Address _a) final;
 
     /// Return the EVM gas-price schedule for this execution context.
@@ -98,12 +98,12 @@ public:
 
     State const& state() const { return m_s; }
 
-	/// Hash of a block if within the last 256 blocks, or h256() otherwise.
+    /// Hash of a block if within the last 256 blocks, or h256() otherwise.
     h256 blockHash(u256 _number) final;
 
 private:
-	State& m_s;  ///< A reference to the base state.
-	SealEngineFace const& m_sealEngine;
+    State& m_s;  ///< A reference to the base state.
+    SealEngineFace const& m_sealEngine;
 };
 
 }
