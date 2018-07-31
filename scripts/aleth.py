400099926c30d69de9001f584521dd1b6cca643d
Paweł Bylica <chfast@gmail.com>
2018-07-31 12:41:28 +0200

    aleth.py: Allow specifying HTTP address for the proxy

--- scripts/aleth.py
+++ scripts/aleth.py
@@ -5,13 +5,13 @@ import subprocess
 import sys
 import os
 
-from jsonrpcproxy import run_daemon
+from jsonrpcproxy import run_daemon, DEFAULT_PROXY_URL
 
 DEFAULT_ALETH_EXEC = '/usr/bin/aleth'
 
 parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument('--aleth-exec', default=DEFAULT_ALETH_EXEC)
-parser.add_argument('--rpc', action='store_true')
+parser.add_argument('--rpc', nargs='?', const=DEFAULT_PROXY_URL, default=False)
 wrapper_args, aleth_args = parser.parse_known_args()
 aleth_exec = wrapper_args.aleth_exec
 
@@ -22,7 +22,7 @@ if not os.path.isfile(aleth_exec):
 
 try:
     if wrapper_args.rpc:
-        run_daemon()
+        run_daemon(wrapper_args.rpc)
     p = subprocess.run([aleth_exec] + aleth_args)
     exit(p.returncode)
 except KeyboardInterrupt:
