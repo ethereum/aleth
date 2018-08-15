24d164fe93eb6f1a614772963210b35a7398728d
Tien Do <tiendq@gmail.com>
2018-08-15 21:19:13 +0700

    Add miner options using boost::program_options and move all MinerCLI implementations to cpp file

--- aleth/MinerAux.h
+++ aleth/MinerAux.h
@@ -1,5 +1,3 @@
-#pragma once
-
 /*
     This file is part of cpp-ethereum.
 
@@ -20,28 +18,14 @@
  * CLI module for mining.
  */
 
-#include <boost/algorithm/string/case_conv.hpp>
-#include <libdevcore/CommonJS.h>
-#include <libethcore/BasicAuthority.h>
+#pragma once
+
+#include <boost/program_options.hpp>
+#include <boost/program_options/options_description.hpp>
+
 #include <libethcore/Exceptions.h>
-#include <libethashseal/Ethash.h>
-#include <libethashseal/EthashCPUMiner.h>
 
-using namespace std;
-using namespace dev;
-using namespace dev::eth;
-
-bool isTrue(std::string const& _m)
-{
-    return _m == "on" || _m == "yes" || _m == "true" || _m == "1";
-}
-
-bool isFalse(std::string const& _m)
-{
-    return _m == "off" || _m == "no" || _m == "false" || _m == "0";
-}
-
-class BadArgument: public Exception {};
+class BadArgument: public dev::Exception {};
 
 class MinerCLI
 {
@@ -52,144 +36,19 @@ public:
         Benchmark
     };
 
-    explicit MinerCLI(OperationMode _mode = OperationMode::None): mode(_mode)
+    explicit MinerCLI(OperationMode _mode = OperationMode::None) : mode(_mode)
     {
     }
 
-    bool interpretOption(size_t& i, vector<string> const& argv)
-    {
-        size_t argc = argv.size();
-        string arg = argv[i];
-        if (arg == "--benchmark-warmup" && i + 1 < argc)
-            try {
-                m_benchmarkWarmup = stol(argv[++i]);
-            }
-            catch (...)
-            {
-                cerr << "Bad " << arg << " option: " << argv[i] << endl;
-                BOOST_THROW_EXCEPTION(BadArgument());
-            }
-        else if (arg == "--benchmark-trial" && i + 1 < argc)
-            try {
-                m_benchmarkTrial = stol(argv[++i]);
-            }
-            catch (...)
-            {
-                cerr << "Bad " << arg << " option: " << argv[i] << endl;
-                BOOST_THROW_EXCEPTION(BadArgument());
-            }
-        else if (arg == "--benchmark-trials" && i + 1 < argc)
-            try {
-                m_benchmarkTrials = stol(argv[++i]);
-            }
-            catch (...)
-            {
-                cerr << "Bad " << arg << " option: " << argv[i] << endl;
-                BOOST_THROW_EXCEPTION(BadArgument());
-            }
-        else if (arg == "-C" || arg == "--cpu")
-            m_minerType = "cpu";
-        else if (arg == "--current-block" && i + 1 < argc)
-            m_currentBlock = stol(argv[++i]);
-        else if (arg == "-M" || arg == "--benchmark")
-            mode = OperationMode::Benchmark;
-        else if ((arg == "-t" || arg == "--mining-threads") && i + 1 < argc)
-        {
-            try {
-                m_miningThreads = stol(argv[++i]);
-            }
-            catch (...)
-            {
-                cerr << "Bad " << arg << " option: " << argv[i] << endl;
-                BOOST_THROW_EXCEPTION(BadArgument());
-            }
-        }
-        else
-            return false;
-        return true;
-    }
-
-    void execute()
-    {
-        if (m_minerType == "cpu")
-            EthashCPUMiner::setNumInstances(m_miningThreads);
-        else if (mode == OperationMode::Benchmark)
-            doBenchmark(m_minerType, m_benchmarkWarmup, m_benchmarkTrial, m_benchmarkTrials);
-    }
-
-    static void streamHelp(ostream& _out)
-    {
-        _out << "BENCHMARKING MODE:\n"
-             << "  -M,--benchmark               Benchmark for mining and exit\n"
-             << "  --benchmark-warmup <seconds> Set the duration of warmup for the benchmark tests "
-                "(default: 3)\n"
-             << "  --benchmark-trial <seconds>  Set the duration for each trial for the benchmark "
-                "tests (default: 3)\n"
-             << "  --benchmark-trials <n>       Set the number of trials for the benchmark tests "
-                "(default: 5)\n\n"
-
-             << "MINING CONFIGURATION:\n"
-             << "  -C,--cpu                   When mining, use the CPU\n"
-             << "  -t, --mining-threads <n>   Limit number of CPU/GPU miners to n (default: use "
-                "everything available on selected platform)\n"
-             << "  --current-block            Let the miner know the current block number at "
-                "configuration time. Will help determine DAG size and required GPU memory\n"
-             << "  --disable-submit-hashrate  When mining, don't submit hashrate to node\n\n";
-    }
+    void execute();
 
     std::string minerType() const { return m_minerType; }
 
+    void interpretOptions(boost::program_options::variables_map const& _options);
+    static boost::program_options::options_description createProgramOptions(unsigned int _lineLength);
+
 private:
-    void doBenchmark(std::string const& _m, unsigned _warmupDuration = 15, unsigned _trialDuration = 3, unsigned _trials = 5)
-    {
-        BlockHeader genesis;
-        genesis.setDifficulty(1 << 18);
-        cdebug << Ethash::boundary(genesis);
-
-        GenericFarm<EthashProofOfWork> f;
-        map<string, GenericFarm<EthashProofOfWork>::SealerDescriptor> sealers;
-        sealers["cpu"] = GenericFarm<EthashProofOfWork>::SealerDescriptor{&EthashCPUMiner::instances, [](GenericMiner<EthashProofOfWork>::ConstructionInfo ci){ return new EthashCPUMiner(ci); }};
-        f.setSealers(sealers);
-        f.onSolutionFound([&](EthashProofOfWork::Solution) { return false; });
-
-        string platformInfo = EthashCPUMiner::platformInfo();
-        cout << "Benchmarking on platform: " << platformInfo << endl;
-
-        genesis.setDifficulty(u256(1) << 63);
-        f.setWork(genesis);
-        f.start(_m);
-
-        map<u256, WorkingProgress> results;
-        u256 mean = 0;
-        u256 innerMean = 0;
-        for (unsigned i = 0; i <= _trials; ++i)
-        {
-            if (!i)
-                cout << "Warming up..." << endl;
-            else
-                cout << "Trial " << i << "... " << flush;
-            this_thread::sleep_for(chrono::seconds(i ? _trialDuration : _warmupDuration));
-
-            auto mp = f.miningProgress();
-            f.resetMiningProgress();
-            if (!i)
-                continue;
-            auto rate = mp.rate();
-
-            cout << rate << endl;
-            results[rate] = mp;
-            mean += rate;
-        }
-        f.stop();
-        int j = -1;
-        for (auto const& r: results)
-            if (++j > 0 && j < (int)_trials - 1)
-                innerMean += r.second.rate();
-        innerMean /= (_trials - 2);
-        cout << "min/mean/max: " << results.begin()->second.rate() << "/" << (mean / _trials) << "/" << results.rbegin()->second.rate() << " H/s" << endl;
-        cout << "inner mean: " << innerMean << " H/s" << endl;
-        exit(0);
-    }
+    void doBenchmark(std::string const& _m, unsigned _warmupDuration = 15, unsigned _trialDuration = 3, unsigned _trials = 5);
 
     /// Operating mode.
     OperationMode mode;
