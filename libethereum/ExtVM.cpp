20ef95b57054741f4ae1e11404a24e72bcbdbb94
Andrei Maiboroda <andrei@ethereum.org>
2018-08-27 20:27:18 +0200

    Move BLOCKHASH refactoring (EIP-210) implementatio to experimental fork

--- libethereum/ExtVM.cpp
+++ libethereum/ExtVM.cpp
@@ -190,7 +190,7 @@ h256 ExtVM::blockHash(u256 _number)
     if (_number >= currentNumber || _number < (std::max<u256>(256, currentNumber) - 256))
         return h256();
 
-    if (currentNumber < m_sealEngine.chainParams().constantinopleForkBlock + 256)
+    if (currentNumber < m_sealEngine.chainParams().experimentalForkBlock + 256)
     {
         h256 const parentHash = envInfo().header().parentHash();
         h256s const lastHashes = envInfo().lastHashes().precedingHashes(parentHash);
