6854af7a3c49cf0d255bb671787a0573cfe8dcda
Andrei Maiboroda <andrei@ethereum.org>
2018-08-29 18:55:45 +0200

    Convert tabs to spaces

--- libethereum/Account.cpp
+++ libethereum/Account.cpp
@@ -1,18 +1,18 @@
 /*
-	This file is part of cpp-ethereum.
+    This file is part of cpp-ethereum.
 
-	cpp-ethereum is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+    cpp-ethereum is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-	cpp-ethereum is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    cpp-ethereum is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
 */
 /** @file Account.cpp
  * @author Gav Wood <i@gavwood.com>
@@ -34,9 +34,9 @@ namespace fs = boost::filesystem;
 
 void Account::setCode(bytes&& _code)
 {
-	m_codeCache = std::move(_code);
-	m_hasNewCode = true;
-	m_codeHash = sha3(m_codeCache);
+    m_codeCache = std::move(_code);
+    m_hasNewCode = true;
+    m_codeHash = sha3(m_codeCache);
 }
 
 namespace js = json_spirit;
@@ -46,42 +46,42 @@ namespace
 
 uint64_t toUnsigned(js::mValue const& _v)
 {
-	switch (_v.type())
-	{
-	case js::int_type: return _v.get_uint64();
-	case js::str_type: return fromBigEndian<uint64_t>(fromHex(_v.get_str()));
-	default: return 0;
-	}
+    switch (_v.type())
+    {
+    case js::int_type: return _v.get_uint64();
+    case js::str_type: return fromBigEndian<uint64_t>(fromHex(_v.get_str()));
+    default: return 0;
+    }
 }
 
 PrecompiledContract createPrecompiledContract(js::mObject const& _precompiled)
 {
     auto n = _precompiled.at("name").get_str();
     try
-	{
-		u256 startingBlock = 0;
-		if (_precompiled.count("startingBlock"))
+    {
+        u256 startingBlock = 0;
+        if (_precompiled.count("startingBlock"))
             startingBlock = u256(_precompiled.at("startingBlock").get_str());
 
         if (!_precompiled.count("linear"))
-			return PrecompiledContract(PrecompiledRegistrar::pricer(n), PrecompiledRegistrar::executor(n), startingBlock);
+            return PrecompiledContract(PrecompiledRegistrar::pricer(n), PrecompiledRegistrar::executor(n), startingBlock);
 
         auto const& l = _precompiled.at("linear").get_obj();
         unsigned base = toUnsigned(l.at("base"));
         unsigned word = toUnsigned(l.at("word"));
         return PrecompiledContract(base, word, PrecompiledRegistrar::executor(n), startingBlock);
-	}
-	catch (PricerNotFound const&)
-	{
-		cwarn << "Couldn't create a precompiled contract account. Missing a pricer called:" << n;
-		throw;
-	}
-	catch (ExecutorNotFound const&)
-	{
-		// Oh dear - missing a plugin?
-		cwarn << "Couldn't create a precompiled contract account. Missing an executor called:" << n;
-		throw;
-	}
+    }
+    catch (PricerNotFound const&)
+    {
+        cwarn << "Couldn't create a precompiled contract account. Missing a pricer called:" << n;
+        throw;
+    }
+    catch (ExecutorNotFound const&)
+    {
+        // Oh dear - missing a plugin?
+        cwarn << "Couldn't create a precompiled contract account. Missing an executor called:" << n;
+        throw;
+    }
 }
 }
 
--- libethereum/Account.h
+++ libethereum/Account.h
@@ -1,18 +1,18 @@
 /*
-	This file is part of cpp-ethereum.
+    This file is part of cpp-ethereum.
 
-	cpp-ethereum is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+    cpp-ethereum is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-	cpp-ethereum is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    cpp-ethereum is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
 */
 /** @file Account.h
  * @author Gav Wood <i@gavwood.com>
@@ -67,179 +67,179 @@ namespace eth
 class Account
 {
 public:
-	/// Changedness of account to create.
-	enum Changedness
-	{
-		/// Account starts as though it has been changed.
-		Changed,
-		/// Account starts as though it has not been changed.
-		Unchanged
-	};
+    /// Changedness of account to create.
+    enum Changedness
+    {
+        /// Account starts as though it has been changed.
+        Changed,
+        /// Account starts as though it has not been changed.
+        Unchanged
+    };
 
-	/// Construct a dead Account.
-	Account() {}
+    /// Construct a dead Account.
+    Account() {}
 
-	/// Construct an alive Account, with given endowment, for either a normal (non-contract) account or for a
-	/// contract account in the
-	/// conception phase, where the code is not yet known.
-	Account(u256 _nonce, u256 _balance, Changedness _c = Changed): m_isAlive(true), m_isUnchanged(_c == Unchanged), m_nonce(_nonce), m_balance(_balance) {}
+    /// Construct an alive Account, with given endowment, for either a normal (non-contract) account or for a
+    /// contract account in the
+    /// conception phase, where the code is not yet known.
+    Account(u256 _nonce, u256 _balance, Changedness _c = Changed): m_isAlive(true), m_isUnchanged(_c == Unchanged), m_nonce(_nonce), m_balance(_balance) {}
 
-	/// Explicit constructor for wierd cases of construction or a contract account.
-	Account(u256 _nonce, u256 _balance, h256 _contractRoot, h256 _codeHash, Changedness _c): m_isAlive(true), m_isUnchanged(_c == Unchanged), m_nonce(_nonce), m_balance(_balance), m_storageRoot(_contractRoot), m_codeHash(_codeHash) { assert(_contractRoot); }
-
-
-	/// Kill this account. Useful for the suicide opcode. Following this call, isAlive() returns false.
-	void kill() { m_isAlive = false; m_storageOverlay.clear(); m_codeHash = EmptySHA3; m_storageRoot = EmptyTrie; m_balance = 0; m_nonce = 0; changed(); }
-
-	/// @returns true iff this object represents an account in the state. Returns false if this object
-	/// represents an account that should no longer exist in the trie (an account that never existed or was
-	/// suicided).
-	bool isAlive() const { return m_isAlive; }
-
-	/// @returns true if the account is unchanged from creation.
-	bool isDirty() const { return !m_isUnchanged; }
-
-	void untouch() { m_isUnchanged = true; }
-
-	/// @returns true if the nonce, balance and code is zero / empty. Code is considered empty
-	/// during creation phase.
-	bool isEmpty() const { return nonce() == 0 && balance() == 0 && codeHash() == EmptySHA3; }
-
-	/// @returns the balance of this account.
-	u256 const& balance() const { return m_balance; }
-
-	/// Increments the balance of this account by the given amount.
-	void addBalance(u256 _value) { m_balance += _value; changed(); }
-
-	/// @returns the nonce of the account.
-	u256 nonce() const { return m_nonce; }
-
-	/// Increment the nonce of the account by one.
-	void incNonce() { ++m_nonce; changed(); }
-
-	/// Set nonce to a new value. This is used when reverting changes made to
-	/// the account.
-	void setNonce(u256 const& _nonce) { m_nonce = _nonce; changed(); }
+    /// Explicit constructor for wierd cases of construction or a contract account.
+    Account(u256 _nonce, u256 _balance, h256 _contractRoot, h256 _codeHash, Changedness _c): m_isAlive(true), m_isUnchanged(_c == Unchanged), m_nonce(_nonce), m_balance(_balance), m_storageRoot(_contractRoot), m_codeHash(_codeHash) { assert(_contractRoot); }
 
 
-	/// @returns the root of the trie (whose nodes are stored in the state db externally to this class)
-	/// which encodes the base-state of the account's storage (upon which the storage is overlaid).
-	h256 baseRoot() const { assert(m_storageRoot); return m_storageRoot; }
+    /// Kill this account. Useful for the suicide opcode. Following this call, isAlive() returns false.
+    void kill() { m_isAlive = false; m_storageOverlay.clear(); m_codeHash = EmptySHA3; m_storageRoot = EmptyTrie; m_balance = 0; m_nonce = 0; changed(); }
 
-	/// @returns the storage overlay as a simple hash map.
-	std::unordered_map<u256, u256> const& storageOverlay() const { return m_storageOverlay; }
+    /// @returns true iff this object represents an account in the state. Returns false if this object
+    /// represents an account that should no longer exist in the trie (an account that never existed or was
+    /// suicided).
+    bool isAlive() const { return m_isAlive; }
 
-	/// Set a key/value pair in the account's storage. This actually goes into the overlay, for committing
-	/// to the trie later.
-	void setStorage(u256 _p, u256 _v) { m_storageOverlay[_p] = _v; changed(); }
+    /// @returns true if the account is unchanged from creation.
+    bool isDirty() const { return !m_isUnchanged; }
 
-	/// Empty the storage.  Used when a contract is overwritten.
-	void clearStorage() { m_storageOverlay.clear(); m_storageRoot = EmptyTrie; changed(); }
+    void untouch() { m_isUnchanged = true; }
 
-	/// Set the storage root.  Used when clearStorage() is reverted.
-	void setStorageRoot(h256 const& _root) { m_storageOverlay.clear(); m_storageRoot = _root; changed(); }
+    /// @returns true if the nonce, balance and code is zero / empty. Code is considered empty
+    /// during creation phase.
+    bool isEmpty() const { return nonce() == 0 && balance() == 0 && codeHash() == EmptySHA3; }
 
-	/// Set a key/value pair in the account's storage to a value that is already present inside the
-	/// database.
-	void setStorageCache(u256 _p, u256 _v) const { const_cast<decltype(m_storageOverlay)&>(m_storageOverlay)[_p] = _v; }
+    /// @returns the balance of this account.
+    u256 const& balance() const { return m_balance; }
 
-	/// @returns the hash of the account's code.
-	h256 codeHash() const { return m_codeHash; }
+    /// Increments the balance of this account by the given amount.
+    void addBalance(u256 _value) { m_balance += _value; changed(); }
 
-	bool hasNewCode() const { return m_hasNewCode; }
+    /// @returns the nonce of the account.
+    u256 nonce() const { return m_nonce; }
 
-	/// Sets the code of the account. Used by "create" messages.
-	void setCode(bytes&& _code);
+    /// Increment the nonce of the account by one.
+    void incNonce() { ++m_nonce; changed(); }
 
-	/// Reset the code set by previous CREATE message.
-	void resetCode() { m_codeCache.clear(); m_hasNewCode = false; m_codeHash = EmptySHA3; }
+    /// Set nonce to a new value. This is used when reverting changes made to
+    /// the account.
+    void setNonce(u256 const& _nonce) { m_nonce = _nonce; changed(); }
 
-	/// Specify to the object what the actual code is for the account. @a _code must have a SHA3 equal to
-	/// codeHash() and must only be called when isFreshCode() returns false.
-	void noteCode(bytesConstRef _code) { assert(sha3(_code) == m_codeHash); m_codeCache = _code.toBytes(); }
 
-	/// @returns the account's code.
-	bytes const& code() const { return m_codeCache; }
+    /// @returns the root of the trie (whose nodes are stored in the state db externally to this class)
+    /// which encodes the base-state of the account's storage (upon which the storage is overlaid).
+    h256 baseRoot() const { assert(m_storageRoot); return m_storageRoot; }
+
+    /// @returns the storage overlay as a simple hash map.
+    std::unordered_map<u256, u256> const& storageOverlay() const { return m_storageOverlay; }
+
+    /// Set a key/value pair in the account's storage. This actually goes into the overlay, for committing
+    /// to the trie later.
+    void setStorage(u256 _p, u256 _v) { m_storageOverlay[_p] = _v; changed(); }
+
+    /// Empty the storage.  Used when a contract is overwritten.
+    void clearStorage() { m_storageOverlay.clear(); m_storageRoot = EmptyTrie; changed(); }
+
+    /// Set the storage root.  Used when clearStorage() is reverted.
+    void setStorageRoot(h256 const& _root) { m_storageOverlay.clear(); m_storageRoot = _root; changed(); }
+
+    /// Set a key/value pair in the account's storage to a value that is already present inside the
+    /// database.
+    void setStorageCache(u256 _p, u256 _v) const { const_cast<decltype(m_storageOverlay)&>(m_storageOverlay)[_p] = _v; }
+
+    /// @returns the hash of the account's code.
+    h256 codeHash() const { return m_codeHash; }
+
+    bool hasNewCode() const { return m_hasNewCode; }
+
+    /// Sets the code of the account. Used by "create" messages.
+    void setCode(bytes&& _code);
+
+    /// Reset the code set by previous CREATE message.
+    void resetCode() { m_codeCache.clear(); m_hasNewCode = false; m_codeHash = EmptySHA3; }
+
+    /// Specify to the object what the actual code is for the account. @a _code must have a SHA3 equal to
+    /// codeHash() and must only be called when isFreshCode() returns false.
+    void noteCode(bytesConstRef _code) { assert(sha3(_code) == m_codeHash); m_codeCache = _code.toBytes(); }
+
+    /// @returns the account's code.
+    bytes const& code() const { return m_codeCache; }
 
 private:
-	/// Note that we've altered the account.
-	void changed() { m_isUnchanged = false; }
+    /// Note that we've altered the account.
+    void changed() { m_isUnchanged = false; }
 
-	/// Is this account existant? If not, it represents a deleted account.
-	bool m_isAlive = false;
+    /// Is this account existant? If not, it represents a deleted account.
+    bool m_isAlive = false;
 
-	/// True if we've not made any alteration to the account having been given it's properties directly.
-	bool m_isUnchanged = false;
+    /// True if we've not made any alteration to the account having been given it's properties directly.
+    bool m_isUnchanged = false;
 
-	/// True if new code was deployed to the account
-	bool m_hasNewCode = false;
+    /// True if new code was deployed to the account
+    bool m_hasNewCode = false;
 
-	/// Account's nonce.
-	u256 m_nonce;
+    /// Account's nonce.
+    u256 m_nonce;
 
-	/// Account's balance.
-	u256 m_balance = 0;
+    /// Account's balance.
+    u256 m_balance = 0;
 
-	/// The base storage root. Used with the state DB to give a base to the storage. m_storageOverlay is
-	/// overlaid on this and takes precedence for all values set.
-	h256 m_storageRoot = EmptyTrie;
+    /// The base storage root. Used with the state DB to give a base to the storage. m_storageOverlay is
+    /// overlaid on this and takes precedence for all values set.
+    h256 m_storageRoot = EmptyTrie;
 
-	/** If c_contractConceptionCodeHash then we're in the limbo where we're running the initialisation code.
-	 * We expect a setCode() at some point later.
-	 * If EmptySHA3, then m_code, which should be empty, is valid.
-	 * If anything else, then m_code is valid iff it's not empty, otherwise, State::ensureCached() needs to
-	 * be called with the correct args.
-	 */
-	h256 m_codeHash = EmptySHA3;
+    /** If c_contractConceptionCodeHash then we're in the limbo where we're running the initialisation code.
+     * We expect a setCode() at some point later.
+     * If EmptySHA3, then m_code, which should be empty, is valid.
+     * If anything else, then m_code is valid iff it's not empty, otherwise, State::ensureCached() needs to
+     * be called with the correct args.
+     */
+    h256 m_codeHash = EmptySHA3;
 
-	/// The map with is overlaid onto whatever storage is implied by the m_storageRoot in the trie.
-	std::unordered_map<u256, u256> m_storageOverlay;
+    /// The map with is overlaid onto whatever storage is implied by the m_storageRoot in the trie.
+    std::unordered_map<u256, u256> m_storageOverlay;
 
-	/// The associated code for this account. The SHA3 of this should be equal to m_codeHash unless m_codeHash
-	/// equals c_contractConceptionCodeHash.
-	bytes m_codeCache;
+    /// The associated code for this account. The SHA3 of this should be equal to m_codeHash unless m_codeHash
+    /// equals c_contractConceptionCodeHash.
+    bytes m_codeCache;
 
-	/// Value for m_codeHash when this account is having its code determined.
-	static const h256 c_contractConceptionCodeHash;
+    /// Value for m_codeHash when this account is having its code determined.
+    static const h256 c_contractConceptionCodeHash;
 };
 
 class AccountMask
 {
 public:
-	AccountMask(bool _all = false):
-		m_hasBalance(_all),
-		m_hasNonce(_all),
-		m_hasCode(_all),
-		m_hasStorage(_all)
-	{}
+    AccountMask(bool _all = false):
+        m_hasBalance(_all),
+        m_hasNonce(_all),
+        m_hasCode(_all),
+        m_hasStorage(_all)
+    {}
 
-	AccountMask(
-		bool _hasBalance,
-		bool _hasNonce,
-		bool _hasCode,
-		bool _hasStorage,
-		bool _shouldNotExist = false
-	):
-		m_hasBalance(_hasBalance),
-		m_hasNonce(_hasNonce),
-		m_hasCode(_hasCode),
-		m_hasStorage(_hasStorage),
-		m_shouldNotExist(_shouldNotExist)
-	{}
+    AccountMask(
+        bool _hasBalance,
+        bool _hasNonce,
+        bool _hasCode,
+        bool _hasStorage,
+        bool _shouldNotExist = false
+    ):
+        m_hasBalance(_hasBalance),
+        m_hasNonce(_hasNonce),
+        m_hasCode(_hasCode),
+        m_hasStorage(_hasStorage),
+        m_shouldNotExist(_shouldNotExist)
+    {}
 
-	bool allSet() const { return m_hasBalance && m_hasNonce && m_hasCode && m_hasStorage; }
-	bool hasBalance() const { return m_hasBalance; }
-	bool hasNonce() const { return m_hasNonce; }
-	bool hasCode() const { return m_hasCode; }
-	bool hasStorage() const { return m_hasStorage; }
-	bool shouldExist() const { return !m_shouldNotExist; }
+    bool allSet() const { return m_hasBalance && m_hasNonce && m_hasCode && m_hasStorage; }
+    bool hasBalance() const { return m_hasBalance; }
+    bool hasNonce() const { return m_hasNonce; }
+    bool hasCode() const { return m_hasCode; }
+    bool hasStorage() const { return m_hasStorage; }
+    bool shouldExist() const { return !m_shouldNotExist; }
 
 private:
-	bool m_hasBalance;
-	bool m_hasNonce;
-	bool m_hasCode;
-	bool m_hasStorage;
-	bool m_shouldNotExist = false;
+    bool m_hasBalance;
+    bool m_hasNonce;
+    bool m_hasCode;
+    bool m_hasStorage;
+    bool m_shouldNotExist = false;
 };
 
 using AccountMap = std::unordered_map<Address, Account>;
