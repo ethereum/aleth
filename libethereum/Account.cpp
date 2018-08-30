fdbb63a668148b7a322bdb4a2808872ca43bf357
Andrei Maiboroda <andrei@ethereum.org>
2018-08-30 17:57:15 +0200

    Move m_storageOverlay and m_storageOriginal caches management into Account.

--- libethereum/Account.cpp
+++ libethereum/Account.cpp
@@ -20,8 +20,10 @@
  */
 
 #include "Account.h"
+#include "SecureTrieDB.h"
 #include "ValidationSchemes.h"
 #include <libdevcore/JsonUtils.h>
+#include <libdevcore/OverlayDB.h>
 #include <libethcore/ChainOperationParams.h>
 #include <libethcore/Precompiled.h>
 
@@ -37,6 +39,20 @@ void Account::setCode(bytes&& _code)
     m_codeCache = std::move(_code);
     m_hasNewCode = true;
     m_codeHash = sha3(m_codeCache);
+}
+
+u256 Account::originalStorageValue(u256 const& _key, OverlayDB const& _db) const
+{
+    auto it = m_storageOriginal.find(_key);
+    if (it != m_storageOriginal.end())
+        return it->second;
+
+    // Not in the original values cache - go to the DB.
+    SecureTrieDB<h256, OverlayDB> const memdb(const_cast<OverlayDB*>(&_db), m_storageRoot);
+    std::string const payload = memdb.at(_key);
+    auto const value = payload.size() ? RLP(payload).toInt<u256>() : 0;
+    m_storageOriginal[_key] = value;
+    return value;
 }
 
 namespace js = json_spirit;
