1bff2c4a973d2b310c1656e5e10aa198618e7362
Aaron Clauson <aaron@sipsorcery.com>
2018-09-18 10:58:40 +0200

    Fix for serialising Boost bigint's when using msvc.

--- libdevcore/RLP.h
+++ libdevcore/RLP.h
@@ -437,7 +437,7 @@ private:
 		m_out.resize(m_out.size() + _br);
 		byte* b = &m_out.back();
 		for (; _i; _i >>= 8)
-			*(b--) = (byte)_i;
+			*(b--) = (byte)(_i & 0xff);
 	}
 
 	/// Our output byte stream.
--- test/unittests/libdevcore/RLP.cpp
+++ test/unittests/libdevcore/RLP.cpp
@@ -47,3 +47,20 @@ TEST(RLP, actualSize)
     b = {0xc1, 0x00};
     EXPECT_EQ(RLP{b}.actualSize(), 2);
 }
+
+TEST(RLP, bignumSerialization)
+{
+    // Tests that a bignum can be serialized and deserialized with the RLP classes.
+
+    dev::u256 bignum("10000000000000000000000");
+
+    dev::RLPStream rlpStm{};
+    rlpStm << bignum;
+
+    auto buffer = rlpStm.out();
+
+    dev::RLP rlp{buffer};
+    dev::u256 bignumPost = rlp.toInt<dev::u256>();
+
+    EXPECT_EQ(bignum, bignumPost) << "The post-processed bignum does not match the original.";
+}
