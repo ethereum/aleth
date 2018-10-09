4d03602cc9807f1081dd01682e26e0c15b47e440
Andrei Maiboroda <andrei@ethereum.org>
2018-10-09 18:44:41 +0200

    Remove p2p::PeerCapability.

--- test/unittests/libp2p/peer.cpp
+++ test/unittests/libp2p/peer.cpp
@@ -23,7 +23,6 @@
 #include <test/tools/libtesteth/TestOutputHelper.h>
 #include <test/tools/libtesteth/Options.h>
 #include <libp2p/Host.h>
-#include <libp2p/PeerCapability.h>
 #include <libp2p/HostCapability.h>
 #include <chrono>
 #include <thread>
