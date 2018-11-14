3abb51562f68aef2d2bb3a29a5b908a03da15d78
Andrei Maiboroda <andrei@ethereum.org>
2018-11-14 18:04:23 +0100

    Remove unused libp2p::Peer::operator<

--- libp2p/Peer.cpp
+++ libp2p/Peer.cpp
@@ -1,18 +1,18 @@
 /*
-	This file is part of cpp-ethereum.
+    This file is part of cpp-ethereum.
 
-	cpp-ethereum is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+    cpp-ethereum is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-	cpp-ethereum is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    cpp-ethereum is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
 */
 /** @file Peer.cpp
  * @author Alex Leverington <nessence@gmail.com>
@@ -32,66 +32,44 @@ namespace p2p
 {
 
 Peer::Peer(Peer const& _original):
-	Node(_original),
-	m_lastConnected(_original.m_lastConnected),
-	m_lastAttempted(_original.m_lastAttempted),
-	m_lastDisconnect(_original.m_lastDisconnect),
-	m_session(_original.m_session)
+    Node(_original),
+    m_lastConnected(_original.m_lastConnected),
+    m_lastAttempted(_original.m_lastAttempted),
+    m_lastDisconnect(_original.m_lastDisconnect),
+    m_session(_original.m_session)
 {
-	m_score = _original.m_score.load();
-	m_rating = _original.m_rating.load();
-	m_failedAttempts = _original.m_failedAttempts.load();
+    m_score = _original.m_score.load();
+    m_rating = _original.m_rating.load();
+    m_failedAttempts = _original.m_failedAttempts.load();
 }
 
 bool Peer::shouldReconnect() const
 {
-	return id && endpoint && chrono::system_clock::now() > m_lastAttempted + chrono::seconds(fallbackSeconds());
+    return id && endpoint && chrono::system_clock::now() > m_lastAttempted + chrono::seconds(fallbackSeconds());
 }
-	
+    
 unsigned Peer::fallbackSeconds() const
 {
-	if (peerType == PeerType::Required)
-		return 5;
-	switch (m_lastDisconnect)
-	{
-	case BadProtocol:
-		return 30 * (m_failedAttempts + 1);
-	case UselessPeer:
-	case TooManyPeers:
-		return 25 * (m_failedAttempts + 1);
-	case ClientQuit:
-		return 15 * (m_failedAttempts + 1);
-	case NoDisconnect:
-	default:
-		if (m_failedAttempts < 5)
-			return m_failedAttempts ? m_failedAttempts * 5 : 5;
-		else if (m_failedAttempts < 15)
-			return 25 + (m_failedAttempts - 5) * 10;
-		else
-			return 25 + 100 + (m_failedAttempts - 15) * 20;
-	}
-}
-	
-bool Peer::operator<(Peer const& _p) const
-{
-	if (isOffline() != _p.isOffline())
-		return isOffline();
-	else if (isOffline())
-		if (m_lastAttempted == _p.m_lastAttempted)
-			return m_failedAttempts < _p.m_failedAttempts;
-		else
-			return m_lastAttempted < _p.m_lastAttempted;
-		else
-			if (m_score == _p.m_score)
-				if (m_rating == _p.m_rating)
-					if (m_failedAttempts == _p.m_failedAttempts)
-						return id < _p.id;
-					else
-						return m_failedAttempts < _p.m_failedAttempts;
-					else
-						return m_rating < _p.m_rating;
-					else
-						return m_score < _p.m_score;
+    if (peerType == PeerType::Required)
+        return 5;
+    switch (m_lastDisconnect)
+    {
+    case BadProtocol:
+        return 30 * (m_failedAttempts + 1);
+    case UselessPeer:
+    case TooManyPeers:
+        return 25 * (m_failedAttempts + 1);
+    case ClientQuit:
+        return 15 * (m_failedAttempts + 1);
+    case NoDisconnect:
+    default:
+        if (m_failedAttempts < 5)
+            return m_failedAttempts ? m_failedAttempts * 5 : 5;
+        else if (m_failedAttempts < 15)
+            return 25 + (m_failedAttempts - 5) * 10;
+        else
+            return 25 + 100 + (m_failedAttempts - 15) * 20;
+    }
 }
 
 }
--- libp2p/Peer.h
+++ libp2p/Peer.h
@@ -1,18 +1,18 @@
 /*
-	This file is part of cpp-ethereum.
+    This file is part of cpp-ethereum.
 
-	cpp-ethereum is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+    cpp-ethereum is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-	cpp-ethereum is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    cpp-ethereum is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
 */
 /** @file Peer.h
  * @author Alex Leverington <nessence@gmail.com>
@@ -51,52 +51,50 @@ namespace p2p
  */
 class Peer: public Node
 {
-	friend class Session;		/// Allows Session to update score and rating.
-	friend class Host;		/// For Host: saveNetwork(), restoreNetwork()
+    friend class Session;		/// Allows Session to update score and rating.
+    friend class Host;		/// For Host: saveNetwork(), restoreNetwork()
 
-	friend class RLPXHandshake;
+    friend class RLPXHandshake;
 
 public:
-	/// Construct Peer from Node.
-	Peer(Node const& _node): Node(_node) {}
+    /// Construct Peer from Node.
+    Peer(Node const& _node): Node(_node) {}
 
-	Peer(Peer const&);
-	
-	bool isOffline() const { return !m_session.lock(); }
+    Peer(Peer const&);
+    
+    bool isOffline() const { return !m_session.lock(); }
 
-	virtual bool operator<(Peer const& _p) const;
-	
-	/// WIP: Returns current peer rating.
-	int rating() const { return m_rating; }
-	
-	/// Return true if connection attempt should be made to this peer or false if
-	bool shouldReconnect() const;
-	
-	/// Number of times connection has been attempted to peer.
-	int failedAttempts() const { return m_failedAttempts; }
+    /// WIP: Returns current peer rating.
+    int rating() const { return m_rating; }
 
-	/// Reason peer was previously disconnected.
-	DisconnectReason lastDisconnect() const { return m_lastDisconnect; }
-	
-	/// Peer session is noted as useful.
-	void noteSessionGood() { m_failedAttempts = 0; }
+    /// Return true if connection attempt should be made to this peer or false if
+    bool shouldReconnect() const;
+
+    /// Number of times connection has been attempted to peer.
+    int failedAttempts() const { return m_failedAttempts; }
+
+    /// Reason peer was previously disconnected.
+    DisconnectReason lastDisconnect() const { return m_lastDisconnect; }
+
+    /// Peer session is noted as useful.
+    void noteSessionGood() { m_failedAttempts = 0; }
 
 private:
     /// Returns number of seconds to wait until attempting connection, based on attempted connection history.
-	unsigned fallbackSeconds() const;
+    unsigned fallbackSeconds() const;
 
-	std::atomic<int> m_score{0};									///< All time cumulative.
-	std::atomic<int> m_rating{0};									///< Trending.
-	
-	/// Network Availability
-	
-	std::chrono::system_clock::time_point m_lastConnected;
-	std::chrono::system_clock::time_point m_lastAttempted;
-	std::atomic<unsigned> m_failedAttempts{0};
-	DisconnectReason m_lastDisconnect = NoDisconnect;	///< Reason for disconnect that happened last.
+    std::atomic<int> m_score{0};									///< All time cumulative.
+    std::atomic<int> m_rating{0};									///< Trending.
+    
+    /// Network Availability
+    
+    std::chrono::system_clock::time_point m_lastConnected;
+    std::chrono::system_clock::time_point m_lastAttempted;
+    std::atomic<unsigned> m_failedAttempts{0};
+    DisconnectReason m_lastDisconnect = NoDisconnect;	///< Reason for disconnect that happened last.
 
-	/// Used by isOffline() and (todo) for peer to emit session information.
-	std::weak_ptr<Session> m_session;
+    /// Used by isOffline() and (todo) for peer to emit session information.
+    std::weak_ptr<Session> m_session;
 };
 using Peers = std::vector<Peer>;
 
