ce41956558df2ea7bbeb009caee1e4bbf3304057
Nils-Erik Frantzell <nilserik.frantzell@gmail.com>
2019-01-09 20:19:21 -0800

    Remove network start/stop RPC methods
    Removing these functions will enable us to significantly simplify the design of the Host class's start/stop logic due to the lack of needing to support network restart.

--- libweb3jsonrpc/AdminNetFace.h
+++ libweb3jsonrpc/AdminNetFace.h
@@ -14,8 +14,6 @@ namespace dev {
             public:
                 AdminNetFace()
                 {
-                    this->bindAndAddMethod(jsonrpc::Procedure("admin_net_start", jsonrpc::PARAMS_BY_POSITION, jsonrpc::JSON_BOOLEAN, "param1",jsonrpc::JSON_STRING, NULL), &dev::rpc::AdminNetFace::admin_net_startI);
-                    this->bindAndAddMethod(jsonrpc::Procedure("admin_net_stop", jsonrpc::PARAMS_BY_POSITION, jsonrpc::JSON_BOOLEAN, "param1",jsonrpc::JSON_STRING, NULL), &dev::rpc::AdminNetFace::admin_net_stopI);
                     this->bindAndAddMethod(jsonrpc::Procedure("admin_net_connect", jsonrpc::PARAMS_BY_POSITION, jsonrpc::JSON_BOOLEAN, "param1",jsonrpc::JSON_STRING,"param2",jsonrpc::JSON_STRING, NULL), &dev::rpc::AdminNetFace::admin_net_connectI);
                     this->bindAndAddMethod(jsonrpc::Procedure("admin_net_peers", jsonrpc::PARAMS_BY_POSITION, jsonrpc::JSON_ARRAY, "param1",jsonrpc::JSON_STRING, NULL), &dev::rpc::AdminNetFace::admin_net_peersI);
                     this->bindAndAddMethod(jsonrpc::Procedure("admin_net_nodeInfo", jsonrpc::PARAMS_BY_POSITION, jsonrpc::JSON_OBJECT, "param1",jsonrpc::JSON_STRING, NULL), &dev::rpc::AdminNetFace::admin_net_nodeInfoI);
@@ -24,14 +22,6 @@ namespace dev {
                     this->bindAndAddMethod(jsonrpc::Procedure("admin_addPeer", jsonrpc::PARAMS_BY_POSITION, jsonrpc::JSON_BOOLEAN, "param1",jsonrpc::JSON_STRING, NULL), &dev::rpc::AdminNetFace::admin_addPeerI);
                 }
 
-                inline virtual void admin_net_startI(const Json::Value &request, Json::Value &response)
-                {
-                    response = this->admin_net_start(request[0u].asString());
-                }
-                inline virtual void admin_net_stopI(const Json::Value &request, Json::Value &response)
-                {
-                    response = this->admin_net_stop(request[0u].asString());
-                }
                 inline virtual void admin_net_connectI(const Json::Value &request, Json::Value &response)
                 {
                     response = this->admin_net_connect(request[0u].asString(), request[1u].asString());
@@ -58,8 +48,6 @@ namespace dev {
                 {
                     response = this->admin_addPeer(request[0u].asString());
                 }
-                virtual bool admin_net_start(const std::string& param1) = 0;
-                virtual bool admin_net_stop(const std::string& param1) = 0;
                 virtual bool admin_net_connect(const std::string& param1, const std::string& param2) = 0;
                 virtual Json::Value admin_net_peers(const std::string& param1) = 0;
                 virtual Json::Value admin_net_nodeInfo(const std::string& param1) = 0;
