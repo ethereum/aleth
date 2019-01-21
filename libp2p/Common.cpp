b1d2f89a9afb15eeca6d4817ff5fce6e3348dd1a
Nils-Erik Frantzell <nilserik.frantzell@gmail.com>
2019-01-21 09:27:09 -0800

    Remove static from constexprs in Network
    Also use c_localhostIp in p2p::isLocalHostAddress()

--- libp2p/Common.cpp
+++ libp2p/Common.cpp
@@ -93,7 +93,7 @@ bool p2p::isLocalHostAddress(bi::address const& _addressToCheck)
 {
     // @todo: ivp6 link-local adresses (macos), ex: fe80::1%lo0
     static const set<bi::address> c_rejectAddresses = {
-        {bi::address_v4::from_string("127.0.0.1")},
+        {bi::address_v4::from_string(c_localhostIp)},
         {bi::address_v4::from_string("0.0.0.0")},
         {bi::address_v6::from_string("::1")},
         {bi::address_v6::from_string("::")}
--- libp2p/Network.h
+++ libp2p/Network.h
@@ -37,8 +37,8 @@ namespace dev
 namespace p2p
 {
 
-static constexpr unsigned short c_defaultListenPort = 30303;
-static constexpr const char* c_localhostIp = "127.0.0.1";
+constexpr unsigned short c_defaultListenPort = 30303;
+constexpr const char* c_localhostIp = "127.0.0.1";
 
 struct NetworkConfig
 {
