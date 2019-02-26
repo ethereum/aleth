0e92c4defc92cc13109176c55f6900fecf1f19d5
Paweł Bylica <chfast@gmail.com>
2019-02-26 14:40:44 +0100

    CMake: Pass custom CXX_FLAGS to C++ ExternalProjects

--- cmake/ProjectLibFF.cmake
+++ cmake/ProjectLibFF.cmake
@@ -19,6 +19,7 @@ ExternalProject_Add(libff
         -DUSE_PT_COMPRESSION=Off
         -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
         -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+        -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
     BUILD_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --config Release
     LOG_BUILD 1
     INSTALL_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --config Release --target install
