b7049d9ada0fa8e1bc2ca25b8000a3ef5dd4dbd7
Andrei Maiboroda <andrei@ethereum.org>
2019-05-21 12:00:43 +0200

    Tests for updating ENR

--- test/unittests/libp2p/ENRTest.cpp
+++ test/unittests/libp2p/ENRTest.cpp
@@ -59,6 +59,31 @@ TEST(enr, createAndParse)
     EXPECT_EQ(enr1.keyValuePairs(), enr2.keyValuePairs());
 }
 
+TEST(enr, update)
+{
+    auto keyPair = KeyPair::create();
+
+    ENR const enr1 = IdentitySchemeV4::createENR(
+        keyPair.secret(), bi::address::from_string("127.0.0.1"), 3322, 5544);
+
+    EXPECT_EQ(enr1.sequenceNumber(), 1);
+
+    ENR const enr2 = IdentitySchemeV4::updateENR(
+        enr1, keyPair.secret(), bi::address::from_string("127.0.0.1"), 3323, 5545);
+
+    EXPECT_EQ(enr2.sequenceNumber(), 2);
+
+    RLPStream s;
+    enr2.streamRLP(s);
+    bytes rlp = s.out();
+
+    ENR enrParsed = IdentitySchemeV4::parseENR(RLP{rlp});
+
+    EXPECT_EQ(enrParsed.sequenceNumber(), 2);
+    EXPECT_EQ(enrParsed.tcpPort(), 3323);
+    EXPECT_EQ(enrParsed.udpPort(), 5545);
+}
+
 TEST(enr, parseTooBigRlp)
 {
     std::map<std::string, bytes> keyValuePairs = {{"key", rlp(bytes(300, 'a'))}};
@@ -168,4 +193,25 @@ TEST(enr, createV4)
     EXPECT_EQ(keyValuePairs["tcp"], rlp(3322));
     EXPECT_TRUE(contains(keyValuePairs, std::string("udp")));
     EXPECT_EQ(keyValuePairs["udp"], rlp(5544));
+}
+
+TEST(enr, predefinedKeys)
+{
+    auto keyPair = KeyPair::create();
+    ENR enr = IdentitySchemeV4::createENR(
+        keyPair.secret(), bi::address::from_string("127.0.0.1"), 3322, 5544);
+
+    EXPECT_EQ(enr.id(), "v4");
+    EXPECT_EQ(enr.ip(), bi::address::from_string("127.0.0.1"));
+    EXPECT_EQ(enr.tcpPort(), 3322);
+    EXPECT_EQ(enr.udpPort(), 5544);
+}
+
+TEST(enr, publicKeyV4)
+{
+    auto keyPair = KeyPair::create();
+    ENR enr = IdentitySchemeV4::createENR(
+        keyPair.secret(), bi::address::from_string("127.0.0.1"), 3322, 5544);
+
+    EXPECT_EQ(IdentitySchemeV4::publicKey(enr), toPublicCompressed(keyPair.secret()));
 }
