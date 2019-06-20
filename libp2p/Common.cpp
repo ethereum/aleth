8ca0e91617b6fe0594f7207804d66e2f8e1a6c04
Andrei Maiboroda <andrei@ethereum.org>
2019-06-20 18:35:53 +0200

    Remove "Ethereum Foundation C++ Bootnode"
    It's outdated, I don't think it's alive anymore.

--- libp2p/Common.cpp
+++ libp2p/Common.cpp
@@ -253,9 +253,6 @@ vector<pair<Public, const char*>> defaultBootNodes()
         {Public("158f8aab45f6d19c6cbf4a089c2670541a8da11978a2f90dbf6a502a4a3bab80d288afdbeb7ec0ef6d92de563767f3b1ea9e8e334ca711e9f8e2df5a0385e8e6"), "13.75.154.138:30303"}, // AU
         {Public("1118980bf48b0a3640bdba04e0fe78b1add18e1cd99bf22d53daac1fd9972ad650df52176e7c7d89d1114cfef2bc23a2959aa54998a46afcf7d91809f0855082"), "52.74.57.123:30303"}, // SG
         
-        // --> Ethereum Foundation C++ Bootnodes
-        {Public("979b7fa28feeb35a4741660a16076f1943202cb72b6af70d327f053e248bab9ba81760f39d0701ef1d8f89cc1fbd2cacba0710a12cd5314d5e0c9021aa3637f9"), "5.1.83.226:30303"}, // DE
-        
         // Ropsten:
         {Public("30b7ab30a01c124a6cceca36863ece12c4f5fa68e3ba9b0b51407ccc002eeed3b3102d20a88f1c1d3c3154e2449317b8ef95090e77b312d5cc39354f86d5d606"), "52.176.7.10:30303"}, // US-Azure geth
         {Public("865a63255b3bb68023b6bffd5095118fcc13e79dcf014fe4e47e065c350c7cc72af2e53eff895f11ba1bbb6a2b33271c1116ee870f266618eadfc2e78aa7349c"), "52.176.100.77:30303"}, // US-Azure parity
