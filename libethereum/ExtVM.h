23457bc10d3f445c9fadd86bc8440ee4ca183c70
Paweł Bylica <chfast@gmail.com>
2019-09-06 16:59:56 +0200

    Rename ExtVMFace.suicide() to ExtVMFace.selfdestruct()

--- libethereum/ExtVM.h
+++ libethereum/ExtVM.h
@@ -94,7 +94,7 @@ public:
             return m_s.addressInUse(_a);
     }
 
-    /// Suicide the associated contract to the given address.
+    /// Selfdestruct the associated contract to the given address.
     void selfdestruct(Address _a) final;
 
     /// Return the EVM gas-price schedule for this execution context.
