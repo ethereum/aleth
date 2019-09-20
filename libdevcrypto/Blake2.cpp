2f8e271ea1174785996c82c730d9675e6c0d3742
Andrei Maiboroda <andrei@ethereum.org>
2019-09-20 16:05:07 +0200

    Minor stylistic and formatting improvements

--- libdevcrypto/Blake2.cpp
+++ libdevcrypto/Blake2.cpp
@@ -4,9 +4,7 @@
 
 #include "Blake2.h"
 
-
 #include <libdevcore/Exceptions.h>
-#include <libdevcore/Log.h>
 
 namespace dev
 {
@@ -32,25 +30,31 @@ typedef struct blake2b_state__
     uint8_t last_node;
 } blake2b_state;
 
-static const uint64_t blake2b_IV[8] = {0x6a09e667f3bcc908ULL, 0xbb67ae8584caa73bULL,
-    0x3c6ef372fe94f82bULL, 0xa54ff53a5f1d36f1ULL, 0x510e527fade682d1ULL, 0x9b05688c2b3e6c1fULL,
-    0x1f83d9abfb41bd6bULL, 0x5be0cd19137e2179ULL};
+// clang-format off
+const uint64_t blake2b_IV[8] =
+{
+  0x6a09e667f3bcc908ULL, 0xbb67ae8584caa73bULL,
+  0x3c6ef372fe94f82bULL, 0xa54ff53a5f1d36f1ULL,
+  0x510e527fade682d1ULL, 0x9b05688c2b3e6c1fULL,
+  0x1f83d9abfb41bd6bULL, 0x5be0cd19137e2179ULL
+};
 
-static const uint8_t blake2b_sigma[12][16] = {
-    {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
-    {14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3},
-    {11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4},
-    {7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8},
-    {9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13},
-    {2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9},
-    {12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11},
-    {13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10},
-    {6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5},
-    {10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0},
-    {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
-    {14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3}};
+const uint8_t blake2b_sigma[12][16] =
+{
+  {  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 } ,
+  { 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3 } ,
+  { 11,  8, 12,  0,  5,  2, 15, 13, 10, 14,  3,  6,  7,  1,  9,  4 } ,
+  {  7,  9,  3,  1, 13, 12, 11, 14,  2,  6,  5, 10,  4,  0, 15,  8 } ,
+  {  9,  0,  5,  7,  2,  4, 10, 15, 14,  1, 11, 12,  6,  8,  3, 13 } ,
+  {  2, 12,  6, 10,  0, 11,  8,  3,  4, 13,  7,  5, 15, 14,  1,  9 } ,
+  { 12,  5,  1, 15, 14, 13,  4, 10,  0,  7,  6,  3,  9,  2,  8, 11 } ,
+  { 13, 11,  7, 14, 12,  1,  3,  9,  5,  0, 15,  4,  8,  6,  2, 10 } ,
+  {  6, 15, 14,  9, 11,  3,  0,  8, 12,  2, 13,  7,  1,  4, 10,  5 } ,
+  { 10,  2,  8,  4,  7,  6,  1,  5, 15, 11,  9, 14,  3, 12, 13 , 0 } ,
+};
+// clang-format on
 
-static inline uint64_t load64(const void* src)
+inline uint64_t load64(const void* src) noexcept
 {
 #if defined(NATIVE_LITTLE_ENDIAN)
     uint64_t w;
@@ -64,7 +68,7 @@ static inline uint64_t load64(const void* src)
 #endif
 }
 
-static inline void store64(void* dst, uint64_t w)
+inline void store64(void* dst, uint64_t w) noexcept
 {
 #if defined(NATIVE_LITTLE_ENDIAN)
     memcpy(dst, &w, sizeof w);
@@ -81,12 +85,13 @@ static inline void store64(void* dst, uint64_t w)
 #endif
 }
 
-static inline uint64_t rotr64(const uint64_t w, const unsigned c)
+inline uint64_t rotr64(const uint64_t w, const unsigned c) noexcept
 {
     return (w >> c) | (w << (64 - c));
 }
 
-inline void G(uint8_t r, uint8_t i, uint64_t& a, uint64_t& b, uint64_t& c, uint64_t& d, uint64_t* m)
+inline void G(
+    uint8_t r, uint8_t i, uint64_t& a, uint64_t& b, uint64_t& c, uint64_t& d, uint64_t* m) noexcept
 {
     a = a + b + m[blake2b_sigma[r][2 * i + 0]];
     d = rotr64(d ^ a, 32);
@@ -98,7 +103,7 @@ inline void G(uint8_t r, uint8_t i, uint64_t& a, uint64_t& b, uint64_t& c, uint6
     b = rotr64(b ^ c, 63);
 }
 
-inline void ROUND(uint32_t round, uint64_t* v, uint64_t* m)
+inline void ROUND(uint32_t round, uint64_t* v, uint64_t* m) noexcept
 {
     uint8_t const r = round % 10;
     G(r, 0, v[0], v[4], v[8], v[12], m);
@@ -112,8 +117,8 @@ inline void ROUND(uint32_t round, uint64_t* v, uint64_t* m)
 }
 
 
-static void blake2b_compress(
-    uint32_t _rounds, blake2b_state* S, const uint8_t block[BLAKE2B_BLOCKBYTES])
+void blake2b_compress(
+    uint32_t rounds, blake2b_state* S, const uint8_t block[BLAKE2B_BLOCKBYTES]) noexcept
 {
     uint64_t m[16];
     uint64_t v[16];
@@ -138,7 +143,7 @@ static void blake2b_compress(
     v[14] = blake2b_IV[6] ^ S->f[0];
     v[15] = blake2b_IV[7] ^ S->f[1];
 
-    for (uint32_t r = 0; r < _rounds; ++r)
+    for (uint32_t r = 0; r < rounds; ++r)
         ROUND(r, v, m);
 
     for (i = 0; i < 8; ++i)
@@ -146,7 +151,6 @@ static void blake2b_compress(
         S->h[i] = S->h[i] ^ v[i] ^ v[i + 8];
     }
 }
-
 
 }  // namespace
 
@@ -161,15 +165,13 @@ bytes blake2FCompression(uint32_t _rounds, bytesConstRef _stateVector, bytesCons
     if (_t0.size() != 8 || _t1.size() != 8)
         BOOST_THROW_EXCEPTION(InvalidInputSize());
 
-
     blake2b_state s{};
     for (size_t i = 0; i < 8; ++i)
-        s.h[i] = load64(&_stateVector[i * sizeof(s.h[i])]);
+        s.h[i] = load64(&_stateVector[i * sizeof(uint64_t)]);
 
     s.t[0] = load64(_t0.data());
     s.t[1] = load64(_t1.data());
     s.f[0] = _lastBlock ? (uint64_t)-1 : 0;
-    s.f[1] = 0;
 
     uint8_t block[BLAKE2B_BLOCKBYTES];
     std::copy(_messageBlockVector.begin(), _messageBlockVector.end(), &block[0]);
@@ -178,11 +180,10 @@ bytes blake2FCompression(uint32_t _rounds, bytesConstRef _stateVector, bytesCons
 
     bytes result(64);
     for (size_t i = 0; i < 8; ++i)
-        store64(&result[i * sizeof(s.h[i])], s.h[i]);
+        store64(&result[i * sizeof(uint64_t)], s.h[i]);
 
     return result;
 }
-
 
 }  // namespace crypto
 }  // namespace dev
