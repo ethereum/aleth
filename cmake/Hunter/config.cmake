47d2be729600b9a863a04a911c4945f065115bd4
moneroexamples <moneroexamples@tuta.io>
2019-10-26 07:34:22 +0800

    Upgrade GTest to 1.8.1
    Related issues are:
    https://github.com/ethereum/aleth/issues/5790
    https://github.com/ethereum/aleth/issues/5637

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -25,3 +25,11 @@ hunter_config(
     URL https://github.com/chfast/intx/archive/v0.4.0.tar.gz
     SHA1 8a2a0b0efa64899db972166a9b3568a6984c61bc
 )
+
+hunter_config(
+    GTest
+    VERSION 1.8.1
+    URL https://github.com/google/googletest/archive/release-1.8.1.tar.gz
+    SHA1 152b849610d91a9dfa1401293f43230c2e0c33f8
+    CMAKE_ARGS BUILD_GMOCK=OFF gtest_force_shared_crt=ON
+)
