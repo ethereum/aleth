7b5adecb6c1e97a3dab4c3bb8d498f650cea1886
Andrei Maiboroda <andrei@ethereum.org>
2019-10-29 19:17:30 +0100

    Convert tabs to spaces in ChainParams

--- libethereum/ChainParams.h
+++ libethereum/ChainParams.h
@@ -20,32 +20,32 @@ class SealEngineFace;
 
 struct ChainParams: public ChainOperationParams
 {
-	ChainParams();
-	ChainParams(ChainParams const& /*_org*/) = default;
-	ChainParams(std::string const& _s, h256 const& _stateRoot = h256());
-	ChainParams(bytes const& _genesisRLP, AccountMap const& _state) { populateFromGenesis(_genesisRLP, _state); }
-	ChainParams(std::string const& _json, bytes const& _genesisRLP, AccountMap const& _state): ChainParams(_json) { populateFromGenesis(_genesisRLP, _state); }
+    ChainParams();
+    ChainParams(ChainParams const& /*_org*/) = default;
+    ChainParams(std::string const& _s, h256 const& _stateRoot = h256());
+    ChainParams(bytes const& _genesisRLP, AccountMap const& _state) { populateFromGenesis(_genesisRLP, _state); }
+    ChainParams(std::string const& _json, bytes const& _genesisRLP, AccountMap const& _state): ChainParams(_json) { populateFromGenesis(_genesisRLP, _state); }
 
-	SealEngineFace* createSealEngine();
+    SealEngineFace* createSealEngine();
 
-	/// Genesis params.
-	h256 parentHash = h256();
-	Address author = Address();
-	u256 difficulty = 1;
-	u256 gasLimit = 1 << 31;
-	u256 gasUsed = 0;
-	u256 timestamp = 0;
-	bytes extraData;
-	mutable h256 stateRoot;	///< Only pre-populate if known equivalent to genesisState's root. If they're different Bad Things Will Happen.
-	AccountMap genesisState;
+    /// Genesis params.
+    h256 parentHash = h256();
+    Address author = Address();
+    u256 difficulty = 1;
+    u256 gasLimit = 1 << 31;
+    u256 gasUsed = 0;
+    u256 timestamp = 0;
+    bytes extraData;
+    mutable h256 stateRoot;	///< Only pre-populate if known equivalent to genesisState's root. If they're different Bad Things Will Happen.
+    AccountMap genesisState;
 
-	unsigned sealFields = 0;
-	bytes sealRLP;
+    unsigned sealFields = 0;
+    bytes sealRLP;
 
-	h256 calculateStateRoot(bool _force = false) const;
+    h256 calculateStateRoot(bool _force = false) const;
 
-	/// Genesis block info.
-	bytes genesisBlock() const;
+    /// Genesis block info.
+    bytes genesisBlock() const;
 
     /// load config
     ChainParams loadConfig(std::string const& _json, h256 const& _stateRoot = {},
