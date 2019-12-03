e05c2d6348b953a938af1509a4563101d58a3962
Andrei Maiboroda <andrei@ethereum.org>
2019-12-03 17:51:24 +0100

    Activate Muir Glacier form on Mainnet and Ropsten

---
+++ libethashseal/genesis/test/muirGlacierTest.cpp
@@ -0,0 +1,47 @@
+// Aleth: Ethereum C++ client, tools and libraries.
+// Copyright 2016-2019 Aleth Authors.
+// Licensed under the GNU General Public License, Version 3.
+
+#include "../../GenesisInfo.h"
+
+static std::string const c_genesisInfoMuirGlacierTest =
+R"E(
+{
+    "sealEngine": "Ethash",
+    "params": {
+        "accountStartNonce": "0x00",
+        "maximumExtraDataSize": "0x20",
+        "homesteadForkBlock": "0x00",
+        "EIP150ForkBlock": "0x00",
+        "EIP158ForkBlock": "0x00",
+        "byzantiumForkBlock": "0x00",
+        "constantinopleForkBlock": "0x00",
+        "constantinopleFixForkBlock": "0x00",
+        "istanbulForkBlock": "0x00",
+        "muirGlacierForkBlock": "0x00",
+        "minGasLimit": "0x1388",
+        "maxGasLimit": "7fffffffffffffff",
+        "tieBreakingGas": false,
+        "gasLimitBoundDivisor": "0x0400",
+        "minimumDifficulty": "0x020000",
+        "difficultyBoundDivisor": "0x0800",
+        "durationLimit": "0x0d",
+        "blockReward": "0x4563918244F40000",
+        "networkID" : "0x1",
+        "chainID": "0x01",
+        "allowFutureBlocks" : true
+    },
+    "genesis": {
+        "nonce": "0x0000000000000042",
+        "difficulty": "0x020000",
+        "mixHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "author": "0x0000000000000000000000000000000000000000",
+        "timestamp": "0x00",
+        "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "extraData": "0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa",
+        "gasLimit": "0x1388"
+    },
+    "accounts": {
+    }
+}
+)E";
