8fadd48c0bd82e2038e95f4ca0bbec6901d9d883
Dimitry Kh <dimitry@ethereum.org>
2020-06-24 13:51:46 +0300

    eth_getBlockBy* empty extraData is exported as "0x"

--- libweb3jsonrpc/JsonHelper.cpp
+++ libweb3jsonrpc/JsonHelper.cpp
@@ -83,7 +83,7 @@ Json::Value toJson(dev::eth::BlockHeader const& _bi, SealEngineFace* _sealer)
         res["number"] = toJS(_bi.number());
         res["gasUsed"] = toJS(_bi.gasUsed());
         res["gasLimit"] = toJS(_bi.gasLimit());
-        res["extraData"] = toJS(_bi.extraData());
+        res["extraData"] = _bi.extraData().empty() ? "0x" : toJS(_bi.extraData());
         res["logsBloom"] = toJS(_bi.logBloom());
         res["timestamp"] = toJS(_bi.timestamp());
         // TODO: remove once JSONRPC spec is updated to use "author" over "miner".
